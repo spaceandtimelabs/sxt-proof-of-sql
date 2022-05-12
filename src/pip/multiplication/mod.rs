45cfbb6d06a808f94bf78658d048a49a2c56fd80
Ryan Burn <ryan.burn@gmail.com>
2022-05-12 14:31:30 -0700

    add skeleton of multiplication protocol

---
+++ src/pip/multiplication/mod.rs
@@ -0,0 +1,41 @@
+use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::scalar::Scalar;
+use merlin::Transcript;
+
+use crate::errors::ProofError;
+
+#[cfg(test)]
+mod test;
+
+#[derive(Clone, Debug)]
+pub struct MultiplicationProof {}
+
+impl MultiplicationProof {
+    /// Create a multiplication proof.
+    ///
+    /// See protocol/multiplication.pdf
+    #[allow(unused_variables)]
+    pub fn create(
+        transcript: &mut Transcript,
+        a_vec: &[Scalar],
+        b_vec: &[Scalar],
+    ) -> MultiplicationProof {
+        let n = a_vec.len();
+
+        assert_eq!(a_vec.len(), n);
+        assert_eq!(b_vec.len(), n);
+
+        MultiplicationProof {}
+    }
+
+    /// Verifies that a multiplication proof is correct given the associated commitments.
+    #[allow(unused_variables)]
+    pub fn verify(
+        &self,
+        transcript: &mut Transcript,
+        commit_a: &RistrettoPoint,
+        commit_b: &RistrettoPoint,
+    ) -> Result<(), ProofError> {
+        Ok(())
+    }
+}
