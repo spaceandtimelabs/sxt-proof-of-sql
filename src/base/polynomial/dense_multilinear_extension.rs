4f9a70be57af2ac75e8605794857cf632c3a48df
Ryan Burn <ryan.burn@gmail.com>
2022-05-26 10:01:26 -0700

    fill in dense multilinear extensions

--- src/base/polynomial/dense_multilinear_extension.rs
+++ src/base/polynomial/dense_multilinear_extension.rs
@@ -1,3 +1,15 @@
-use ark_poly::DenseMultilinearExtension;
+use ark_poly;
+use curve25519_dalek::scalar::Scalar;
 
+use crate::base::polynomial::ark_scalar::ArkScalar;
 
+pub struct DenseMultilinearExtension(ark_poly::DenseMultilinearExtension<ArkScalar>);
+
+impl DenseMultilinearExtension {
+    /// Construct a new polynomial from a list of evaluations where the index
+    /// represents a point in {0,1}^`num_vars` in little endian form. For
+    /// example, `0b1011` represents `P(1,1,0,1)`
+    #[allow(unused_variables)]
+    pub fn from_evaluations_slice(num_vars: usize, evaluations: &[Scalar]) {
+    }
+}
