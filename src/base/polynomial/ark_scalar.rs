bff18030993e982e77123272d1e644db8232626e
Ryan Burn <ryan.burn@gmail.com>
2022-05-26 15:36:58 -0700

    work on dalek interop

--- src/base/polynomial/ark_scalar.rs
+++ src/base/polynomial/ark_scalar.rs
@@ -1,7 +1,17 @@
+use curve25519_dalek::scalar::Scalar;
+use byte_slice_cast::AsMutByteSlice;
 use ark_ff::fields::{Fp256, MontBackend, MontConfig, MontFp};
+use ark_ff::BigInt;
 
 #[derive(MontConfig)]
 #[modulus = "7237005577332262213973186563042994240857116359379907606001950938285454250989"]
 #[generator = "2"]
-pub struct FqConfig;
-pub type ArkScalar = Fp256<MontBackend<FqConfig, 4>>;
+pub struct ArkScalarConfig;
+pub type ArkScalar = Fp256<MontBackend<ArkScalarConfig, 4>>;
+
+#[allow(unused_variables)]
+pub fn to_ark_scalar(x: Scalar) -> ArkScalar {
+    let mut values: [u64; 4] = [0; 4];
+    values.as_mut_byte_slice().clone_from_slice(x.as_bytes());
+    ArkScalar::new(BigInt::new(values))
+}
--- src/base/polynomial/ark_scalar_test.rs
+++ src/base/polynomial/ark_scalar_test.rs
@@ -1,5 +1,6 @@
-use crate::base::polynomial::ark_scalar::ArkScalar;
+use crate::base::polynomial::ark_scalar::*;
 
+use curve25519_dalek::scalar::Scalar;
 use ark_ff::BigInt;
 
 #[test]
@@ -13,10 +14,19 @@ fn test_add() {
 
 #[test]
 fn test_mod() {
-    const pm1 : BigInt<4> = BigInt!("7237005577332262213973186563042994240857116359379907606001950938285454250988");
+    let pm1 : BigInt<4> = BigInt!("7237005577332262213973186563042994240857116359379907606001950938285454250988");
     let x = ArkScalar::new(pm1);
     let one = ArkScalar::new(BigInt::new([1, 0, 0, 0]));
     let zero = ArkScalar::new(BigInt::new([0, 0, 0, 0]));
     let xp1 = x + one;
     assert!(xp1 == zero);
+}
+
+#[test]
+fn test_dalek_interop() {
+    let x = Scalar::from(123u64);
+    let mx = -x;
+    let xp = to_ark_scalar(x);
+    let mxp = -xp;
+    assert!(mxp == to_ark_scalar(mx));
 }
