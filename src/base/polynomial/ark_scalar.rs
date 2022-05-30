b04795dd4ec9e768273cf79f2fa50ee41ae56df0
Ryan Burn <ryan.burn@gmail.com>
2022-05-30 12:43:24 -0700

    work on scalar conversion

--- src/base/polynomial/ark_scalar.rs
+++ src/base/polynomial/ark_scalar.rs
@@ -1,7 +1,7 @@
 use curve25519_dalek::scalar::Scalar;
 use byte_slice_cast::AsMutByteSlice;
 use ark_ff::fields::{Fp256, MontBackend, MontConfig, MontFp};
-use ark_ff::BigInt;
+use ark_ff::{BigInt, ToBytes, One};
 
 #[derive(MontConfig)]
 #[modulus = "7237005577332262213973186563042994240857116359379907606001950938285454250989"]
@@ -9,9 +9,15 @@ use ark_ff::BigInt;
 pub struct ArkScalarConfig;
 pub type ArkScalar = Fp256<MontBackend<ArkScalarConfig, 4>>;
 
-#[allow(unused_variables)]
 pub fn to_ark_scalar(x: &Scalar) -> ArkScalar {
     let mut values: [u64; 4] = [0; 4];
     values.as_mut_byte_slice().clone_from_slice(x.as_bytes());
-    ArkScalar::new(BigInt::new(values))
+    ArkScalarConfig::from_bigint(BigInt::new(values)).unwrap()
+}
+
+pub fn from_ark_scalar(x: &ArkScalar) -> Scalar {
+    let x = ArkScalarConfig::into_bigint(x.clone());
+    let mut bytes = [0u8; 32];
+    x.write(bytes.as_mut()).unwrap();
+    Scalar::from_canonical_bytes(bytes).unwrap()
 }
