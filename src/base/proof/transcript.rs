b5a5d08135fddab139075c46963a6762ea737f24
Ryan Burn <ryan.burn@gmail.com>
2022-05-31 14:20:06 -0700

    integrate sumcheck

--- src/base/proof/transcript.rs
+++ src/base/proof/transcript.rs
@@ -2,6 +2,8 @@ use curve25519_dalek::ristretto::CompressedRistretto;
 use curve25519_dalek::scalar::Scalar;
 use merlin::Transcript;
 
+use crate::base::proof::ProofError;
+
 pub trait TranscriptProtocol {
     /// Append a `scalar` with the given `label`.
     fn append_scalar(&mut self, label: &'static [u8], scalar: &Scalar);
@@ -11,6 +13,14 @@ pub trait TranscriptProtocol {
 
     /// Compute a `label`ed challenge variable.
     fn challenge_scalars(&mut self, scalars: &mut [Scalar], label: &'static [u8]);
+
+    /// Check that a point is not the identity, then append it to the
+    /// transcript.  Otherwise, return an error.
+    fn validate_and_append_point(
+        &mut self,
+        label: &'static [u8],
+        point: &CompressedRistretto,
+    ) -> Result<(), ProofError>;
 }
 
 impl TranscriptProtocol for Transcript {
@@ -20,6 +30,20 @@ impl TranscriptProtocol for Transcript {
 
     fn append_point(&mut self, label: &'static [u8], point: &CompressedRistretto) {
         self.append_message(label, point.as_bytes());
+    }
+
+    fn validate_and_append_point(
+        &mut self,
+        label: &'static [u8],
+        point: &CompressedRistretto,
+    ) -> Result<(), ProofError> {
+        use curve25519_dalek::traits::IsIdentity;
+
+        if point.is_identity() {
+            Err(ProofError::VerificationError)
+        } else {
+            Ok(self.append_message(label, point.as_bytes()))
+        }
     }
 
     fn challenge_scalars(&mut self, scalars: &mut [Scalar], label: &'static [u8]) {
