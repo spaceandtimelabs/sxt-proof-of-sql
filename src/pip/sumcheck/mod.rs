81246d2875c637b5614ba6747f6997b322aca4c5
Ryan Burn <ryan.burn@gmail.com>
2022-06-03 16:37:58 -0700

    add verifier state

--- src/pip/sumcheck/mod.rs
+++ src/pip/sumcheck/mod.rs
@@ -9,5 +9,8 @@ pub use proof::SumcheckProof;
 mod prover_state;
 pub use prover_state::{init_prover_state, ProverState};
 
+mod verifier_state;
+pub use verifier_state::{init_verifier_state, VerifierState};
+
 mod prover_round;
 pub use prover_round::prove_round;
