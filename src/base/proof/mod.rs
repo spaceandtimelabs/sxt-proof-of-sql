639ba5e58d5099751a56e69cc4e36c993ad85755
Jay White <JayWhite2357@users.noreply.github.com>
2022-06-06 06:12:50 -0500

    comments

--- src/base/proof/mod.rs
+++ src/base/proof/mod.rs
@@ -9,19 +9,29 @@ pub use transcript::TranscriptProtocol;
 use curve25519_dalek::scalar::Scalar;
 
 pub struct Commitment {
+    //The actual commitment to a column/vector. It may make sense for this to be non compressed, and only serialized as compressed.
     pub commitment : curve25519_dalek::ristretto::CompressedRistretto,
+    //The length of the column/vector.
     pub length : usize,
 }
 
 pub trait PIPProof {
     fn create(
+        //The merlin transcript for the prover
         transcript: &mut dyn TranscriptProtocol, 
+        //The inputs to the PIP. This is several columns. We may eventually wish for this to be a arrow::record_batch::RecordBatch instead.
         inputs : Vec<&[Scalar]>, 
+        //The output of the PIP. Note: these are not computed by the PIP itself. The PIP simply produces a proof that these are correct.
         outputs : Vec<&[Scalar]>,
     ) -> Self;
     fn verify(&self, 
+        //The merlin transcript for the verifier
         transcript: &mut dyn TranscriptProtocol, 
+        //The commitments of the inputs to the PIP. Typically, these are known by the verifier.
         inputs : Vec<Commitment>, 
+        //The commitments of the outputs to the PIP. Typically, these are sent from the prover to the verifier before the PIPproof is created.
         outputs : Vec<Commitment>,
     ) -> Result<(), ProofError>;
+    //fn to_bytes(&self) -> &[u8];
+    //fn from_bytes(data : &[u8]) -> Self;
 }
