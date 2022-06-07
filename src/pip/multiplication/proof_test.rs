d61ea20edf33fdad35c01799c0642a66fa9cfd2d
Ryan Burn <ryan.burn@gmail.com>
2022-06-07 10:30:27 -0700

    refactor

--- src/pip/multiplication/proof_test.rs
+++ src/pip/multiplication/proof_test.rs
@@ -1,6 +1,6 @@
-use crate::pip::multiplication::proof::*;
-use crate::base::proof::PIPProof;
 use crate::base::proof::Commitment;
+use crate::base::proof::PIPProof;
+use crate::pip::multiplication::proof::*;
 
 use crate::base::proof::Transcript;
 use curve25519_dalek::ristretto::RistrettoPoint;
@@ -18,9 +18,17 @@ fn test_create_verify_proof() {
     // verify proof
     let mut transcript = Transcript::new(b"multiplicationtest");
     let c_a = RistrettoPoint::hash_from_bytes::<Sha3_512>(b"a").compress(); // pretend like this is the commitment of a
-    let commitment_a = Commitment{commitment : c_a, length : a.len()};
+    let commitment_a = Commitment {
+        commitment: c_a,
+        length: a.len(),
+    };
     let c_b = RistrettoPoint::hash_from_bytes::<Sha3_512>(b"b").compress(); // pretend like this is the commitment of b
-    let commitment_b = Commitment{commitment : c_b, length : b.len()};
+    let commitment_b = Commitment {
+        commitment: c_b,
+        length: b.len(),
+    };
 
-    assert!(proof.verify(&mut transcript, vec![commitment_a, commitment_b], vec![]).is_ok());
+    assert!(proof
+        .verify(&mut transcript, vec![commitment_a, commitment_b], vec![])
+        .is_ok());
 }
