37470ddc946814e7c8e7cbdf814c3745d69be57d
Jay White <JayWhite2357@users.noreply.github.com>
2022-06-30 15:29:13 -0500

    refactor: clean up equality pip (#49)
    * refactor: clean up equality pip
    
    * fix: add transcript calls
    
    * Apply suggestions from code review
    
    Co-authored-by: Trevor Lovell <trevorlovelldesign@gmail.com>
    
    * Update src/base/proof/commitment.rs
    
    Co-authored-by: Trevor Lovell <trevorlovelldesign@gmail.com>
    
    * add suggested changes
    
    Co-authored-by: Trevor Lovell <trevorlovelldesign@gmail.com>

--- src/pip/equality/proof_test1.rs
+++ src/pip/equality/proof_test1.rs
@@ -1,60 +1,59 @@
-#![allow(non_snake_case)]
-
-use std::slice;
-
-use curve25519_dalek::ristretto::CompressedRistretto;
-use curve25519_dalek::scalar::Scalar;
-use curve25519_dalek::traits::Identity;
-use pedersen::commitments::compute_commitments;
 use crate::base::proof::{Commitment, PIPProof, Transcript};
 use crate::pip::equality::EqualityProof;
+use curve25519_dalek::scalar::Scalar;
 
 #[test]
-    fn test_equality() {
-        let a = vec![
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(2 as u32)
-        ];
-        let b = vec![
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(3 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(2 as u32)
-        ];
+fn test_equality() {
+    let a = vec![
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(2_u32),
+        Scalar::from(2_u32),
+        Scalar::from(2_u32),
+    ];
+    let b = vec![
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(2_u32),
+        Scalar::from(3_u32),
+        Scalar::from(2_u32),
+        Scalar::from(2_u32),
+        Scalar::from(2_u32),
+    ];
 
-        let output = vec![
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(0 as u32),
-            Scalar::from(0 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32)
-        ];
+    let output = vec![
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(0_u32),
+        Scalar::from(0_u32),
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+    ];
 
-        let mut C_a = CompressedRistretto::identity();
-        compute_commitments(slice::from_mut(&mut C_a), &[&a[..]]);
-        let commitment_a = Commitment {
-            commitment: C_a,
-            length: a.len(),
-        };
+    let c_a = Commitment::from(&a[..]);
+    let c_b = Commitment::from(&b[..]);
 
-        let mut C_b = CompressedRistretto::identity();
-        compute_commitments(slice::from_mut(&mut C_b), &[&b[..]]);
-        let commitment_b = Commitment {
-            commitment: C_b,
-            length: b.len(),
-        };
+    let mut transcript = Transcript::new(b"equalitytest");
+    let equalityproof = EqualityProof::create(&mut transcript, &[&a, &b], &[&output], &[c_a, c_b]);
 
-        let mut transcript = Transcript::new(b"equalitytest");
-        let equalityproof = EqualityProof::create(&mut transcript, &[&a, &b], &[&output], &[]);
-        assert!(equalityproof.verify(&mut transcript, &[commitment_a, commitment_b]).is_ok());
-    }
+    //the proof confirms as correct
+    let mut transcript = Transcript::new(b"equalitytest");
+    assert!(equalityproof.verify(&mut transcript, &[c_a, c_b]).is_ok());
+
+    //the output commitment is correct as well
+    assert_eq!(
+        Commitment::from(&output[..]),
+        equalityproof.get_output_commitments()[0]
+    );
+
+    //wrong transcript
+    let mut transcript = Transcript::new(b"equalitytest oops");
+    assert!(equalityproof.verify(&mut transcript, &[c_a, c_b]).is_err());
+
+    //wrong input commitments
+    let mut transcript = Transcript::new(b"equalitytest");
+    assert!(equalityproof.verify(&mut transcript, &[c_a, c_a]).is_err());
+}
--- src/pip/equality/proof_test2.rs
+++ src/pip/equality/proof_test2.rs
@@ -1,65 +1,46 @@
-#![allow(non_snake_case)]
-
-use std::slice;
-
-use curve25519_dalek::ristretto::CompressedRistretto;
-use curve25519_dalek::scalar::Scalar;
-use curve25519_dalek::traits::Identity;
-use pedersen::commitments::compute_commitments;
 use crate::base::proof::{Commitment, PIPProof, Transcript};
 use crate::pip::equality::EqualityProof;
+use curve25519_dalek::scalar::Scalar;
 
 #[test]
-    fn test_equality() {
-        let a = vec![
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(2 as u32),
-            Scalar::from(2 as u32)
-        ];
+fn test_equality() {
+    let a = vec![
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(1_u32),
+        Scalar::from(2_u32),
+        Scalar::from(2_u32),
+        Scalar::from(2_u32),
+    ];
 
-        let b = vec![
-            Scalar::from(4 as u32),
-            Scalar::from(4 as u32),
-            Scalar::from(4 as u32),
-            Scalar::from(4 as u32),
-            Scalar::from(4 as u32),
-            Scalar::from(4 as u32),
-            Scalar::from(4 as u32)
-        ];
+    let b = vec![
+        Scalar::from(4_u32),
+        Scalar::from(4_u32),
+        Scalar::from(4_u32),
+        Scalar::from(4_u32),
+        Scalar::from(4_u32),
+        Scalar::from(4_u32),
+        Scalar::from(4_u32),
+    ];
 
-        let output = vec![
-            Scalar::from(0 as u32),
-            Scalar::from(0 as u32),
-            Scalar::from(0 as u32),
-            Scalar::from(0 as u32),
-            Scalar::from(1 as u32),
-            Scalar::from(0 as u32),
-            Scalar::from(0 as u32)
-        ];
+    let output = vec![
+        Scalar::from(0_u32),
+        Scalar::from(0_u32),
+        Scalar::from(0_u32),
+        Scalar::from(0_u32),
+        Scalar::from(1_u32),
+        Scalar::from(0_u32),
+        Scalar::from(0_u32),
+    ];
 
-        let mut C_a = CompressedRistretto::identity();
-        compute_commitments(slice::from_mut(&mut C_a), &[&a[..]]);
-        let commitment_a = Commitment {
-            commitment: C_a,
-            length: a.len(),
-        };
+    let c_a = Commitment::from(&a[..]);
+    let c_b = Commitment::from(&b[..]);
 
-        let mut C_b = CompressedRistretto::identity();
-        compute_commitments(slice::from_mut(&mut C_b), &[&b[..]]);
-        let commitment_b = Commitment {
-            commitment: C_b,
-            length: b.len(),
-        };
-        let mut transcript = Transcript::new(b"equalitytest");
+    //wrong output
+    let mut transcript = Transcript::new(b"equalitytest");
+    let equalityproof = EqualityProof::create(&mut transcript, &[&a, &b], &[&output], &[c_a, c_b]);
 
-    let equalityproof = EqualityProof::create(&mut transcript, &[&a, &b], &[&output], &[]);
-    assert!(equalityproof.verify(&mut transcript, &[commitment_a, commitment_b]).is_err());
-
-    /*    let equalityproof = EqualityProof::create(&mut transcript, &[&a, &b], &[]);
-        assert!(equalityproof.verify(&mut transcript, &[commitment_a, commitment_b], &[]).is_ok());
-     */
-    }
+    let mut transcript = Transcript::new(b"equalitytest");
+    assert!(equalityproof.verify(&mut transcript, &[c_a, c_b]).is_err());
+}
