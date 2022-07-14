1cb3b2ef5dd18cf63513d7e6087c2de1023d8581
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-07-14 05:34:18 -0600

    feat: implement AdditionProof  (#77)
    * feat: add AdditionProof
    
    * feat: add Transcript::addition_domain_sep
    
    * feat: implement PipProve for AdditionProof
    
    * feat: implement PipVerify for AdditionProof
    
    * test: add test_addition
    
    * test: add test_addition_wrong
    
    * refactor: move addition tests to their own file
    
    * chore: remove moved tests from addition::proof module
    
    * perf: calculate c_sum from the input commitments in the addition proof
    
    * test: replace verification check with output commitment check

---
+++ src/pip/addition/proof.rs
@@ -0,0 +1,59 @@
+use crate::base::{
+    proof::{Column, Commitment, PipProve, PipVerify, ProofError, Transcript},
+    scalar::IntoScalar,
+};
+use std::ops::Add;
+
+pub struct AdditionProof {
+    pub c_sum: Commitment,
+}
+
+impl<T> PipProve<(Column<T>, Column<T>), Column<T>> for AdditionProof
+where
+    T: IntoScalar + Clone + Add,
+{
+    fn prove(
+        transcript: &mut Transcript,
+        input: (Column<T>, Column<T>),
+        output: Column<T>,
+        input_commitments: (Commitment, Commitment),
+    ) -> Self {
+        let (a, b) = input;
+        let sum = output;
+        let (c_a, c_b) = input_commitments;
+
+        assert_eq!(a.len(), b.len());
+        assert_eq!(a.len(), sum.len());
+        assert_eq!(a.len(), c_a.length);
+        assert_eq!(b.len(), c_b.length);
+
+        transcript.addition_domain_sep(a.len() as u64);
+
+        let c_sum = c_a + c_b;
+        transcript.append_point(b"c_sum", &c_sum.commitment);
+        AdditionProof { c_sum }
+    }
+}
+
+impl PipVerify<(Commitment, Commitment), Commitment> for AdditionProof {
+    fn verify(
+        &self,
+        transcript: &mut Transcript,
+        input_commitments: (Commitment, Commitment),
+    ) -> Result<(), ProofError> {
+        let (c_a, c_b) = input_commitments;
+        transcript.addition_domain_sep(c_a.length as u64);
+
+        transcript.append_point(b"c_sum", &self.c_sum.commitment);
+
+        if c_a + c_b == self.c_sum {
+            Ok(())
+        } else {
+            Err(ProofError::VerificationError)
+        }
+    }
+
+    fn get_output_commitments(&self) -> Commitment {
+        self.c_sum
+    }
+}
--- src/pip/mod.rs
+++ src/pip/mod.rs
@@ -1,3 +1,4 @@
+pub mod addition;
 pub mod equality;
 pub mod hadamard;
 pub mod inequality;
