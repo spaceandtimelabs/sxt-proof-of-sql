ad1377a0a5a9575688d3edab0b5a5921130cf9f5
dibyenduc <dibyendu.chakrabarti@yahoo.com>
2022-07-14 17:12:36 -0700

    Feat/pip subtraction (#83)
    * feat: subtraction pip
    
    * fix: exploring the negative number case
    
    * feat: subtraction pip finalized
    
    * fix: clippy
    
    Co-authored-by: Dibyendu Chakrabarti <dibyendu@Dibyendus-MacBook-Pro-2.local>

--- src/pip/mod.rs
+++ src/pip/mod.rs
@@ -2,3 +2,4 @@ pub mod addition;
 pub mod equality;
 pub mod hadamard;
 pub mod inequality;
+pub mod subtraction;
---
+++ src/pip/subtraction/mod.rs
@@ -0,0 +1,5 @@
+mod proof;
+pub use proof::SubtractionProof;
+
+#[cfg(test)]
+mod proof_test;
