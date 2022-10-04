e3f81bf84496e46f66847eea3b56d29dc919765e
tlovell-sxt <110196418+tlovell-sxt@users.noreply.github.com>
2022-10-04 11:01:14 -0600

    bench: benchmark sumcheck (PROOF-143) (#139)
    * bench: add benches crate, start sumcheck_degree bench
    
    * bench: finish first version of benchmark_sumcheck_degree
    
    * bench: add different benches for prove/verify of sumcheck
    
    * bench: re-add accidentally-removed sumcheck.rs
    
    * bench: made degree benches single-term
    
    * bench: add benches for sumcheck increasing terms
    
    * bench: add benches varying row count for sumcheck
    
    * bench: add new benches to criterion group
    
    * bench: exclude 0-row from sumcheck rows bench
    
    * bench: add random_mle_with_num_vars function, update benches using it
    
    * bench: generate new mle for each multiplicand in degree benches
    
    * bench: have terms benches generate new mle for each term
    
    * bench: fix rows benches to actually vary the number of rows
    
    * bench: tweak sumcheck benchmark sample sizes
    
    * fix: move bench to crates directory
    
    * fix: use merlin transcript in benches instead of old proofs transcript
    
    * bench: remove unnecessary zero_evaluation_point clones

---
+++ crates/proofs/benches/sumcheck.rs
@@ -0,0 +1,267 @@
+use ark_std::rc::Rc;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use curve25519_dalek::scalar::Scalar;
+use merlin::Transcript;
+use proofs::{
+    base::{
+        polynomial::{CompositePolynomial, DenseMultilinearExtension},
+        proof::Column,
+    },
+    proof_primitive::sumcheck::SumcheckProof,
+};
+use rand::{thread_rng, Rng};
+use std::time::Duration;
+
+fn random_mle_with_num_vars<R: Rng>(v: usize, rng: &mut R) -> (Scalar, DenseMultilinearExtension) {
+    let column: Column<Scalar> = (0..2u32.pow(v as u32))
+        .map(|_| rng.gen::<u32>())
+        .map(Scalar::from)
+        .collect();
+
+    let sum = column.iter().sum();
+    let mle = DenseMultilinearExtension::from_evaluations_slice(v, column.as_slice());
+
+    (sum, mle)
+}
+
+pub fn bench_sumcheck_prove_degree(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let num_vars = 10;
+
+    let zero_evaluation_point = vec![Scalar::zero(); num_vars];
+
+    let mut mles = Vec::new();
+
+    let mut group = c.benchmark_group("sumcheck_prove_degree");
+    group
+        .sample_size(50)
+        .measurement_time(Duration::from_secs(10));
+    for degree in 1..=16 {
+        let (_, new_mle) = random_mle_with_num_vars(num_vars, &mut rng);
+        mles.push(Rc::new(new_mle));
+
+        let mut polynomial = CompositePolynomial::new(num_vars);
+        polynomial.add_product(mles.clone(), Scalar::one());
+
+        let mut transcript = Transcript::new(b"sumcheck_degree");
+        let mut zero_evaluation_point = zero_evaluation_point.clone();
+
+        group.throughput(Throughput::Elements(degree as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(degree),
+            &polynomial,
+            |b, polynomial| {
+                b.iter(|| {
+                    SumcheckProof::create(
+                        black_box(&mut transcript),
+                        black_box(&mut zero_evaluation_point),
+                        polynomial,
+                    )
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+pub fn bench_sumcheck_verify_degree(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let num_vars = 10;
+
+    let zero_evaluation_point = vec![Scalar::zero(); num_vars];
+
+    let mut expected = Scalar::one();
+    let mut mles = Vec::new();
+
+    let mut group = c.benchmark_group("sumcheck_verify_degree");
+    for degree in 1..=16 {
+        let (new_sum, new_mle) = random_mle_with_num_vars(num_vars, &mut rng);
+        expected *= new_sum;
+        mles.push(Rc::new(new_mle));
+
+        let mut polynomial = CompositePolynomial::new(num_vars);
+        polynomial.add_product(mles.clone(), Scalar::one());
+
+        // Create proof
+        let mut transcript = Transcript::new(b"sumcheck_degree");
+        let mut zero_evaluation_point = zero_evaluation_point.clone();
+        let proof = SumcheckProof::create(&mut transcript, &mut zero_evaluation_point, &polynomial);
+
+        // Verify proof w/ measurements
+        let mut transcript = Transcript::new(b"sumcheck_degree");
+
+        group.throughput(Throughput::Elements(degree as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(degree),
+            &polynomial,
+            |b, polynomial| {
+                b.iter(|| {
+                    proof.verify_without_evaluation(
+                        black_box(&mut transcript),
+                        polynomial.info(),
+                        black_box(&expected),
+                    )
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+pub fn bench_sumcheck_prove_terms(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let num_vars = 10;
+
+    let zero_evaluation_point = vec![Scalar::zero(); num_vars];
+    let mut polynomial = CompositePolynomial::new(num_vars);
+
+    let mut group = c.benchmark_group("sumcheck_prove_terms");
+    for terms in 1..=16 {
+        let (_, new_mle) = random_mle_with_num_vars(num_vars, &mut rng);
+        polynomial.add_product([Rc::new(new_mle)], Scalar::one());
+
+        let mut transcript = Transcript::new(b"sumcheck_terms");
+        let mut zero_evaluation_point = zero_evaluation_point.clone();
+
+        group.throughput(Throughput::Elements(terms as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(terms),
+            &polynomial,
+            |b, polynomial| {
+                b.iter(|| {
+                    SumcheckProof::create(
+                        black_box(&mut transcript),
+                        black_box(&mut zero_evaluation_point),
+                        polynomial,
+                    )
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+pub fn bench_sumcheck_verify_terms(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let num_vars = 10;
+
+    let zero_evaluation_point = vec![Scalar::zero(); num_vars];
+
+    let mut expected_sum = Scalar::zero();
+    let mut polynomial = CompositePolynomial::new(num_vars);
+
+    let mut group = c.benchmark_group("sumcheck_verify_terms");
+    for terms in 1..=16 {
+        let (new_sum, new_mle) = random_mle_with_num_vars(num_vars, &mut rng);
+        polynomial.add_product([Rc::new(new_mle)], Scalar::one());
+        expected_sum += new_sum;
+
+        // Create proof
+        let mut transcript = Transcript::new(b"sumcheck_terms");
+        let mut zero_evaluation_point = zero_evaluation_point.clone();
+        let proof = SumcheckProof::create(&mut transcript, &mut zero_evaluation_point, &polynomial);
+
+        // Verify proof w/ measurements
+        let mut transcript = Transcript::new(b"sumcheck_terms");
+
+        group.throughput(Throughput::Elements(terms as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(terms),
+            &polynomial,
+            |b, polynomial| {
+                b.iter(|| {
+                    proof.verify_without_evaluation(
+                        black_box(&mut transcript),
+                        polynomial.info(),
+                        black_box(&expected_sum),
+                    )
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+pub fn bench_sumcheck_prove_rows(c: &mut Criterion) {
+    let mut rng = thread_rng();
+
+    let mut group = c.benchmark_group("sumcheck_prove_rows");
+    group
+        .sample_size(50)
+        .measurement_time(Duration::from_secs(10));
+    for num_vars in 1..=16 {
+        let (_, mle) = random_mle_with_num_vars(num_vars, &mut rng);
+        let rows = 2u32.pow(num_vars as u32);
+
+        let mut polynomial = CompositePolynomial::new(num_vars);
+
+        polynomial.add_product([Rc::new(mle.clone())], Scalar::one());
+
+        let mut transcript = Transcript::new(b"sumcheck_rows");
+        let mut zero_evaluation_point = vec![Scalar::zero(); num_vars];
+
+        group.throughput(Throughput::Elements(rows as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(rows),
+            &polynomial,
+            |b, polynomial| {
+                b.iter(|| {
+                    SumcheckProof::create(
+                        black_box(&mut transcript),
+                        black_box(&mut zero_evaluation_point),
+                        polynomial,
+                    )
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+pub fn bench_sumcheck_verify_rows(c: &mut Criterion) {
+    let mut rng = thread_rng();
+
+    let mut group = c.benchmark_group("sumcheck_verify_rows");
+    for num_vars in 1..=16 {
+        let (column_sum, mle) = random_mle_with_num_vars(num_vars, &mut rng);
+        let rows = 2u32.pow(num_vars as u32);
+
+        let mut polynomial = CompositePolynomial::new(num_vars);
+        polynomial.add_product([Rc::new(mle.clone())], Scalar::one());
+
+        // Create proof
+        let mut transcript = Transcript::new(b"sumcheck_rows");
+        let mut zero_evaluation_point = vec![Scalar::zero(); num_vars];
+        let proof = SumcheckProof::create(&mut transcript, &mut zero_evaluation_point, &polynomial);
+
+        // Verify proof w/ measurements
+        let mut transcript = Transcript::new(b"sumcheck_rows");
+
+        group.throughput(Throughput::Elements(rows as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(rows),
+            &polynomial,
+            |b, polynomial| {
+                b.iter(|| {
+                    proof.verify_without_evaluation(
+                        black_box(&mut transcript),
+                        polynomial.info(),
+                        black_box(&column_sum),
+                    )
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    bench_sumcheck_prove_degree,
+    bench_sumcheck_verify_degree,
+    bench_sumcheck_prove_terms,
+    bench_sumcheck_verify_terms,
+    bench_sumcheck_prove_rows,
+    bench_sumcheck_verify_rows
+);
+criterion_main!(benches);
