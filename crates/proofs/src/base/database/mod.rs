778935f382aacf3ab0553b1676d2a7264363d383
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2022-10-06 14:54:19 -0300

    feat: add test accessor ( PROOF-161 ) (#145)
    * feat: add test accessor struct and implementations
    
    * test: add tests for the test_accessor
    
    * docs: add documentation for the test_accessor
    
    * feat: add test_accessor as a public component
    
    * chore: fix clippy
    
    * test: fix clippy
    
    * refactor: initialize commitments with CompressedRistretto::from_slice instead of constant
    
    * test: restrict test_accessor to only test environment
    
    * docs: fix test_accessor documentation
    
    * refactor: format code

--- crates/proofs/src/base/database/mod.rs
+++ crates/proofs/src/base/database/mod.rs
@@ -3,3 +3,8 @@ pub use accessor::{CommitmentAccessor, DataAccessor, MetadataAccessor};
 
 mod column;
 pub use column::Column;
+
+#[cfg(test)]
+mod test_accessor;
+#[cfg(test)]
+pub use test_accessor::TestAccessor;
