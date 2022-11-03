993eec23f862bd657508b3165cf763cb210e3471
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2022-11-03 15:48:16 -0300

    fix: fix scalar zigzag-encoding compatibility with protobuf table ( PROOF-180 ) (#161)
    * fix: fix zigzag encoding to be aligned with the correct protobuf zigzag table
    
    * test: fix zigzag tests and add more edge cases
    
    * chore: format code
    
    * test: add more tests to zigzag component
    
    * test: refactor tests

--- crates/proofs/src/base/encode/zigzag_test.rs
+++ crates/proofs/src/base/encode/zigzag_test.rs
@@ -15,15 +15,36 @@ fn small_scalars_are_encoded_as_positive_zigzag_values() {
     // x = 2
     // since x < y, where x + y = 0, the ZigZag value is encoded as 2 * x
     assert!(Scalar::from(2_u32).zigzag() == U256::from_words(4, 0));
+
+    // x = u128::MAX
+    // since x < y, where x + y = 0, the ZigZag value is encoded as 2 * x
+    assert!(
+        Scalar::from(u128::MAX).zigzag()
+            == U256::from_words(0xfffffffffffffffffffffffffffffffe, 0x1)
+    );
+
+    for x in 1..1000_u128 {
+        // since x < y, where x + y = 0, the ZigZag value is encoded as 2 * x
+        assert!(Scalar::from(x as u128).zigzag() == U256::from_words(2 * x, 0));
+    }
 }
 
 #[test]
 fn big_scalars_with_small_additive_inverses_are_encoded_as_negative_zigzag_values() {
     // x = p - 1 (p = 2^252 + 27742317777372353535851937790883648493 is the ristretto group order)
-    let val: Scalar = -Scalar::from(1_u32);
     // the additive inverse of x is y = 1. Since y < x, the ZigZag encodes -y, which is
-    // encoded as 2 * y + 1 = 3
-    assert!(val.zigzag() == U256::from_words(3, 0));
+    // encoded as 2 * y - 1 = 1
+    assert!((-Scalar::from(1_u32)).zigzag() == U256::from_words(1, 0));
+
+    // x = p - 2 (p = 2^252 + 27742317777372353535851937790883648493 is the ristretto group order)
+    // the additive inverse of x is y = 2. Since y < x, the ZigZag encodes -y, which is
+    // encoded as 2 * y - 1 = 3
+    assert!((-Scalar::from(2_u32)).zigzag() == U256::from_words(3, 0));
+
+    for y in 1..1000_u128 {
+        // since x > y, where x + y = 0, the ZigZag value is encoded as 2 * y - 1
+        assert!((-Scalar::from(y)).zigzag() == U256::from_words(2 * y - 1, 0));
+    }
 }
 
 #[test]
@@ -54,13 +75,23 @@ fn big_additive_inverses_that_are_smaller_than_the_input_scalars_are_encoded_as_
         0x8000000000000000000000000000000,
     ))
         .into();
+
     // the additive inverse of x is y = -x = (p - 1) / 2
-    // since we have y < x, the ZigZag encoding is 2 * y + 1 = p
+    // since we have y < x, the ZigZag encoding is 2 * y - 1 = p - 2
     assert!(
         val.zigzag()
             == U256::from_words(
-                27742317777372353535851937790883648493,
+                27742317777372353535851937790883648491,
                 21267647932558653966460912964485513216
             )
     );
+
+    // x = - U256 { low: 0, high: 0x1_u128 }
+    // since x > y, where x + y = 0, the ZigZag value is encoded as 2 * y - 1
+    let val: Scalar = (&U256 {
+        low: 0x0_u128,
+        high: 0x1_u128,
+    })
+        .into();
+    assert!((-val).zigzag() == U256::from_words(0xffffffffffffffffffffffffffffffff_u128, 0x1_u128));
 }
