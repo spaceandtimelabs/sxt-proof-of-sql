a7c4a2b1be9027d8458a68f0aaca4cf80cfe2b8f
Ryan Burn <ryan.burn@gmail.com>
2022-11-07 17:46:54 -0800

    feat: add missing proof counters (PROOF-128) (#175)
    feat: add more proof counters

--- crates/proofs/src/sql/proof/proof_counts.rs
+++ crates/proofs/src/sql/proof/proof_counts.rs
@@ -3,7 +3,9 @@ use std::fmt::Debug;
 /// Counters for different terms used within a proof
 #[derive(Default, Debug, Clone, Copy)]
 pub struct ProofCounts {
+    pub table_length: usize,
     pub sumcheck_variables: usize,
+    pub sumcheck_max_multiplicands: usize,
     pub result_columns: usize,
     pub anchored_mles: usize,
     pub intermediate_mles: usize,
