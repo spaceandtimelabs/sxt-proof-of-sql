227cbcc843fe087d5b7099d7efdae7a66df3b3d2
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2022-11-12 01:51:10 -0300

    feat: add get-ref-tables-from-ast api to proofs-sql crate ( PROOF-176 ) (#182)
    * feat: add get_ref_tables_from_ast api to proofs-sql crate
    
    * chore: small test name fix
    
    * refactor: change get_ref_tables_from_ast to return Vec<TableRef> instead of Vec<String>
    
    * chore: format code
    
    * refactor: update ast utilities to use the new sql TableRef

--- crates/proofs-sql/src/lib.rs
+++ crates/proofs-sql/src/lib.rs
@@ -6,5 +6,8 @@ pub mod symbols;
 #[cfg(test)]
 mod test;
 
+mod intermediate_ast_utility;
+pub use intermediate_ast_utility::{get_ref_tables_from_ast, TableRef};
+
 // lalrpop-generated code is not clippy-compliant
 lalrpop_mod!(#[allow(clippy::all)] pub sql);
