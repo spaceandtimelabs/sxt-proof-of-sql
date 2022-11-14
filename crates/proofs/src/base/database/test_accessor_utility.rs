e1ec97149002ceeb1b089eb9f3ac95e4c2fc4caa
Ryan Burn <ryan.burn@gmail.com>
2022-11-14 14:11:46 -0800

    feat: generate random TestAccessors (PROOF-128) (#188)
    * feat: generate random TestAccessors
    
    * docs: comment random test accessor generation

---
+++ crates/proofs/src/base/database/test_accessor_utility.rs
@@ -0,0 +1,61 @@
+use super::{MetadataAccessor, TestAccessor};
+
+use rand::{
+    distributions::{Distribution, Uniform},
+    rngs::StdRng,
+};
+use rand_core::SeedableRng;
+use std::collections::HashMap;
+
+/// Specify what form a randomly generated TestAccessor can take
+pub struct RandomTestAccessorDescriptor {
+    min_rows: usize,
+    max_rows: usize,
+    min_value: i64,
+    max_value: i64,
+}
+
+impl Default for RandomTestAccessorDescriptor {
+    fn default() -> Self {
+        Self {
+            min_rows: 0,
+            max_rows: 100,
+            min_value: -5,
+            max_value: 5,
+        }
+    }
+}
+
+/// Generate a TestAccessor with random data
+pub fn make_random_test_accessor(
+    rng: &mut StdRng,
+    table: &str,
+    cols: &[&str],
+    descriptor: &RandomTestAccessorDescriptor,
+) -> TestAccessor {
+    let n = Uniform::new(descriptor.min_rows, descriptor.max_rows + 1).sample(rng);
+    let mut data = HashMap::new();
+    let dist = Uniform::new(descriptor.min_value, descriptor.max_value + 1);
+    for col in cols {
+        let values = dist.sample_iter(&mut *rng).take(n).collect();
+        data.insert(col.to_string(), values);
+    }
+    let mut res = TestAccessor::new();
+    res.add_table(table, &data);
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn we_can_construct_a_random_test_accessor() {
+        let descriptor = RandomTestAccessorDescriptor::default();
+        let mut rng = StdRng::from_seed([0u8; 32]);
+        let cols = ["A", "B"];
+        let accessor1 = make_random_test_accessor(&mut rng, "abc", &cols, &descriptor);
+        let accessor2 = make_random_test_accessor(&mut rng, "abc", &cols, &descriptor);
+        assert_ne!(accessor1.get_length("abc"), accessor2.get_length("abc"));
+    }
+}
