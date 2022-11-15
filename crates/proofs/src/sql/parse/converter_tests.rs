2233759a23a19c1953359efd55282a915ec15f09
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2022-11-15 18:49:17 -0300

    feat: add code to convert intermediate ast into provable ast ( PROOF-176 )  (#186)
    * feat: add ast converter
    
    * refactor: update ast converter to be aligned with new intermediate ast code
    
    * refactor: drop unnecessary errors
    
    * test: add converter tests
    
    * chore: format code
    
    * refactor: clippy code
    
    * docs: add comments
    
    * refactor: drop unnecessary dependency
    
    * test: add more tests and rename test cases
    
    * refactor: rename visit_expression to visit_bool_expression
    
    * refactor: add namespace assert to converter ast code
    
    * test: remove namespace test from converter code
    
    * chore: format code
    
    * test: refactor tests and simplify them
    
    * refactor: pass schema_accessor as a reference instead of being consumed by the converter struct
    
    * refactor: clippy code
    
    * test: substitute val.into_scalar by Scalar::from

---
+++ crates/proofs/src/sql/parse/converter_tests.rs
@@ -0,0 +1,340 @@
+use crate::base::database::TestAccessor;
+use crate::sql::ast::{
+    AndExpr, EqualsExpr, FilterExpr, FilterResultExpr, NotExpr, OrExpr, TableExpr,
+};
+use crate::sql::parse::Converter;
+use curve25519_dalek::scalar::Scalar;
+use proofs_sql::sql::SelectStatementParser;
+use std::collections::HashMap;
+
+#[test]
+fn we_can_convert_an_ast_with_one_column() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where a = 3")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table("SXT_TAB", &HashMap::from([("A".to_string(), vec![3])]));
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(EqualsExpr::new("A".to_string(), Scalar::from(3_u64))),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_two_columns() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a,  b from sxt_tab where c = 123")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([
+            ("A".to_string(), vec![]),
+            ("B".to_string(), vec![]),
+            ("C".to_string(), vec![]),
+        ]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![
+            FilterResultExpr::new("A".to_string()),
+            FilterResultExpr::new("B".to_string()),
+        ],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(EqualsExpr::new("C".to_string(), Scalar::from(123_u64))),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+// Filter
+#[test]
+fn we_can_convert_an_ast_with_one_positive_cond() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where b = +4")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([("A".to_string(), vec![]), ("B".to_string(), vec![])]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::from(4_u64))),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+// Filter
+#[test]
+fn we_can_convert_an_ast_with_one_negative_cond() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where b = -4")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([("A".to_string(), vec![]), ("B".to_string(), vec![])]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), -Scalar::from(4_u64))),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_cond_and() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where (b = 3) and (c = -2)")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([
+            ("A".to_string(), vec![]),
+            ("B".to_string(), vec![]),
+            ("C".to_string(), vec![]),
+        ]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(AndExpr::new(
+            Box::new(EqualsExpr::new("B".to_string(), Scalar::from(3_u64))),
+            Box::new(EqualsExpr::new("C".to_string(), -Scalar::from(2_u64))),
+        )),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_cond_or() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where (b = 3) or (c = -2)")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([
+            ("A".to_string(), vec![]),
+            ("B".to_string(), vec![]),
+            ("C".to_string(), vec![]),
+        ]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(OrExpr::new(
+            Box::new(EqualsExpr::new("B".to_string(), Scalar::from(3_u64))),
+            Box::new(EqualsExpr::new("C".to_string(), -Scalar::from(2_u64))),
+        )),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_conds_or_not() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where (b = 3) or (not (c = -2))")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([
+            ("A".to_string(), vec![]),
+            ("B".to_string(), vec![]),
+            ("C".to_string(), vec![]),
+        ]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(OrExpr::new(
+            Box::new(EqualsExpr::new("B".to_string(), Scalar::from(3_u64))),
+            Box::new(NotExpr::new(Box::new(EqualsExpr::new(
+                "C".to_string(),
+                -Scalar::from(2_u64),
+            )))),
+        )),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_conds_not_and_or() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where not (((f = 45) or (c = -2)) and (b = 3))")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "SXT_TAB",
+        &HashMap::from([
+            ("A".to_string(), vec![]),
+            ("B".to_string(), vec![]),
+            ("C".to_string(), vec![]),
+            ("F".to_string(), vec![]),
+        ]),
+    );
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(NotExpr::new(Box::new(AndExpr::new(
+            Box::new(OrExpr::new(
+                Box::new(EqualsExpr::new("F".to_string(), Scalar::from(45_u64))),
+                Box::new(EqualsExpr::new("C".to_string(), -Scalar::from(2_u64))),
+            )),
+            Box::new(EqualsExpr::new("B".to_string(), Scalar::from(3_u64))),
+        )))),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_the_min_i64_filter_value() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where a = -9223372036854775808")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table("SXT_TAB", &HashMap::from([("A".to_string(), vec![3])]));
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(EqualsExpr::new(
+            "A".to_string(),
+            -Scalar::from(9223372036854775808u64),
+        )),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_can_convert_an_ast_with_the_max_i64_filter_value() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where a = 9223372036854775807")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table("SXT_TAB", &HashMap::from([("A".to_string(), vec![3])]));
+
+    let provable_ast = Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .unwrap();
+
+    let expected_provable_ast = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "SXT_TAB".to_string(),
+        },
+        Box::new(EqualsExpr::new(
+            "A".to_string(),
+            Scalar::from(9223372036854775807_u64),
+        )),
+    );
+
+    assert_eq!(expected_provable_ast, provable_ast);
+}
+
+#[test]
+fn we_cannot_convert_an_ast_with_a_nonexistent_column() {
+    let intermediate_ast = SelectStatementParser::new()
+        .parse("select a from sxt_tab where a = 3")
+        .unwrap();
+
+    let mut accessor = TestAccessor::new();
+    accessor.add_table("SXT_TAB", &HashMap::from([("B".to_string(), vec![3])]));
+
+    assert!(Converter::default()
+        .visit_intermediate_ast(&intermediate_ast, &accessor)
+        .is_err());
+}
+
+#[test]
+fn we_cannot_convert_an_ast_with_a_namespaced_table_yet() {
+    assert!(SelectStatementParser::new()
+        .parse("select a from eth.sxt_tab where a = -3")
+        .is_err());
+}
