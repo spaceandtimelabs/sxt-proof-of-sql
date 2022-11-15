6685ccbb4d6bc9b046b2658ae7a1ed3a22ff39e1
Ryan Burn <ryan.burn@gmail.com>
2022-11-15 10:55:25 -0800

    feat: implement equality proof (PROOF-128) (#169)
    * feat: implement provable equals node
    
    * test: expand testing
    
    * docs: fix comments
    
    * test: tweak equality test

--- crates/proofs/src/sql/ast/filter_expr.rs
+++ crates/proofs/src/sql/ast/filter_expr.rs
@@ -1,9 +1,11 @@
-use crate::sql::proof::{ProofBuilder, ProofCounts, QueryExpr, VerificationBuilder};
-
-use crate::sql::ast::{BoolExpr, FilterResultExpr, TableExpr};
+use super::{BoolExpr, FilterResultExpr, TableExpr};
 
 use crate::base::database::{CommitmentAccessor, DataAccessor, MetadataAccessor};
+use crate::base::math::log2_up;
+use crate::sql::proof::{ProofBuilder, ProofCounts, QueryExpr, VerificationBuilder};
+
 use bumpalo::Bump;
+use std::cmp;
 
 /// Provable expressions for queries of the form
 /// ```ignore
@@ -33,12 +35,20 @@ impl FilterExpr {
 }
 
 impl QueryExpr for FilterExpr {
-    #[allow(unused_variables)]
     fn count(&self, counts: &mut ProofCounts, accessor: &dyn MetadataAccessor) {
-        todo!();
+        let n = accessor.get_length(&self.table.name);
+        counts.table_length = n;
+        if n > 0 {
+            counts.sumcheck_variables = cmp::max(log2_up(n), 1);
+        } else {
+            counts.sumcheck_variables = 0;
+        }
+        self.where_clause.count(counts);
+        for expr in self.results.iter() {
+            expr.count(counts);
+        }
     }
 
-    #[allow(unused_variables)]
     fn prover_evaluate<'a>(
         &self,
         builder: &mut ProofBuilder<'a>,
@@ -46,16 +56,43 @@ impl QueryExpr for FilterExpr {
         counts: &ProofCounts,
         accessor: &'a dyn DataAccessor,
     ) {
-        todo!();
+        // evaluate where clause
+        let selection =
+            self.where_clause
+                .prover_evaluate(builder, alloc, &self.table, counts, accessor);
+
+        // set result indexes
+        let mut cnt: usize = 0;
+        for b in selection {
+            cnt += *b as usize;
+        }
+        let indexes = alloc.alloc_slice_fill_default::<u64>(cnt);
+        cnt = 0;
+        for (i, b) in selection.iter().enumerate() {
+            if *b {
+                indexes[cnt] = i as u64;
+                cnt += 1;
+            }
+        }
+        builder.set_result_indexes(indexes);
+
+        // evaluate result columns
+        for expr in self.results.iter() {
+            expr.prover_evaluate(builder, alloc, &self.table, counts, accessor, selection);
+        }
     }
 
-    #[allow(unused_variables)]
     fn verifier_evaluate(
         &self,
         builder: &mut VerificationBuilder,
         counts: &ProofCounts,
         accessor: &dyn CommitmentAccessor,
     ) {
-        todo!();
+        let selection_eval =
+            self.where_clause
+                .verifier_evaluate(builder, &self.table, counts, accessor);
+        for expr in self.results.iter() {
+            expr.verifier_evaluate(builder, &self.table, counts, accessor, &selection_eval);
+        }
     }
 }
