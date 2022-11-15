dd227416e7c4052cc0348b997b9d39a35be6122a
Ryan Burn <ryan.burn@gmail.com>
2022-11-15 10:55:25 -0800

    feat: implement equality proof (PROOF-128) (#169)
    * feat: implement provable equals node
    
    * test: expand testing
    
    * docs: fix comments
    
    * test: tweak equality test

---
+++ crates/proofs/src/sql/ast/equals_expr_test.rs
@@ -0,0 +1,168 @@
+use super::{EqualsExpr, FilterExpr, FilterResultExpr, TableExpr};
+
+use crate::base::database::TestAccessor;
+use crate::sql::proof::{exercise_verification, make_schema, VerifiableQueryResult};
+
+use arrow::array::Int64Array;
+use arrow::record_batch::RecordBatch;
+use curve25519_dalek::scalar::Scalar;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+#[test]
+fn we_can_prove_an_equality_query_with_no_rows() {
+    let expr = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "T".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::zero())),
+    );
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([("A".to_string(), vec![]), ("B".to_string(), vec![])]),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+
+    exercise_verification(&res, &expr, &accessor);
+
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let res_col: Vec<i64> = vec![];
+    let expected_res =
+        RecordBatch::try_new(make_schema(1), vec![Arc::new(Int64Array::from(res_col))]).unwrap();
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_prove_an_equality_query_with_a_single_selected_row() {
+    let expr = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "T".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::zero())),
+    );
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([("A".to_string(), vec![123]), ("B".to_string(), vec![0])]),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+
+    exercise_verification(&res, &expr, &accessor);
+
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let res_col: Vec<i64> = vec![123];
+    let expected_res =
+        RecordBatch::try_new(make_schema(1), vec![Arc::new(Int64Array::from(res_col))]).unwrap();
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_prove_an_equality_query_with_a_single_non_selected_row() {
+    let expr = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "T".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::zero())),
+    );
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([("A".to_string(), vec![123]), ("B".to_string(), vec![55])]),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+
+    exercise_verification(&res, &expr, &accessor);
+
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let res_col: Vec<i64> = vec![];
+    let expected_res =
+        RecordBatch::try_new(make_schema(1), vec![Arc::new(Int64Array::from(res_col))]).unwrap();
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_prove_an_equality_query_with_multiple_rows() {
+    let expr = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "T".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::zero())),
+    );
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([
+            ("A".to_string(), vec![1, 2, 3, 4]),
+            ("B".to_string(), vec![0, 5, 0, 5]),
+        ]),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+
+    exercise_verification(&res, &expr, &accessor);
+
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let expected_res =
+        RecordBatch::try_new(make_schema(1), vec![Arc::new(Int64Array::from(vec![1, 3]))]).unwrap();
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_prove_an_equality_query_with_a_nonzero_comparison() {
+    let expr = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "T".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::from(123u64))),
+    );
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([
+            ("A".to_string(), vec![1, 2, 3, 4, 5]),
+            ("B".to_string(), vec![123, 5, 123, 5, 0]),
+        ]),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+
+    exercise_verification(&res, &expr, &accessor);
+
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let expected_res =
+        RecordBatch::try_new(make_schema(1), vec![Arc::new(Int64Array::from(vec![1, 3]))]).unwrap();
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn verify_fails_if_data_between_prover_and_verifier_differ() {
+    let expr = FilterExpr::new(
+        vec![FilterResultExpr::new("A".to_string())],
+        TableExpr {
+            name: "T".to_string(),
+        },
+        Box::new(EqualsExpr::new("B".to_string(), Scalar::zero())),
+    );
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([
+            ("A".to_string(), vec![1, 2, 3, 4]),
+            ("B".to_string(), vec![0, 5, 0, 5]),
+        ]),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(
+        "T",
+        &HashMap::from([
+            ("A".to_string(), vec![1, 2, 3, 4]),
+            ("B".to_string(), vec![0, 2, 0, 5]),
+        ]),
+    );
+    assert!(res.verify(&expr, &accessor).is_err());
+}
--- crates/proofs/src/sql/ast/filter_result_expr.rs
+++ crates/proofs/src/sql/ast/filter_result_expr.rs
@@ -1,15 +1,19 @@
-use crate::base::database::{CommitmentAccessor, DataAccessor};
-use crate::sql::ast::TableExpr;
-use crate::sql::proof::{ProofBuilder, ProofCounts, VerificationBuilder};
+use super::TableExpr;
+
+use crate::base::database::{Column, CommitmentAccessor, DataAccessor};
+use crate::sql::proof::{
+    make_sumcheck_term, DenseProvableResultColumn, ProofBuilder, ProofCounts,
+    SumcheckSubpolynomial, VerificationBuilder,
+};
 
 use bumpalo::Bump;
 use curve25519_dalek::scalar::Scalar;
+use std::cmp::max;
 
 /// Provable expression for a result column within a filter SQL expression
 ///
 /// Note: this is currently limited to named column expressions.
 #[derive(Debug, PartialEq, Eq)]
-#[allow(dead_code)]
 pub struct FilterResultExpr {
     column: String,
 }
@@ -21,35 +25,73 @@ impl FilterResultExpr {
     }
 
     /// Count the number of proof terms needed by this expression
-    #[allow(unused_variables)]
     pub fn count(&self, counts: &mut ProofCounts) {
-        todo!();
+        counts.result_columns += 1;
+        counts.sumcheck_subpolynomials += 1;
+        counts.anchored_mles += 1;
+        counts.sumcheck_max_multiplicands = max(counts.sumcheck_max_multiplicands, 3);
     }
 
     /// Given the selected rows (as a slice of booleans), evaluate the filter result expression and
     /// add the components needed to prove the result
-    #[allow(unused_variables)]
-    pub fn prove<'a>(
+    pub fn prover_evaluate<'a>(
         &self,
         builder: &mut ProofBuilder<'a>,
         alloc: &'a Bump,
         table: &TableExpr,
+        counts: &ProofCounts,
         accessor: &'a dyn DataAccessor,
         selection: &'a [bool],
     ) {
-        todo!();
+        let Column::BigInt(col) = accessor.get_column(&table.name, &self.column);
+
+        // add result column
+        builder.produce_result_column(Box::new(DenseProvableResultColumn::new(col)));
+
+        // add MLE for result column
+        builder.produce_anchored_mle(col);
+
+        // make a column of selected result values only
+        let selected_vals =
+            alloc.alloc_slice_fill_with(
+                counts.table_length,
+                |i| if selection[i] { col[i] } else { 0 },
+            );
+
+        // add sumcheck term for col * selection
+        let terms = vec![
+            (
+                Scalar::one(),
+                vec![make_sumcheck_term(counts.sumcheck_variables, selected_vals)],
+            ),
+            (
+                -Scalar::one(),
+                vec![
+                    make_sumcheck_term(counts.sumcheck_variables, col),
+                    make_sumcheck_term(counts.sumcheck_variables, selection),
+                ],
+            ),
+        ];
+        builder.produce_sumcheck_subpolynomial(SumcheckSubpolynomial::new(terms));
     }
 
     /// Give the evaluation of the selected row's multilinear extension at sumcheck's random point,
     /// add components needed to verify this filter result expression
-    #[allow(unused_variables)]
-    pub fn verify(
+    pub fn verifier_evaluate(
         &self,
         builder: &mut VerificationBuilder,
         table: &TableExpr,
+        _counts: &ProofCounts,
         accessor: &dyn CommitmentAccessor,
-        selection_eval: Scalar,
+        selection_eval: &Scalar,
     ) {
-        todo!();
+        let col_commit = accessor.get_commitment(&table.name, &self.column);
+
+        let result_eval = builder.consume_result_mle();
+        let col_eval = builder.consume_anchored_mle(&col_commit);
+
+        let poly_eval =
+            builder.mle_evaluations.random_evaluation * (result_eval - col_eval * selection_eval);
+        builder.produce_sumcheck_subpolynomial_evaluation(&poly_eval);
     }
 }
