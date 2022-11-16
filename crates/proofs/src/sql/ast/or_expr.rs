2071adea246d7b0babd6c79f903078766931dfe5
Ryan Burn <ryan.burn@gmail.com>
2022-11-16 15:10:52 -0800

    feat: implement "or" logical node (PROOF-194) (#198)
    * feat: fill in logical or
    
    * fix: fix or counts
    
    * test: add randomized tests

--- crates/proofs/src/sql/ast/or_expr.rs
+++ crates/proofs/src/sql/ast/or_expr.rs
@@ -1,14 +1,16 @@
 use crate::base::database::{CommitmentAccessor, DataAccessor};
 use crate::sql::ast::{BoolExpr, TableExpr};
-use crate::sql::proof::{ProofBuilder, ProofCounts, VerificationBuilder};
+use crate::sql::proof::{
+    make_sumcheck_term, ProofBuilder, ProofCounts, SumcheckSubpolynomial, VerificationBuilder,
+};
 
 use bumpalo::Bump;
 use curve25519_dalek::scalar::Scalar;
 use dyn_partial_eq::DynPartialEq;
+use std::cmp::max;
 
 /// Provable logical OR expression
 #[derive(Debug, DynPartialEq, PartialEq)]
-#[allow(dead_code)]
 pub struct OrExpr {
     lhs: Box<dyn BoolExpr>,
     rhs: Box<dyn BoolExpr>,
@@ -22,12 +24,15 @@ impl OrExpr {
 }
 
 impl BoolExpr for OrExpr {
-    #[allow(unused_variables)]
     fn count(&self, counts: &mut ProofCounts) {
-        todo!();
+        self.lhs.count(counts);
+        self.rhs.count(counts);
+
+        counts.sumcheck_subpolynomials += 1;
+        counts.intermediate_mles += 1;
+        counts.sumcheck_max_multiplicands = max(counts.sumcheck_max_multiplicands, 3);
     }
 
-    #[allow(unused_variables)]
     fn prover_evaluate<'a>(
         &self,
         builder: &mut ProofBuilder<'a>,
@@ -36,10 +41,39 @@ impl BoolExpr for OrExpr {
         counts: &ProofCounts,
         accessor: &'a dyn DataAccessor,
     ) -> &'a [bool] {
-        todo!();
+        let lhs = self
+            .lhs
+            .prover_evaluate(builder, alloc, table, counts, accessor);
+        let rhs = self
+            .rhs
+            .prover_evaluate(builder, alloc, table, counts, accessor);
+        let n = lhs.len();
+        assert_eq!(n, rhs.len());
+
+        // lhs_and_rhs
+        let lhs_and_rhs = alloc.alloc_slice_fill_with(n, |i| lhs[i] && rhs[i]);
+        builder.produce_intermediate_mle(lhs_and_rhs);
+
+        // subpolynomial: lhs_and_rhs - lhs * rhs
+        let terms = vec![
+            (
+                Scalar::one(),
+                vec![make_sumcheck_term(counts.sumcheck_variables, lhs_and_rhs)],
+            ),
+            (
+                -Scalar::one(),
+                vec![
+                    make_sumcheck_term(counts.sumcheck_variables, lhs),
+                    make_sumcheck_term(counts.sumcheck_variables, rhs),
+                ],
+            ),
+        ];
+        builder.produce_sumcheck_subpolynomial(SumcheckSubpolynomial::new(terms));
+
+        // selection
+        alloc.alloc_slice_fill_with(n, |i| lhs[i] || rhs[i])
     }
 
-    #[allow(unused_variables)]
     fn verifier_evaluate(
         &self,
         builder: &mut VerificationBuilder,
@@ -47,6 +81,17 @@ impl BoolExpr for OrExpr {
         counts: &ProofCounts,
         accessor: &dyn CommitmentAccessor,
     ) -> Scalar {
-        todo!();
+        let lhs = self.lhs.verifier_evaluate(builder, table, counts, accessor);
+        let rhs = self.rhs.verifier_evaluate(builder, table, counts, accessor);
+
+        // lhs_and_rhs
+        let lhs_and_rhs = builder.consume_intermediate_mle();
+
+        // subpolynomial: lhs_and_rhs - lhs * rhs
+        let eval = builder.mle_evaluations.random_evaluation * (lhs_and_rhs - lhs * rhs);
+        builder.produce_sumcheck_subpolynomial_evaluation(&eval);
+
+        // selection
+        lhs + rhs - lhs_and_rhs
     }
 }
