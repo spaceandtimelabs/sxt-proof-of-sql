2bfb4f8157d332afa5e29c8bc8cced6f43a0af8d
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-01-16 17:04:45 -0300

    refactor!: update get table references to use identifier ( PROOF-215 ) (#237)
    * refactor: restructure ResourceId grammar rule to return tuple with two elements instead of vector of length 2
    
    * feat: add identifier parse error
    
    * feat: add identifier struct
    
    * refactor: update resource_id to use identifiers instead of strings
    
    * refactor: restruct `get_table_references` to receive default_schema as an identifier instead of a &str
    
    * feat: add identifier fields to resource_id
    
    * refactor: clippy code
    
    * chore: format code
    
    * docs: update test names and add comments
    
    * refactor: drop unnecessary resource_id methods

--- crates/proofs-sql/src/error.rs
+++ crates/proofs-sql/src/error.rs
@@ -3,7 +3,8 @@ use thiserror::Error;
 /// Errors encountered during the parsing process
 #[derive(Debug, Error, Eq, PartialEq)]
 pub enum ParseError {
-    /// Unable to parse resource id
+    #[error("Unable to parse identifier")]
+    IdentifierParseError(String),
     #[error("Unable to parse resource_id")]
     ResourceIdParseError(String),
 }
