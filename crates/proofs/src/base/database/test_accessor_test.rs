f8a592565ec62faad2bdb978b646381a9107c62e
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-02-15 09:22:17 -0300

    refactor: update test accessors to support more data types ( PROOF-293 ) (#256)
    * feat: add TestAccessorColumn implementation
    
    * feat: add TestAccessor2 implementation
    
    * test: add make_random_test_accessor_data utility function
    
    * feat: add TestAccessor2 to mod file
    
    * refactor: drop support for strings in the test_accessor_column
    
    * test: simplify test_accessor_column by dropping cow
    
    * refactor: generalize TestAccessor2 to use multi tables
    
    * fix: fix test_accessor2 lookup_column
    
    * test: refactor s/TestAccessor/TestAccessor2
    
    * bench: fix bench by s/TestAccessor/TestAccessor2
    
    * refactor: drop TestAccessor and make_random_test_accessor
    
    * refactor: s/TestAccessor2/TestAccessor
    
    * refactor: restructure and clean up test accessors
    
    * refactor: s/Indentifier::try_new/id.parse()
    
    * chore: format code
    
    * refactor: clippy code
    
    * docs: add documentation to test_accessor code
    
    * chore: format code
    
    * test: add more tests to test_accessors
    
    * chore: format code
    
    * refactor: clippy code
    
    * test: small fix to test accessor test
    
    * docs: add more docs to test_accessor
    
    * chore: format code
    
    * test: add update_offsets test to test_accessor
    
    * chore: small tweak
    
    * refactor: small rename
    
    * refactor: remove lifetimes from TestAccessor and TestAccessorColumn
    
    * Add bump allocator to TestAccessor
    * Drop all lifetimes from TestAccessor and TestAccessorColumn
    * Pass bump allocator to TestAccessorColumn::to_column function
    
    * refactor: pass dataframe as value to TestAccessor::add_table
    
    * refactor: restructure benchmarks
    
    * chore: format code

--- crates/proofs/src/base/database/test_accessor_test.rs
+++ crates/proofs/src/base/database/test_accessor_test.rs
@@ -6,9 +6,7 @@ use crate::base::scalar::compute_commitment_for_testing;
 use arrow::array::Int64Array;
 use arrow::datatypes::{DataType, Field, Schema};
 use arrow::record_batch::RecordBatch;
-use indexmap::IndexMap;
 use polars::prelude::*;
-use proofs_sql::Identifier;
 use std::sync::Arc;
 
 #[test]
@@ -17,25 +15,21 @@ fn we_can_query_the_length_of_a_table() {
     let table_ref_1 = "sxt.test".parse().unwrap();
     let table_ref_2 = "sxt.test2".parse().unwrap();
 
-    accessor.add_table(
-        table_ref_1,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3]),
-            ("b".to_string(), vec![4, 5, 6]),
-        ]),
-        0_usize,
-    );
+    let data1 = df!(
+        "a" => [1, 2, 3],
+        "b" => [4, 5, 6]
+    )
+    .unwrap();
+    accessor.add_table(table_ref_1, data1, 0_usize);
 
     assert_eq!(accessor.get_length(table_ref_1), 3);
 
-    accessor.add_table(
-        table_ref_2,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3, 4]),
-            ("b".to_string(), vec![4, 5, 6, 5]),
-        ]),
-        0_usize,
-    );
+    let data2 = df!(
+        "a" => [1, 2, 3, 4],
+        "b" => [4, 5, 6, 5],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_2, data2, 0_usize);
 
     assert_eq!(accessor.get_length(table_ref_1), 3);
     assert_eq!(accessor.get_length(table_ref_2), 4);
@@ -47,47 +41,31 @@ fn we_can_access_the_columns_of_a_table() {
     let table_ref_1 = "sxt.test".parse().unwrap();
     let table_ref_2 = "sxt.test2".parse().unwrap();
 
-    accessor.add_table(
-        table_ref_1,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3]),
-            ("b".to_string(), vec![4, 5, 6]),
-        ]),
-        0_usize,
-    );
+    let data1 = df!(
+        "a" => [1, 2, 3],
+        "b" => [4, 5, 6],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_1, data1, 0_usize);
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("b").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "b".parse().unwrap(), ColumnType::BigInt);
     match accessor.get_column(column) {
         Column::BigInt(col) => assert_eq!(col.to_vec(), vec![4, 5, 6]),
     };
 
-    accessor.add_table(
-        table_ref_2,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3, 4]),
-            ("b".to_string(), vec![4, 5, 6, 5]),
-        ]),
-        0_usize,
-    );
+    let data2 = df!(
+        "a" => [1, 2, 3, 4],
+        "b" => [4, 5, 6, 5],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_2, data2, 0_usize);
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("a").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "a".parse().unwrap(), ColumnType::BigInt);
     match accessor.get_column(column) {
         Column::BigInt(col) => assert_eq!(col.to_vec(), vec![1, 2, 3]),
     };
 
-    let column = ColumnRef::new(
-        table_ref_2,
-        Identifier::try_new("b").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_2, "b".parse().unwrap(), ColumnType::BigInt);
     match accessor.get_column(column) {
         Column::BigInt(col) => assert_eq!(col.to_vec(), vec![4, 5, 6, 5]),
     };
@@ -99,49 +77,33 @@ fn we_can_access_the_commitments_of_table_columns() {
     let table_ref_1 = "sxt.test".parse().unwrap();
     let table_ref_2 = "sxt.test2".parse().unwrap();
 
-    accessor.add_table(
-        table_ref_1,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3]),
-            ("b".to_string(), vec![4, 5, 6]),
-        ]),
-        0_usize,
-    );
+    let data1 = df!(
+        "a" => [1, 2, 3],
+        "b" => [4, 5, 6],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_1, data1, 0_usize);
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("b").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "b".parse().unwrap(), ColumnType::BigInt);
     assert_eq!(
         accessor.get_commitment(column),
         compute_commitment_for_testing(&[4, 5, 6], 0_usize)
     );
 
-    accessor.add_table(
-        table_ref_2,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3, 4]),
-            ("b".to_string(), vec![4, 5, 6, 5]),
-        ]),
-        0_usize,
-    );
+    let data2 = df!(
+        "a" => [1, 2, 3, 4],
+        "b" => [4, 5, 6, 5],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_2, data2, 0_usize);
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("a").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "a".parse().unwrap(), ColumnType::BigInt);
     assert_eq!(
         accessor.get_commitment(column),
         compute_commitment_for_testing(&[1, 2, 3], 0_usize)
     );
 
-    let column = ColumnRef::new(
-        table_ref_2,
-        Identifier::try_new("b").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_2, "b".parse().unwrap(), ColumnType::BigInt);
     assert_eq!(
         accessor.get_commitment(column),
         compute_commitment_for_testing(&[4, 5, 6, 5], 0_usize)
@@ -154,57 +116,33 @@ fn we_can_access_the_type_of_table_columns() {
     let table_ref_1 = "sxt.test".parse().unwrap();
     let table_ref_2 = "sxt.test2".parse().unwrap();
 
-    accessor.add_table(
-        table_ref_1,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3]),
-            ("b".to_string(), vec![4, 5, 6]),
-        ]),
-        0_usize,
-    );
+    let data1 = df!(
+        "a" => [1, 2, 3],
+        "b" => [4, 5, 6],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_1, data1, 0_usize);
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("b").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "b".parse().unwrap(), ColumnType::BigInt);
     assert_eq!(accessor.lookup_column(column), Some(ColumnType::BigInt));
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("c").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "c".parse().unwrap(), ColumnType::BigInt);
     assert!(accessor.lookup_column(column).is_none());
 
-    accessor.add_table(
-        table_ref_2,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3, 4]),
-            ("b".to_string(), vec![4, 5, 6, 5]),
-        ]),
-        0_usize,
-    );
+    let data2 = df!(
+        "a" => [1, 2, 3, 4],
+        "b" => [4, 5, 6, 5],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_2, data2, 0_usize);
 
-    let column = ColumnRef::new(
-        table_ref_1,
-        Identifier::try_new("a").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_1, "a".parse().unwrap(), ColumnType::BigInt);
     assert_eq!(accessor.lookup_column(column), Some(ColumnType::BigInt));
 
-    let column = ColumnRef::new(
-        table_ref_2,
-        Identifier::try_new("b").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_2, "b".parse().unwrap(), ColumnType::BigInt);
     assert_eq!(accessor.lookup_column(column), Some(ColumnType::BigInt));
 
-    let column = ColumnRef::new(
-        table_ref_2,
-        Identifier::try_new("c").unwrap(),
-        ColumnType::BigInt,
-    );
+    let column = ColumnRef::new(table_ref_2, "c".parse().unwrap(), ColumnType::BigInt);
     assert!(accessor.lookup_column(column).is_none());
 }
 
@@ -213,14 +151,12 @@ fn we_can_run_arbitrary_queries_on_a_table() {
     let mut accessor = TestAccessor::new();
     let table_ref_1 = "sxt.test".parse().unwrap();
 
-    accessor.add_table(
-        table_ref_1,
-        &IndexMap::from([
-            ("a".to_string(), vec![1, 2, 3]),
-            ("b".to_string(), vec![123, 5, 123]),
-        ]),
-        0_usize,
-    );
+    let data = df!(
+        "a" => [1, 2, 3],
+        "b" => [123, 5, 123],
+    )
+    .unwrap();
+    accessor.add_table(table_ref_1, data, 0_usize);
     let res = accessor.query_table(table_ref_1, |df| {
         df.clone()
             .lazy()
@@ -233,4 +169,51 @@ fn we_can_run_arbitrary_queries_on_a_table() {
     let expected_res =
         RecordBatch::try_new(schema, vec![Arc::new(Int64Array::from(vec![1, 3]))]).unwrap();
     assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_correctly_update_offsets() {
+    let mut accessor1 = TestAccessor::new();
+    let table_ref = "sxt.test".parse().unwrap();
+
+    let data = df!(
+        "a" => [1, 2, 3],
+        "b" => [123, 5, 123],
+    )
+    .unwrap();
+    accessor1.add_table(table_ref, data.clone(), 0_usize);
+
+    let offset = 123;
+    let mut accessor2 = TestAccessor::new();
+    accessor2.add_table(table_ref, data, offset);
+
+    let column = ColumnRef::new(table_ref, "a".parse().unwrap(), ColumnType::BigInt);
+    assert_ne!(
+        accessor1.get_commitment(column),
+        accessor2.get_commitment(column)
+    );
+    let column = ColumnRef::new(table_ref, "b".parse().unwrap(), ColumnType::BigInt);
+    assert_ne!(
+        accessor1.get_commitment(column),
+        accessor2.get_commitment(column)
+    );
+
+    assert_eq!(accessor1.get_offset(table_ref), 0);
+    assert_eq!(accessor2.get_offset(table_ref), offset);
+
+    accessor1.update_offset(table_ref, offset);
+
+    let column = ColumnRef::new(table_ref, "a".parse().unwrap(), ColumnType::BigInt);
+    assert_eq!(
+        accessor1.get_commitment(column),
+        accessor2.get_commitment(column)
+    );
+    let column = ColumnRef::new(table_ref, "b".parse().unwrap(), ColumnType::BigInt);
+    assert_eq!(
+        accessor1.get_commitment(column),
+        accessor2.get_commitment(column)
+    );
+
+    assert_eq!(accessor1.get_offset(table_ref), offset);
+    assert_eq!(accessor2.get_offset(table_ref), offset);
 }
