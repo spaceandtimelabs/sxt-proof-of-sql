cee2489a162fe5b4ee8c684080ab629a8f6d39d6
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-02-16 17:56:26 -0300

    refactor: simplify ast and converter tests ( PROOF-297 ) (#260)
    * test: add test_utility module
    
    * test: simplify and_expr_tests by using test_utility module
    
    * test: simplify const_expr_tests by using test_utility module
    
    * test: simplify equals_expr_tests by using test_utility module
    
    * test: simplify not_expr_tests by using test_utility module
    
    * test: simplify not_expr_tests by using test_utility module
    
    * test: simplify ast converter_tests by using test_utility module
    
    * test: add test_utility module to mod.rs file
    
    * refactor: move test_expr to its own module
    
    * test: simplify ast tests
    
    * test: simplify not_expr_test
    
    * chore: format code
    
    * chore: simplify test_utility

---
+++ crates/proofs/src/sql/ast/test_expr.rs
@@ -0,0 +1,65 @@
+use super::{BoolExpr, FilterExpr};
+use crate::base::database::{TableRef, TestAccessor};
+use crate::sql::ast::test_utility::{cols_result, tab};
+use crate::sql::proof::{exercise_verification, VerifiableQueryResult};
+
+use arrow::record_batch::RecordBatch;
+use polars::prelude::Expr;
+use polars::prelude::*;
+
+pub struct TestExpr {
+    pub table_ref: TableRef,
+    pub results: Vec<Expr>,
+    pub ast: FilterExpr,
+    pub accessor: TestAccessor,
+    pub df_filter: Expr,
+}
+
+impl TestExpr {
+    pub fn new(
+        table_ref: TableRef,
+        results: &[&str],
+        filter_expr: Box<dyn BoolExpr>,
+        df_filter: Expr,
+        accessor: TestAccessor,
+    ) -> Self {
+        let polar_results = results
+            .iter()
+            .map(|v| polars::prelude::col(v))
+            .collect::<Vec<_>>();
+        let ast = FilterExpr::new(
+            cols_result(table_ref, results, &accessor),
+            tab(table_ref),
+            filter_expr,
+        );
+
+        Self {
+            table_ref,
+            df_filter,
+            results: polar_results,
+            ast,
+            accessor,
+        }
+    }
+
+    pub fn create_verifiable_result(&self) -> VerifiableQueryResult {
+        VerifiableQueryResult::new(&self.ast, &self.accessor)
+    }
+
+    pub fn verify_expr(&self) -> RecordBatch {
+        let res = VerifiableQueryResult::new(&self.ast, &self.accessor);
+        exercise_verification(&res, &self.ast, &self.accessor, self.table_ref);
+        res.verify(&self.ast, &self.accessor).unwrap().unwrap()
+    }
+
+    pub fn query_table(&self) -> RecordBatch {
+        self.accessor.query_table(self.table_ref, |df| {
+            df.clone()
+                .lazy()
+                .filter(self.df_filter.clone())
+                .select(&self.results[..])
+                .collect()
+                .unwrap()
+        })
+    }
+}
