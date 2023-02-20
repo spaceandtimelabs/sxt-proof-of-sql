d982d2019653237ce56e6078b09c0c81fd453402
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-02-20 16:28:44 -0300

    refactor!: simplify parser code and make it more generic ( PROOF-307 ) (#262)
    * refactor: drop not_equal parser node
    
    * feat: add Literal node to parser
    
    * test: fix intermediate_ast_tests after add literal node
    
    * feat: add MismatchTypeError to proof code
    
    * fix: fix converter code after add Literal node to parser
    
    * refactor: change ResultColumn::output_name field from Option<Identifier> to Identifier
    
    * feat: add QueryParseError to parser error
    
    * feat: add FromStr implementation to select_statement
    
    * feat: implement from integer to ast Literal
    
    * test: add test_utility module
    
    * test: refactor intermediate_ast_tests with test_utility module
    
    * chore: format code
    
    * refactor: s/IntegerLiteral/BigInt
    
    * refactor: clippy code
    
    * chore: drop unnecessary grammar rule
    
    * refactor: s/literal/scalar
    
    * refactor: s/ParseError/ConversionError and s/ParseResult/ConversionResult
    
    * docs: fix ConversionError docs

--- crates/proofs/src/sql/parse/error.rs
+++ crates/proofs/src/sql/parse/error.rs
@@ -1,17 +1,17 @@
 use thiserror::Error;
 
-/// Errors from parsing SQL.
-///
-/// We parse a query into an intermediate AST and then convert it to a provable AST.
-/// Errors can happen in both processes and they are stored here.
+/// Errors from converting an intermediate AST into a provable AST.
 #[derive(Error, Debug)]
-pub enum ParseError {
+pub enum ConversionError {
     /// This error occurs when a part of the query is of a wrong type (e.g. applying + to booleans)
     #[error("Type error")]
     TypeError(String),
     /// This error occurs when a column doesn't exist
     #[error("Column missing")]
     MissingColumnError(String),
+    /// This error occurs when the lhs column has a type different from the rhs literal in the equal expression
+    #[error("Column type mismatch from query expression")]
+    MismatchTypeError(String),
 }
 
-pub type ParseResult<T> = std::result::Result<T, ParseError>;
+pub type ConversionResult<T> = std::result::Result<T, ConversionError>;
