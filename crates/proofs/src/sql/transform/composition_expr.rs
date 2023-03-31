1e4ba48fa117770d8854d1ef7dc6b1c8bac1e65c
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-03-31 14:20:41 -0300

    feat: add order by expr and composition expr to code ( PROOF-353 ) (#282)
    * feat: add TransformResultExpr trait to transform directory
    
    * feat: add composition_expr struct to transform directory
    
    * feat: add order_by_exprs to transform directory
    
    * feat: add implementation of result_expr node
    
    * test: add tests to order_by_exprs
    
    * test: add orders and composite_result methods to test_utility
    
    * feat: add new modules to transform mod file
    
    * feat: add result_expr_builder to decouple transform module from the converter module
    
    * feat: add conversion from order_by_exprs to composition_expr
    
    * chore: add more comments to code
    
    * test: refactor tests after changing composition_expr new interface
    
    * refactor: simplify composition_expr interface
    
    * feat: add result_expr builder to transform module
    
    * docs: add comments to composition_expr
    
    * refactor: drop unnecessary chain implementation from result_expr
    
    * chore: format code
    
    * refactor: derive default for ResultExprBuilder
    
    * chore: fix comment
    
    * refactor: update result_expr to accept TransformResultExpr instead of CompositionExpr
    
    * refactor: implement TransformResultExpr for composition_expr
    
    * refactor: build empty result_expr from result_expr_builder
    
    * test: refactor test_utility result_expr method
    
    * refactor: drop from order_by_expr int composition_expr implementation
    
    * refactor: move result_expr_builder to parse module
    
    * refactor: add `add` method to result_expr
    
    * chore: format code
    
    * refactor: drop unnecessary function
    
    * refactor: s/TransformResultExpr/DataFrameExpr
    
    * refactor: s/transform_result_expr/data_frame_expr
    
    * feat: add send and sync to data_frame_expr trait
    
    * refactor: drop result_expr_builder for now

---
+++ crates/proofs/src/sql/transform/composition_expr.rs
@@ -0,0 +1,42 @@
+use crate::sql::transform::DataFrameExpr;
+use polars::prelude::LazyFrame;
+
+use dyn_partial_eq::DynPartialEq;
+
+/// A node representing a list of transformations to be applied to a `LazyFrame`.
+#[derive(Debug, Default, DynPartialEq, PartialEq)]
+pub struct CompositionExpr {
+    transformations: Vec<Box<dyn DataFrameExpr>>,
+}
+
+impl CompositionExpr {
+    /// Create a new `CompositionExpr` node.
+    pub fn new(transformation: Box<dyn DataFrameExpr>) -> Self {
+        Self {
+            transformations: vec![transformation],
+        }
+    }
+
+    /// Verify if the `CompositionExpr` node is empty.
+    pub fn is_empty(&self) -> bool {
+        self.transformations.is_empty()
+    }
+
+    /// Append a new transformation to the end of the current `CompositionExpr` node.
+    pub fn add(&mut self, transformation: Box<dyn DataFrameExpr>) {
+        self.transformations.push(transformation);
+    }
+}
+
+impl DataFrameExpr for CompositionExpr {
+    /// Apply the transformations to the `LazyFrame`.
+    fn apply_transformation(&self, lazy_frame: LazyFrame) -> LazyFrame {
+        let mut lazy_frame = lazy_frame;
+
+        for transformation in self.transformations.iter() {
+            lazy_frame = transformation.apply_transformation(lazy_frame);
+        }
+
+        lazy_frame
+    }
+}
---
+++ crates/proofs/src/sql/transform/order_by_exprs_test.rs
@@ -0,0 +1,52 @@
+use crate::record_batch;
+use crate::sql::proof::TransformExpr;
+use crate::sql::transform::test_utility::{composite_result, orders};
+use proofs_sql::intermediate_ast::OrderByDirection::{Asc, Desc};
+
+#[test]
+fn we_can_transform_a_result_using_a_single_order_by_in_ascending_direction() {
+    let data = record_batch!("c" => [1, -5, 2], "a" => ["a", "d", "b"]);
+    let result_expr = composite_result(orders(&["a"], &[Asc]));
+    let data = result_expr.transform_results(data);
+    let expected_data = record_batch!("c" => [1, 2, -5], "a" => ["a", "b", "d"]);
+    assert_eq!(data, expected_data);
+}
+
+#[test]
+fn we_can_transform_a_result_using_a_single_order_by_in_descending_direction() {
+    let data = record_batch!("c" => [1, -5, 2], "a" => ["a", "d", "b"]);
+    let result_expr = composite_result(orders(&["c"], &[Desc]));
+    let data = result_expr.transform_results(data);
+    let expected_data = record_batch!("c" => [2, 1, -5], "a" => ["b", "a", "d"]);
+    assert_eq!(data, expected_data);
+}
+
+#[test]
+fn we_can_transform_a_result_ordering_by_the_first_column_then_the_second_column() {
+    let data = record_batch!(
+        "a" => [123, 342, -234, 777, 123, 34],
+        "d" => ["alfa", "beta", "abc", "f", "kl", "f"]
+    );
+    let result_expr = composite_result(orders(&["a", "d"], &[Desc, Desc]));
+    let data = result_expr.transform_results(data);
+    let expected_data = record_batch!(
+        "a" => [777, 342, 123, 123, 34, -234],
+        "d" => ["f", "beta", "kl", "alfa", "f", "abc"]
+    );
+    assert_eq!(data, expected_data);
+}
+
+#[test]
+fn we_can_transform_a_result_ordering_by_the_second_column_then_the_first_column() {
+    let data = record_batch!(
+        "a" => [123, 342, -234, 777, 123, 34],
+        "d" => ["alfa", "beta", "abc", "f", "kl", "f"]
+    );
+    let result_expr = composite_result(orders(&["d", "a"], &[Desc, Asc]));
+    let data = result_expr.transform_results(data);
+    let expected_data = record_batch!(
+        "a" => [123, 34, 777, 342, 123, -234],
+        "d" => ["kl", "f", "f", "beta", "alfa", "abc", ]
+    );
+    assert_eq!(data, expected_data);
+}
