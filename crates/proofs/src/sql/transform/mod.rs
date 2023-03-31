b201032de50d3d4cf27fba47d5ed5154b3d856c4
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-03-31 14:20:41 -0300

    feat: add order by expr and composition expr to code ( PROOF-353 ) (#282)
    * feat: add TransformResultExpr trait to transform directory
    
    * feat: add composition_expr struct to transform directory
    
    * feat: add order_by_exprs to transform directory
    
    * feat: add implementation of result_expr node
    
    * test: add tests to order_by_exprs
    
    * test: add orders and composite_result methods to test_utility
    
    * feat: add new modules to transform mod file
    
    * feat: add result_expr_builder to decouple transform module from the converter module
    
    * feat: add conversion from order_by_exprs to composition_expr
    
    * chore: add more comments to code
    
    * test: refactor tests after changing composition_expr new interface
    
    * refactor: simplify composition_expr interface
    
    * feat: add result_expr builder to transform module
    
    * docs: add comments to composition_expr
    
    * refactor: drop unnecessary chain implementation from result_expr
    
    * chore: format code
    
    * refactor: derive default for ResultExprBuilder
    
    * chore: fix comment
    
    * refactor: update result_expr to accept TransformResultExpr instead of CompositionExpr
    
    * refactor: implement TransformResultExpr for composition_expr
    
    * refactor: build empty result_expr from result_expr_builder
    
    * test: refactor test_utility result_expr method
    
    * refactor: drop from order_by_expr int composition_expr implementation
    
    * refactor: move result_expr_builder to parse module
    
    * refactor: add `add` method to result_expr
    
    * chore: format code
    
    * refactor: drop unnecessary function
    
    * refactor: s/TransformResultExpr/DataFrameExpr
    
    * refactor: s/transform_result_expr/data_frame_expr
    
    * feat: add send and sync to data_frame_expr trait
    
    * refactor: drop result_expr_builder for now

--- crates/proofs/src/sql/transform/mod.rs
+++ crates/proofs/src/sql/transform/mod.rs
@@ -3,3 +3,15 @@ pub use result_expr::ResultExpr;
 
 #[cfg(test)]
 pub mod test_utility;
+
+mod composition_expr;
+pub use composition_expr::CompositionExpr;
+
+mod data_frame_expr;
+pub use data_frame_expr::DataFrameExpr;
+
+mod order_by_exprs;
+pub use order_by_exprs::OrderByExprs;
+
+#[cfg(test)]
+mod order_by_exprs_test;
---
+++ crates/proofs/src/sql/transform/order_by_exprs.rs
@@ -0,0 +1,36 @@
+use super::DataFrameExpr;
+use proofs_sql::intermediate_ast::{OrderBy, OrderByDirection};
+
+use dyn_partial_eq::DynPartialEq;
+use polars::prelude::col;
+use polars::prelude::LazyFrame;
+
+/// A node representing a list of `OrderBy` expressions.
+#[derive(Debug, DynPartialEq, PartialEq)]
+pub struct OrderByExprs {
+    by_exprs: Vec<OrderBy>,
+}
+
+impl OrderByExprs {
+    /// Create a new `OrderByExprs` node.
+    pub fn new(by_exprs: Vec<OrderBy>) -> Self {
+        Self { by_exprs }
+    }
+}
+
+impl DataFrameExpr for OrderByExprs {
+    /// Sort the `LazyFrame` by the `OrderBy` expressions.
+    fn apply_transformation(&self, lazy_frame: LazyFrame) -> LazyFrame {
+        assert!(!self.by_exprs.is_empty());
+
+        let nulls_last = false;
+        let reverse: Vec<_> = self
+            .by_exprs
+            .iter()
+            .map(|v| v.direction == OrderByDirection::Desc)
+            .collect();
+        let by_column: Vec<_> = self.by_exprs.iter().map(|v| col(v.expr.name())).collect();
+
+        lazy_frame.sort_by_exprs(by_column, reverse, nulls_last)
+    }
+}
