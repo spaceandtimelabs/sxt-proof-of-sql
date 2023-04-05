6a87e16cd055defd47d36d5ac55823b352eba4aa
Jay White <JayWhite2357@users.noreply.github.com>
2023-04-05 15:44:43 -0500

    perf: add CPU parallelization to prover_evaluate ( PROOF-403 ) (#289)
    * switch make_sumcheck_term use rayon
    
    * switch scalar conversion to rayon
    
    * break batch invert into chunks for parallelization
    
    * add test for batch_pseudo_inverse
    
    * add another test based on chunking size

--- crates/proofs/src/base/scalar/batch_pseudo_inverse.rs
+++ crates/proofs/src/base/scalar/batch_pseudo_inverse.rs
@@ -1,4 +1,9 @@
 use curve25519_dalek::scalar::Scalar;
+use rayon::{iter::ParallelIterator, slice::ParallelSliceMut};
+
+// These constants can likely be tuned. There is no reason for the current values other that that they are sane.
+const NUMBER_OF_CHUNKS: usize = 16; // The number of chunks to split the inversion into for parallelization. This is the number of actual inverses required.
+const MIN_CHUNKING_SIZE: usize = 64; // The minimum size for which we should actually split into chunks.
 
 /// Given a slice of `input` scalars, compute their pseudo inverses in a batch and store the result in `res`.
 ///
@@ -22,7 +27,17 @@ pub fn batch_pseudo_invert(res: &mut [Scalar], input: &[Scalar]) {
 
     // we invert only the non-zero elements from input
     // note: this function can possibly allocate memory
-    Scalar::batch_invert(&mut res[0..count_non_zeros]);
+
+    // we should break this up into chunks in order to parallelize it if there are enough non-zero elements
+    if count_non_zeros >= MIN_CHUNKING_SIZE {
+        res[0..count_non_zeros]
+            .par_chunks_mut(1 + (count_non_zeros - 1) / NUMBER_OF_CHUNKS)
+            .for_each(|c| {
+                Scalar::batch_invert(c);
+            });
+    } else {
+        Scalar::batch_invert(&mut res[0..count_non_zeros]);
+    }
 
     // we can stop here in case all the elements from `res` are already non-zero
     if count_non_zeros == input.len() {
@@ -83,6 +98,64 @@ mod tests {
             Scalar::from(0_u32),
             Scalar::from(47_u32),
         ];
+        let mut res = vec![Scalar::from(0_u32); input.len()];
+        batch_pseudo_invert(&mut res[..], &input[..]);
+
+        for (input_val, res_val) in input.iter().zip(res) {
+            if *input_val != Scalar::zero() {
+                assert!(input_val.invert() == res_val);
+            } else {
+                assert!(Scalar::zero() == res_val);
+            }
+        }
+    }
+
+    #[test]
+    fn we_can_pseudo_invert_arrays_with_nonzero_count_bigger_than_min_chunking_size_with_zeros_and_non_zeros(
+    ) {
+        let input: Vec<_> = vec![
+            Scalar::from(0_u32),
+            Scalar::from(2_u32),
+            (-33_i32).to_scalar(),
+            Scalar::from(0_u32),
+            Scalar::from(45_u32),
+            Scalar::from(0_u32),
+            Scalar::from(47_u32),
+        ]
+        .into_iter()
+        .cycle()
+        .take(MIN_CHUNKING_SIZE * 10)
+        .collect();
+
+        let mut res = vec![Scalar::from(0_u32); input.len()];
+        batch_pseudo_invert(&mut res[..], &input[..]);
+
+        for (input_val, res_val) in input.iter().zip(res) {
+            if *input_val != Scalar::zero() {
+                assert!(input_val.invert() == res_val);
+            } else {
+                assert!(Scalar::zero() == res_val);
+            }
+        }
+    }
+
+    #[test]
+    fn we_can_pseudo_invert_arrays_with_nonzero_count_smaller_than_min_chunking_size_with_zeros_and_non_zeros(
+    ) {
+        let input: Vec<_> = vec![
+            Scalar::from(0_u32),
+            Scalar::from(2_u32),
+            (-33_i32).to_scalar(),
+            Scalar::from(0_u32),
+            Scalar::from(45_u32),
+            Scalar::from(0_u32),
+            Scalar::from(47_u32),
+        ]
+        .into_iter()
+        .cycle()
+        .take(MIN_CHUNKING_SIZE - 1)
+        .collect();
+
         let mut res = vec![Scalar::from(0_u32); input.len()];
         batch_pseudo_invert(&mut res[..], &input[..]);
 
--- crates/proofs/src/sql/proof/sumcheck_utility.rs
+++ crates/proofs/src/sql/proof/sumcheck_utility.rs
@@ -1,6 +1,7 @@
-use crate::base::polynomial::DenseMultilinearExtension;
+use crate::base::polynomial::{to_ark_scalar, ArkScalar, DenseMultilinearExtension};
 use crate::base::scalar::ToScalar;
-use curve25519_dalek::scalar::Scalar;
+use num_traits::Zero;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::rc::Rc;
 
 /// Form a multilinear extension that can be added to a sumcheck polynomial.
@@ -8,20 +9,18 @@ use std::rc::Rc;
 /// Note: Currently our sumcheck algorithm doesn't support working on MLEs in-place
 /// so we use this function to copy and convert the MLE into a form that works with
 /// sumcheck.
-pub fn make_sumcheck_term<T: ToScalar>(
+pub fn make_sumcheck_term<T: ToScalar + Sync>(
     num_vars: usize,
     values: &[T],
 ) -> Rc<DenseMultilinearExtension> {
     let n = 1 << num_vars;
     assert!(n >= values.len());
-    let mut scalars = Vec::with_capacity(n);
-    for val in values.iter() {
-        scalars.push(val.to_scalar());
-    }
-    for _ in values.len()..n {
-        scalars.push(Scalar::zero());
-    }
-    Rc::new(DenseMultilinearExtension::from_evaluations_slice(
-        num_vars, &scalars,
-    ))
+    let scalars = values
+        .par_iter()
+        .map(|val| to_ark_scalar(&val.to_scalar()))
+        .chain(rayon::iter::repeatn(ArkScalar::zero(), n - values.len()))
+        .collect();
+    Rc::new(DenseMultilinearExtension {
+        ark_impl: ark_poly::DenseMultilinearExtension::from_evaluations_vec(num_vars, scalars),
+    })
 }
