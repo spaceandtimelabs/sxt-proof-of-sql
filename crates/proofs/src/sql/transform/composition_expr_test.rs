736d13a1f940a3323ad3ea74a0823ba78fc76bad
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-04-06 14:06:58 -0300

    feat: add slice expr to transform package ( PROOF-398 ) (#290)
    * feat: add slice_expr
    
    * test: add slice_expr_test
    
    * test: add slice_expr to mod file
    
    * feat: add big int support to polars idx
    
    * feat: update offset type to i64
    
    * test: add `slice` test_utility function
    
    * test: add slice_expr_test
    
    * refactor: update variables names in the slice_expr
    
    * docs: add docs for slice expr
    
    * chore: format code
    
    * test: add composition_expr_test
    
    * chore: format code
    
    * docs: update slice_expr comment
    
    * test: rename test
    
    * test: rename test
    
    * test: more tests renaming

---
+++ crates/proofs/src/sql/transform/composition_expr_test.rs
@@ -0,0 +1,44 @@
+use crate::record_batch;
+use crate::sql::proof::TransformExpr;
+use crate::sql::transform::test_utility::{composite_result, orders, slice};
+use crate::sql::transform::CompositionExpr;
+use proofs_sql::intermediate_ast::OrderByDirection::Desc;
+
+#[test]
+fn we_can_chain_expressions() {
+    let limit = 2;
+    let offset = 1;
+    let data = record_batch!("c" => [-5, 1, -56, 2], "a" => ["d", "a", "f", "b"]);
+    let mut composition = CompositionExpr::new(orders(&["c"], &[Desc]));
+    composition.add(slice(limit, offset));
+
+    let result_expr = composite_result(Box::new(composition));
+    let data = result_expr.transform_results(data);
+    let expected_data = record_batch!("c" => [1, -5], "a" => ["a", "d"]);
+    assert_eq!(data, expected_data);
+}
+
+#[test]
+fn the_order_that_we_chain_expressions_is_relevant() {
+    let limit = 2;
+    let offset = 1;
+    let data = record_batch!("c" => [-5, 1, -56, 2], "a" => ["d", "a", "f", "b"]);
+
+    let mut composition1 = CompositionExpr::new(orders(&["c"], &[Desc]));
+    composition1.add(slice(limit, offset));
+    let result_expr1 = composite_result(Box::new(composition1));
+    let data1 = result_expr1.transform_results(data.clone());
+
+    let mut composition2 = CompositionExpr::new(slice(limit, offset));
+    composition2.add(orders(&["c"], &[Desc]));
+    let result_expr2 = composite_result(Box::new(composition2));
+    let data2 = result_expr2.transform_results(data);
+
+    assert_ne!(data1, data2);
+
+    let expected_data1 = record_batch!("c" => [1, -5], "a" => ["a", "d"]);
+    assert_eq!(data1, expected_data1);
+
+    let expected_data2 = record_batch!("c" => [1, -56], "a" => ["a", "f"]);
+    assert_eq!(data2, expected_data2);
+}
--- crates/proofs/src/sql/transform/mod.rs
+++ crates/proofs/src/sql/transform/mod.rs
@@ -7,6 +7,9 @@ pub mod test_utility;
 mod composition_expr;
 pub use composition_expr::CompositionExpr;
 
+#[cfg(test)]
+pub mod composition_expr_test;
+
 mod data_frame_expr;
 pub use data_frame_expr::DataFrameExpr;
 
@@ -15,3 +18,9 @@ pub use order_by_exprs::OrderByExprs;
 
 #[cfg(test)]
 mod order_by_exprs_test;
+
+mod slice_expr;
+pub use slice_expr::SliceExpr;
+
+#[cfg(test)]
+mod slice_expr_test;
