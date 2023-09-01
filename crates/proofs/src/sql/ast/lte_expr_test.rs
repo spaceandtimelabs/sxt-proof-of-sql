0bd87cb64a33cbc764b47acd14c89fabbce0025d
Ryan Burn <ryan.burn@gmail.com>
2023-09-01 08:52:26 -0700

    feat: support inequalities with varying sign and varying absolute value (PROOF-548) (#354)
    * fill in binary proof
    
    * fill in sign expr
    
    * fill in sign expr
    
    * rework verification builder
    
    * rework const verification
    
    * rework
    
    * refactor
    
    * add tests
    
    * add tests
    
    * fill in tests
    
    * add tests
    
    * doc
    
    * work on const absolute verification
    
    * constant abs decomposition
    
    * support constant absolute
    
    * add test
    
    * fix tests
    
    * add tests
    
    * fill in general sign expr support
    
    * fill in sign expr
    
    * fill in sign expr
    
    * fix clippy
    
    * fix verification
    
    * add tests
    
    * work on lte expr testing
    
    * fill in random test

--- crates/proofs/src/sql/ast/lte_expr_test.rs
+++ crates/proofs/src/sql/ast/lte_expr_test.rs
@@ -2,12 +2,24 @@ use super::{prover_evaluate_equals_zero, prover_evaluate_or, FilterExpr, LteExpr
 
 use crate::base::bit::BitDistribution;
 use crate::base::database::TestAccessor;
+use crate::base::database::{
+    make_random_test_accessor_data, ColumnType, RandomTestAccessorDescriptor,
+};
 use crate::base::scalar::ArkScalar;
 use crate::record_batch;
+use crate::sql::ast::test_expr::TestExprNode;
 use crate::sql::ast::test_utility::{col, cols_result, tab};
 use crate::sql::proof::{ProofBuilder, QueryProof, VerifiableQueryResult};
 use bumpalo::Bump;
 use num_traits::Zero;
+
+use arrow::record_batch::RecordBatch;
+use polars::prelude::*;
+use rand::{
+    distributions::{Distribution, Uniform},
+    rngs::StdRng,
+};
+use rand_core::SeedableRng;
 
 #[test]
 fn we_can_compare_a_constant_column() {
@@ -100,6 +112,44 @@ fn we_can_compare_a_varying_column_with_negative_only_signs() {
     let res = res.verify(&expr, &accessor).unwrap().unwrap();
     let expected_res = record_batch!(
         "b" => [1_i64, 2, 3],
+    );
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_compare_a_column_with_varying_absolute_values_and_signs() {
+    let data = record_batch!(
+        "a" => [-1_i64, 9, 0],
+        "b" => [1_i64, 2, 3],
+    );
+    let t = "sxt.t".parse().unwrap();
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(t, data, 0);
+    let where_clause = Box::new(LteExpr::new(col(t, "a", &accessor), 1.into()));
+    let expr = FilterExpr::new(cols_result(t, &["b"], &accessor), tab(t), where_clause);
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let expected_res = record_batch!(
+        "b" => [1_i64, 3],
+    );
+    assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_compare_a_column_with_varying_absolute_values_and_signs_and_a_constant_bit() {
+    let data = record_batch!(
+        "a" => [-2_i64, 3, 2],
+        "b" => [1_i64, 2, 3],
+    );
+    let t = "sxt.t".parse().unwrap();
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(t, data, 0);
+    let where_clause = Box::new(LteExpr::new(col(t, "a", &accessor), 0.into()));
+    let expr = FilterExpr::new(cols_result(t, &["b"], &accessor), tab(t), where_clause);
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+    let res = res.verify(&expr, &accessor).unwrap().unwrap();
+    let expected_res = record_batch!(
+        "b" => [1_i64],
     );
     assert_eq!(res, expected_res);
 }
@@ -236,4 +286,69 @@ fn verification_fails_if_commitments_dont_match_for_a_constant_sign_column() {
     let where_clause = Box::new(LteExpr::new(col(t, "a", &accessor), 5.into()));
     let expr = FilterExpr::new(cols_result(t, &["b"], &accessor), tab(t), where_clause);
     assert!(res.verify(&expr, &accessor).is_err());
+}
+
+#[test]
+fn verification_fails_if_commitments_dont_match() {
+    let data = record_batch!(
+        "a" => [-523_i64, 923, 823],
+        "b" => [1_i64, 2, 3],
+    );
+    let t = "sxt.t".parse().unwrap();
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(t, data, 0);
+    let where_clause = Box::new(LteExpr::new(col(t, "a", &accessor), 5.into()));
+    let expr = FilterExpr::new(cols_result(t, &["b"], &accessor), tab(t), where_clause);
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+
+    let data = record_batch!(
+        "a" => [-523_i64, 923, 83],
+        "b" => [1_i64, 2, 3],
+    );
+    let t = "sxt.t".parse().unwrap();
+    let mut accessor = TestAccessor::new();
+    accessor.add_table(t, data, 0);
+    let where_clause = Box::new(LteExpr::new(col(t, "a", &accessor), 5.into()));
+    let expr = FilterExpr::new(cols_result(t, &["b"], &accessor), tab(t), where_clause);
+    assert!(res.verify(&expr, &accessor).is_err());
+}
+
+fn create_test_lte_expr<T: Into<ArkScalar> + Copy + Literal>(
+    table_ref: &str,
+    result_col: &str,
+    filter_col: &str,
+    filter_val: T,
+    data: RecordBatch,
+) -> TestExprNode {
+    let mut accessor = TestAccessor::new();
+    let t = table_ref.parse().unwrap();
+    accessor.add_table(t, data, 0);
+
+    let where_clause = Box::new(LteExpr::new(
+        col(t, filter_col, &accessor),
+        filter_val.into(),
+    ));
+
+    let df_filter = polars::prelude::col(filter_col).lt_eq(lit(filter_val));
+    TestExprNode::new(t, &[result_col], where_clause, df_filter, accessor)
+}
+
+#[test]
+fn we_can_query_random_data_of_varying_size() {
+    let descr = RandomTestAccessorDescriptor {
+        min_rows: 1,
+        max_rows: 20,
+        min_value: -3,
+        max_value: 3,
+    };
+    let mut rng = StdRng::from_seed([0u8; 32]);
+    let cols = [("a", ColumnType::BigInt), ("b", ColumnType::BigInt)];
+    for _ in 0..10 {
+        let data = make_random_test_accessor_data(&mut rng, &cols, &descr);
+        let filter_val = Uniform::new(descr.min_value, descr.max_value + 1).sample(&mut rng);
+        let test_expr = create_test_lte_expr("sxt.t", "b", "a", filter_val, data);
+        let res = test_expr.verify_expr();
+        let expected = test_expr.query_table();
+        assert_eq!(res, expected);
+    }
 }
--- crates/proofs/src/sql/ast/sign_expr.rs
+++ crates/proofs/src/sql/ast/sign_expr.rs
@@ -7,8 +7,8 @@ use crate::base::bit::{compute_varying_bit_matrix, BitDistribution};
 use crate::base::proof::ProofError;
 use crate::base::scalar::ArkScalar;
 use crate::sql::proof::{
-    CountBuilder, MultilinearExtensionImpl, ProofBuilder, SumcheckSubpolynomial,
-    VerificationBuilder,
+    CountBuilder, MultilinearExtension, MultilinearExtensionImpl, ProofBuilder,
+    SumcheckSubpolynomial, VerificationBuilder,
 };
 
 use bumpalo::Bump;
@@ -30,10 +30,11 @@ pub fn count_sign(builder: &mut CountBuilder) -> Result<(), ProofError> {
     builder.count_intermediate_mles(dist.num_varying_bits());
     builder.count_subpolynomials(dist.num_varying_bits());
     builder.count_degree(3);
-    if !dist.has_varying_sign_bit() || dist.num_varying_bits() == 1 {
-        return Ok(());
+    if dist.has_varying_sign_bit() && dist.num_varying_bits() > 1 {
+        builder.count_anchored_mles(1);
+        builder.count_subpolynomials(1);
     }
-    todo!();
+    Ok(())
 }
 
 /// Prove the sign decomposition for a column of scalars.
@@ -66,11 +67,11 @@ pub fn prover_evaluate_sign<'a>(
         return alloc.alloc_slice_fill_copy(table_length, dist.sign_bit());
     }
 
-    if dist.num_varying_bits() == 1 {
-        return bits.last().unwrap();
+    if dist.num_varying_bits() > 1 {
+        prove_bit_decomposition(builder, alloc, expr, &bits, &dist);
     }
 
-    todo!();
+    bits.last().unwrap()
 }
 
 /// Verify the sign decomposition for a column of scalars.
@@ -106,10 +107,11 @@ pub fn verifier_evaluate_sign(
     // handle the special case of the absolute part being constant
     if dist.num_varying_bits() == 1 {
         verify_constant_abs_decomposition(&dist, commit, one_commit, &bit_commits[0])?;
-        return Ok(bit_evals[0]);
+    } else {
+        verify_bit_decomposition(builder, commit, &bit_evals, &dist);
     }
 
-    todo!();
+    Ok(*bit_evals.last().unwrap())
 }
 
 fn verifier_const_sign_evaluate(
@@ -152,4 +154,69 @@ fn verify_bits_are_binary(builder: &mut VerificationBuilder, bit_evals: &[ArkSca
         eval *= builder.mle_evaluations.random_evaluation;
         builder.produce_sumcheck_subpolynomial_evaluation(&eval);
     }
+}
+
+fn prove_bit_decomposition<'a>(
+    builder: &mut ProofBuilder<'a>,
+    alloc: &'a Bump,
+    expr: &'a [ArkScalar],
+    bits: &[&'a [bool]],
+    dist: &BitDistribution,
+) {
+    builder.produce_anchored_mle(expr);
+
+    let sign_mle = bits.last().unwrap();
+    let sign_mle = alloc.alloc_slice_fill_with(sign_mle.len(), |i| 1 - 2 * (sign_mle[i] as i32));
+    let mut terms: Vec<(ArkScalar, Vec<Box<dyn MultilinearExtension>>)> = Vec::new();
+
+    // expr
+    terms.push((
+        ArkScalar::one(),
+        vec![Box::new(MultilinearExtensionImpl::new(expr))],
+    ));
+
+    // expr bit decomposition
+    let const_part = dist.constant_part();
+    if !const_part.is_zero() {
+        terms.push((
+            -const_part,
+            vec![Box::new(MultilinearExtensionImpl::new(sign_mle))],
+        ));
+    }
+    let mut vary_index = 0;
+    dist.for_each_abs_varying_bit(|int_index: usize, bit_index: usize| {
+        let mut mult = [0u64; 4];
+        mult[int_index] = 1u64 << bit_index;
+        terms.push((
+            -ArkScalar::from_bigint(mult),
+            vec![
+                Box::new(MultilinearExtensionImpl::new(sign_mle)),
+                Box::new(MultilinearExtensionImpl::new(bits[vary_index])),
+            ],
+        ));
+        vary_index += 1;
+    });
+    builder.produce_sumcheck_subpolynomial(SumcheckSubpolynomial::new(terms));
+}
+
+fn verify_bit_decomposition(
+    builder: &mut VerificationBuilder<'_>,
+    expr_commit: &RistrettoPoint,
+    bit_evals: &[ArkScalar],
+    dist: &BitDistribution,
+) {
+    let sign_eval = bit_evals.last().unwrap();
+    let sign_eval = builder.mle_evaluations.one_evaluation - ArkScalar::from(2) * *sign_eval;
+    let mut vary_index = 0;
+    let mut eval = builder.consume_anchored_mle(expr_commit);
+    eval -= sign_eval * dist.constant_part();
+    dist.for_each_abs_varying_bit(|int_index: usize, bit_index: usize| {
+        let mut mult = [0u64; 4];
+        mult[int_index] = 1u64 << bit_index;
+        let bit_eval = bit_evals[vary_index];
+        eval -= ArkScalar::from_bigint(mult) * sign_eval * bit_eval;
+        vary_index += 1;
+    });
+    eval *= builder.mle_evaluations.random_evaluation;
+    builder.produce_sumcheck_subpolynomial_evaluation(&eval);
 }
