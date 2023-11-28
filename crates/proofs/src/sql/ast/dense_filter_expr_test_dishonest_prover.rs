968029bd26df43451909b0ce8ea7e94550af187c
Jay White <JayWhite2357@users.noreply.github.com>
2023-11-28 09:33:01 -0500

    feat: dense filter results ( PROOF-634 ) (#412)
    * feat: copy filter to dense filter
    
    * create/rename DenseFilterExpr and ColumnExpr nodes
    
    * make results dense
    
    * feat: add `result_evaluate` to `QueryExpr`
    
    * feat: refactor `QueryProof` with `ResultBuilder`
    
    * test: result_evaluate for FilterExpr
    
    * style
    
    * style
    
    * style
    
    * fix merge
    
    * refactoring code
    
    * docs and refactoring
    
    * test: filter_columns
    
    * comment and simplify
    
    * update ignored test
    
    * add missing_docs warning
    
    * update docs

--- crates/proofs/src/sql/ast/dense_filter_expr_test_dishonest_prover.rs
+++ crates/proofs/src/sql/ast/dense_filter_expr_test_dishonest_prover.rs
@@ -1,21 +1,23 @@
-use super::OstensibleDenseFilterExpr;
+use super::{filter_columns, OstensibleDenseFilterExpr};
 use crate::{
     base::{
-        database::{DataAccessor, RecordBatchTestAccessor, TestAccessor},
+        database::{Column, DataAccessor, OwnedTableTestAccessor, TestAccessor},
         proof::ProofError,
+        scalar::ArkScalar,
     },
-    record_batch,
+    owned_table,
     sql::{
         // Making this explicit to ensure that we don't accidentally use the
         // sparse filter for these tests
         ast::test_utility::{cols_expr, equal, tab},
         proof::{
-            Indexes, ProofBuilder, ProverEvaluate, ProverHonestyMarker, QueryError, ResultBuilder,
-            VerifiableQueryResult,
+            exercise_verification, Indexes, ProofBuilder, ProverEvaluate, ProverHonestyMarker,
+            QueryError, ResultBuilder, VerifiableQueryResult,
         },
     },
 };
 use bumpalo::Bump;
+use num_traits::One;
 
 #[derive(Debug, PartialEq)]
 struct Dishonest;
@@ -29,20 +31,25 @@ impl ProverEvaluate for DishonestDenseFilterExpr {
         alloc: &'a Bump,
         accessor: &'a dyn DataAccessor,
     ) {
-        // evaluate where clause
+        // 1. selection
         let selection = self
             .where_clause
             .result_evaluate(builder.table_length(), alloc, accessor);
-
-        // set result indexes
-        let mut indexes: Vec<_> = selection
-            .iter()
-            .enumerate()
-            .filter(|(_, &b)| b)
-            .map(|(i, _)| i as u64)
-            .collect();
-        indexes[0] += 1;
-        builder.set_result_indexes(Indexes::Sparse(indexes));
+        // 2. columns
+        let columns = Vec::from_iter(
+            self.results
+                .iter()
+                .map(|expr| expr.result_evaluate(accessor)),
+        );
+        // Compute filtered_columns and indexes
+        let (filtered_columns, result_len) = filter_columns(alloc, &columns, selection);
+        let filtered_columns = tamper_column(alloc, filtered_columns);
+        // 3. set indexes
+        builder.set_result_indexes(Indexes::Dense(0..(result_len as u64)));
+        // 4. set filtered_columns
+        for col in filtered_columns {
+            builder.produce_result_column(col.into());
+        }
     }
 
     #[tracing::instrument(
@@ -50,36 +57,96 @@ impl ProverEvaluate for DishonestDenseFilterExpr {
         level = "info",
         skip_all
     )]
+    #[allow(unused_variables)]
     fn prover_evaluate<'a>(
         &self,
         builder: &mut ProofBuilder<'a>,
         alloc: &'a Bump,
         accessor: &'a dyn DataAccessor,
     ) {
-        // evaluate where clause
+        // 1. selection
         let selection = self.where_clause.prover_evaluate(builder, alloc, accessor);
-
-        // evaluate result columns
-        for expr in self.results.iter() {
-            expr.prover_evaluate(builder, alloc, accessor, selection);
-        }
+        // 2. columns
+        let columns = Vec::from_iter(
+            self.results
+                .iter()
+                .map(|expr| expr.prover_evaluate(builder, accessor)),
+        );
+        // Compute filtered_columns and indexes
+        let (filtered_columns, result_len) = filter_columns(alloc, &columns, selection);
+        let filtered_columns = tamper_column(alloc, filtered_columns);
+        // todo!: build the proof components that show that the filtered_columns are actually correct.
     }
 }
 
+/// Tamper with the first element of the first column that is a Scalar. This could be changed for different types of tests.
+fn tamper_column<'a>(alloc: &'a Bump, mut columns: Vec<Column<'a>>) -> Vec<Column<'a>> {
+    for column in columns.iter_mut() {
+        if let Column::Scalar(tampered_column) = column {
+            if !tampered_column.is_empty() {
+                let tampered_column = alloc.alloc_slice_copy(tampered_column);
+                // The following could be changed for different types of tests, but for the simplest one, we will simply increase the first element by 1.
+                tampered_column[0] += ArkScalar::one();
+                *column = Column::Scalar(tampered_column);
+                break;
+            }
+        }
+    }
+    columns
+}
+
+// This test should eventually pass, but it does not because the above dense filter isn't actually secure.
+#[ignore]
 #[test]
 fn we_fail_to_verify_a_basic_dense_filter_with_a_dishonest_prover() {
-    let data = record_batch!(
-        "a" => [1_i64, 4_i64, 5_i64, 2_i64, 5_i64],
+    let data = owned_table!(
+        "a" => [101_i64, 104, 105, 102, 105],
         "b" => [1_i64, 2, 3, 4, 5],
+        "c" => [1_i128, 2, 3, 4, 5],
+        "d" => ["1", "2", "3", "4", "5"],
+        "e" => [ArkScalar::from(1), 2.into(), 3.into(), 4.into(), 5.into()],
     );
     let t = "sxt.t".parse().unwrap();
-    let mut accessor = RecordBatchTestAccessor::new_empty();
+    let mut accessor = OwnedTableTestAccessor::new_empty();
     accessor.add_table(t, data, 0);
-    let where_clause = equal(t, "a", 5, &accessor);
-    let expr = DishonestDenseFilterExpr::new(cols_expr(t, &["b"], &accessor), tab(t), where_clause);
+    let expr = DishonestDenseFilterExpr::new(
+        cols_expr(t, &["b", "c", "d", "e"], &accessor),
+        tab(t),
+        equal(t, "a", 105, &accessor),
+    );
     let res = VerifiableQueryResult::new(&expr, &accessor);
     assert!(matches!(
         res.verify(&expr, &accessor),
         Err(QueryError::ProofError(ProofError::VerificationError(_)))
     ));
+}
+
+// This test shows that the above dense filter isn't actually secure. This test should eventually be removed in favor of the previous one.
+#[test]
+fn we_can_incorrectly_prove_a_dense_filter_even_when_tampered() {
+    let data = owned_table!(
+        "a" => [101_i64, 104, 105, 102, 105],
+        "b" => [1_i64, 2, 3, 4, 5],
+        "c" => [1_i128, 2, 3, 4, 5],
+        "d" => ["1", "2", "3", "4", "5"],
+        "e" => [ArkScalar::from(1), 2.into(), 3.into(), 4.into(), 5.into()],
+    );
+    let t = "sxt.t".parse().unwrap();
+    let mut accessor = OwnedTableTestAccessor::new_empty();
+    accessor.add_table(t, data, 0);
+    let expr = DishonestDenseFilterExpr::new(
+        cols_expr(t, &["b", "c", "d", "e"], &accessor),
+        tab(t),
+        equal(t, "a", 105, &accessor),
+    );
+    let res = VerifiableQueryResult::new(&expr, &accessor);
+    exercise_verification(&res, &expr, &accessor, t);
+    let res = res.verify(&expr, &accessor).unwrap().table;
+    let expected = owned_table!(
+        "b" => [3_i64, 5],
+        "c" => [3_i128, 5],
+        "d" => ["3", "5"],
+        "e" => [ArkScalar::from(3), 5.into()],
+    );
+    assert_ne!(res, expected);
 }
