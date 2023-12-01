c6a02e5154fad55a33139b9ec304e20105ecaf4e
Jay White <JayWhite2357@users.noreply.github.com>
2023-12-01 12:18:26 -0500

    feat: add Dense Filter fold helper functions ( PROOF-634 ) (#430)
    * add dense_filter_util fold
    
    * fix test

--- crates/proofs/src/base/slice_ops/mul_add_assign.rs
+++ crates/proofs/src/base/slice_ops/mul_add_assign.rs
@@ -4,9 +4,10 @@ use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelI
 /// This operation does `result[i] += multiplier * to_mul_add[i]` for `i` in `0..to_mul_add.len()`.
 ///
 /// It panics if `result.len() < to_mul_add.len()`.
-pub fn mul_add_assign<T>(result: &mut [T], multiplier: T, to_mul_add: &[T])
+pub fn mul_add_assign<T, S>(result: &mut [T], multiplier: T, to_mul_add: &[S])
 where
     T: Send + Sync + Mul<Output = T> + AddAssign + Copy,
+    S: Into<T> + Sync + Copy,
 {
     assert!(result.len() >= to_mul_add.len());
     result
@@ -14,6 +15,6 @@ where
         .with_min_len(super::MIN_RAYON_LEN)
         .zip(to_mul_add)
         .for_each(|(res_i, &data_i)| {
-            *res_i += multiplier * data_i;
+            *res_i += multiplier * data_i.into();
         })
 }
--- crates/proofs/src/sql/ast/dense_filter_util.rs
+++ crates/proofs/src/sql/ast/dense_filter_util.rs
@@ -1,5 +1,6 @@
-use crate::base::database::Column;
+use crate::base::{database::Column, scalar::ArkScalar, slice_ops};
 use bumpalo::Bump;
+use num_traits::One;
 
 /// This function takes a selection vector and a set of columns and returns a
 /// new set of columns that only contains the selected rows. The function
@@ -45,4 +46,56 @@ pub fn filter_columns<'a>(
         })),
         indexes.len(),
     )
+}
+
+/// This function takes a set of columns and returns a slice of scalars that
+/// is the result of folding the columns.
+///
+/// The result `fold` satisfies
+/// `fold[i] = alpha + sum (beta^j * columns[j][i]) for j in 0..columns.len()`
+/// where each column is padded with 0s as needed.
+///
+/// Note: this means that `alpha` is the "default" value for the fold. (i.e. if `n` is larger than the longest column, the result will be padded with `alpha`.)
+///
+/// This is similar to applying `fold_vals` on each row with `one_val` set to `1`.
+pub fn fold_columns<'a>(
+    alloc: &'a Bump,
+    alpha: ArkScalar,
+    beta: ArkScalar,
+    columns: &[Column],
+    n: usize,
+) -> &'a mut [ArkScalar] {
+    let fold = alloc.alloc_slice_fill_copy(n, alpha);
+    let mut multiplier = ArkScalar::one();
+    for col in columns.iter() {
+        match col {
+            Column::BigInt(c) => slice_ops::mul_add_assign(fold, multiplier, c),
+            Column::VarChar((_, c)) => slice_ops::mul_add_assign(fold, multiplier, c),
+            Column::Int128(c) => slice_ops::mul_add_assign(fold, multiplier, c),
+            #[cfg(test)]
+            Column::Scalar(c) => slice_ops::mul_add_assign(fold, multiplier, c),
+        }
+        multiplier *= beta;
+    }
+    fold
+}
+
+/// This function takes a set of values and returns a scalar that is the
+/// result of folding the values.
+///
+/// The result `fold` satisfies
+/// `fold = alpha * one_val + sum (beta^j * vals[j]) for j in 0..vals.len()`
+pub fn fold_vals(
+    alpha: ArkScalar,
+    beta: ArkScalar,
+    vals: impl IntoIterator<Item = ArkScalar>,
+    one_val: ArkScalar,
+) -> ArkScalar {
+    let mut fold = alpha * one_val;
+    let mut multiplier = ArkScalar::one();
+    for val in vals {
+        fold += multiplier * val;
+        multiplier *= beta;
+    }
+    fold
 }
