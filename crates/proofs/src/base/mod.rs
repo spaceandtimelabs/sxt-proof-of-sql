de86959d11237ec54a1fda410e7ba2d61df0b8ed
Dustin Ray <40841027+drcapybara@users.noreply.github.com>
2023-12-08 10:09:49 -0800

    fix!: ensure that all public parameters are being added to the transcript (PROOF-755) (#437)
    * fix: include public params in transcript, init
    
    * fix: add length and offset to transcript
    
    * fix: update docs
    
    * fix: update docs and make_transcript params
    
    * docs: warn(missing_docs)
    
    * fix: fmt
    
    * fix: docs
    
    * fix: docs
    
    * fix: docs
    
    * fix: docs

--- crates/proofs/src/base/mod.rs
+++ crates/proofs/src/base/mod.rs
@@ -4,7 +4,6 @@ pub mod encode;
 pub mod math;
 pub mod polynomial;
 #[warn(missing_docs)]
-/// Contains the transcript protocol used to construct a proof
 pub mod proof;
 pub mod scalar;
 pub mod slice_ops;
