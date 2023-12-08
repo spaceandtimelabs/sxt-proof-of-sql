016deadd2d57009f327bf88c8e035e53b286561d
Dustin Ray <40841027+drcapybara@users.noreply.github.com>
2023-12-08 10:09:49 -0800

    fix!: ensure that all public parameters are being added to the transcript (PROOF-755) (#437)
    * fix: include public params in transcript, init
    
    * fix: add length and offset to transcript
    
    * fix: update docs
    
    * fix: update docs and make_transcript params
    
    * docs: warn(missing_docs)
    
    * fix: fmt
    
    * fix: docs
    
    * fix: docs
    
    * fix: docs
    
    * fix: docs

--- crates/proofs/src/base/proof/mod.rs
+++ crates/proofs/src/base/proof/mod.rs
@@ -1,8 +1,10 @@
+//! Contains the transcript protocol used to construct a proof,
+//! as well as an error type which can occur when verification fails.
 mod error;
 pub use error::ProofError;
 
 #[warn(missing_docs)]
-/// Transcript protocol used to construct a proof.
+/// Contains an extension trait for `merlin::Transcript`, which is used to construct a proof.
 mod transcript_protocol;
 #[cfg(test)]
 mod transcript_protocol_test;
