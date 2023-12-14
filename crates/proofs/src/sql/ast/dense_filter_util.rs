22e45bf682f27f7569c1828811607589e0e793aa
Jay White <JayWhite2357@users.noreply.github.com>
2023-12-14 14:34:25 -0500

    refactor: update dense_filter_util helpers for reuse with group by ( PROOF-765 ) (#442)
    * `impl MultilinearExtension for Column`
    
    * add tests
    
    * add len and is_empty to Column
    
    * make ColumnField Serialize, Deserialize
    
    * refactor dense_filter_util for group by
    
    * update docs
    
    * docs
    
    * docs and export

--- crates/proofs/src/sql/ast/dense_filter_util.rs
+++ crates/proofs/src/sql/ast/dense_filter_util.rs
@@ -1,4 +1,7 @@
-use crate::base::{database::Column, scalar::ArkScalar, slice_ops};
+use crate::{
+    base::{database::Column, scalar::ArkScalar},
+    sql::proof::MultilinearExtension,
+};
 use bumpalo::Bump;
 use num_traits::One;
 
@@ -13,89 +16,79 @@ pub fn filter_columns<'a>(
     columns: &[Column<'a>],
     selection: &[bool],
 ) -> (Vec<Column<'a>>, usize) {
+    for col in columns {
+        assert_eq!(col.len(), selection.len());
+    }
     let indexes: Vec<_> = selection
         .iter()
         .enumerate()
         .filter(|(_, &b)| b)
         .map(|(i, _)| i)
         .collect();
-
-    (
-        Vec::from_iter(columns.iter().map(|column| match column {
-            Column::BigInt(col) => {
-                assert_eq!(col.len(), selection.len());
-                Column::BigInt(alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])))
-            }
-            Column::Int128(col) => {
-                assert_eq!(col.len(), selection.len());
-                Column::Int128(alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])))
-            }
-            Column::VarChar((col, scals)) => {
-                assert_eq!(col.len(), selection.len());
-                assert_eq!(scals.len(), selection.len());
-                Column::VarChar((
-                    alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])),
-                    alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| scals[i])),
-                ))
-            }
-            #[cfg(test)]
-            Column::Scalar(col) => {
-                assert_eq!(col.len(), selection.len());
-                Column::Scalar(alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])))
-            }
-        })),
-        indexes.len(),
-    )
+    let result_length = indexes.len();
+    let filtered_result = Vec::from_iter(
+        columns
+            .iter()
+            .map(|column| filter_column_by_index(alloc, column, &indexes)),
+    );
+    (filtered_result, result_length)
+}
+/// This function takes an index vector and a `Column` and returns a
+/// new set of columns that only contains the selected indexes. It is assumed that
+/// the indexes are valid.
+pub fn filter_column_by_index<'a>(
+    alloc: &'a Bump,
+    column: &Column<'a>,
+    indexes: &[usize],
+) -> Column<'a> {
+    match column {
+        Column::BigInt(col) => {
+            Column::BigInt(alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])))
+        }
+        Column::Int128(col) => {
+            Column::Int128(alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])))
+        }
+        Column::VarChar((col, scals)) => Column::VarChar((
+            alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])),
+            alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| scals[i])),
+        )),
+        #[cfg(test)]
+        Column::Scalar(col) => {
+            Column::Scalar(alloc.alloc_slice_fill_iter(indexes.iter().map(|&i| col[i])))
+        }
+    }
 }
 
-/// This function takes a set of columns and returns a slice of scalars that
-/// is the result of folding the columns.
+/// This function takes a set of columns and fold it into a slice of scalars.
 ///
-/// The result `fold` satisfies
-/// `fold[i] = alpha + sum (beta^j * columns[j][i]) for j in 0..columns.len()`
+/// The result `res` is updated with
+/// `res[i] += mul * sum (beta^j * columns[j][i]) for j in 0..columns.len()`
 /// where each column is padded with 0s as needed.
 ///
-/// Note: this means that `alpha` is the "default" value for the fold. (i.e. if `n` is larger than the longest column, the result will be padded with `alpha`.)
-///
-/// This is similar to applying `fold_vals` on each row with `one_val` set to `1`.
-pub fn fold_columns<'a>(
-    alloc: &'a Bump,
-    alpha: ArkScalar,
+/// This is similar to adding `mul * fold_vals(beta,...)` on each row.
+
+pub fn fold_columns(
+    res: &mut [ArkScalar],
+    mul: ArkScalar,
     beta: ArkScalar,
-    columns: &[Column],
-    n: usize,
-) -> &'a mut [ArkScalar] {
-    let fold = alloc.alloc_slice_fill_copy(n, alpha);
-    let mut multiplier = ArkScalar::one();
-    for col in columns.iter() {
-        match col {
-            Column::BigInt(c) => slice_ops::mul_add_assign(fold, multiplier, c),
-            Column::VarChar((_, c)) => slice_ops::mul_add_assign(fold, multiplier, c),
-            Column::Int128(c) => slice_ops::mul_add_assign(fold, multiplier, c),
-            #[cfg(test)]
-            Column::Scalar(c) => slice_ops::mul_add_assign(fold, multiplier, c),
-        }
-        multiplier *= beta;
+    columns: &[impl MultilinearExtension],
+) {
+    for (m, col) in powers(mul, beta).zip(columns) {
+        col.mul_add(res, &m);
     }
-    fold
 }
 
 /// This function takes a set of values and returns a scalar that is the
 /// result of folding the values.
 ///
-/// The result `fold` satisfies
-/// `fold = alpha * one_val + sum (beta^j * vals[j]) for j in 0..vals.len()`
-pub fn fold_vals(
-    alpha: ArkScalar,
-    beta: ArkScalar,
-    vals: impl IntoIterator<Item = ArkScalar>,
-    one_val: ArkScalar,
-) -> ArkScalar {
-    let mut fold = alpha * one_val;
-    let mut multiplier = ArkScalar::one();
-    for val in vals {
-        fold += multiplier * val;
-        multiplier *= beta;
-    }
-    fold
+/// The result is
+/// `sum (beta^j * vals[j]) for j in 0..vals.len()`
+pub fn fold_vals(beta: ArkScalar, vals: &[ArkScalar]) -> ArkScalar {
+    let beta_powers = powers(ArkScalar::one(), beta);
+    beta_powers.zip(vals).map(|(pow, &val)| pow * val).sum()
+}
+
+/// Returns an iterator for the lazily evaluated sequence `init, init * base, init * base^2, ...`
+fn powers(init: ArkScalar, base: ArkScalar) -> impl Iterator<Item = ArkScalar> {
+    core::iter::successors(Some(init), move |&m| Some(m * base))
 }
