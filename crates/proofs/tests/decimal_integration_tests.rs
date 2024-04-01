1708a829654d4c5d051987700cba5b7d433eaba9
Dustin Ray <40841027+drcapybara@users.noreply.github.com>
2024-04-01 08:49:27 -0700

    feat: end to end decimal tests (PROOF-799) (#511)
    * end to end decimal tests
    
    * unit tests new_empty_with_setup fix
    
    * test features
    
    * test feature
    
    * test feature
    
    * test mod
    
    * refactor!: rename all instances of `ArkScalar` to `Curve25519Scalar` (PROOF-829) (#517)
    
    refactor: rename all instances of `ArkScalar` to `Curve25519Scalar`
    
    - Since both RistrettoPoint and DoryCommitment use elliptic curves in crates developed by ArkWorks
    (curve25519 and BLS12-381 respectively) it is a misnomer to refer to MontScalar<ark_curve25519::FrConfig> as ArkScalar.
    Hence the renaming to Curve25519Scalar.
    - Rename instances of ark_scalar and arkscalar
    
    * rebase
    
    ---------
    
    Co-authored-by: Ian Alexander Joiner <14581281+iajoiner@users.noreply.github.com>

---
+++ crates/proofs/tests/decimal_integration_tests.rs
@@ -0,0 +1,212 @@
+#![cfg(feature = "test")]
+#[cfg(feature = "blitzar")]
+use blitzar::proof::InnerProductProof;
+#[cfg(feature = "blitzar")]
+use proofs::{base::scalar::Curve25519Scalar as S, owned_table};
+
+#[cfg(feature = "blitzar")]
+fn run_query(
+    query_str: &str,
+    expected_precision: u8,
+    expected_scale: i8,
+    test_decimal_values: Vec<S>,
+    expected_decimal_values: Vec<S>,
+) {
+    use proofs::{
+        base::database::{OwnedTable, OwnedTableTestAccessor, TestAccessor},
+        sql::{parse::QueryExpr, proof::QueryProof},
+    };
+
+    // Setup common data and accessor for each run
+    let mut accessor = OwnedTableTestAccessor::<InnerProductProof>::new_empty_with_setup(());
+    let mut data: OwnedTable<S> = owned_table!("a" => [1i64, 2, 3], "b" => ["t", "u", "v"]);
+    data.append_decimal_columns_for_testing(
+        "c",
+        expected_precision,
+        expected_scale,
+        test_decimal_values,
+    );
+
+    accessor.add_table("sxt.table".parse().unwrap(), data, 0);
+
+    let query = QueryExpr::try_new(
+        query_str.parse().unwrap(),
+        "sxt".parse().unwrap(),
+        &accessor,
+    )
+    .unwrap();
+    let (proof, serialized_result) =
+        QueryProof::<InnerProductProof>::new(query.proof_expr(), &accessor, &());
+    let owned_table_result = proof
+        .verify(query.proof_expr(), &accessor, &serialized_result, &())
+        .unwrap()
+        .table;
+
+    // Adjust expected result based on the precision and scale provided
+    let mut expected_result = owned_table!("a" => [1_i64, 3], "b" => ["t", "v"]);
+    expected_result.append_decimal_columns_for_testing(
+        "c",
+        expected_precision,
+        expected_scale,
+        expected_decimal_values,
+    );
+    // Verify the result matches the expectation
+    assert_eq!(owned_table_result, expected_result);
+}
+
+#[cfg(feature = "blitzar")]
+mod decimal_query_tests {
+    use crate::run_query;
+    use proofs::base::scalar::{Curve25519Scalar as S, Scalar};
+
+    #[test]
+    fn we_can_query_decimals_exactly_matching_db_data() {
+        run_query(
+            "SELECT * FROM table WHERE c = 1.0",
+            2,
+            1,
+            vec![S::from(1), S::ZERO, S::ONE],
+            vec![S::from(1), S::ONE],
+        );
+    }
+
+    #[test]
+    fn we_can_query_simple_decimals() {
+        run_query(
+            "SELECT * FROM table WHERE c = 1.1",
+            2,
+            1,
+            vec![S::from(11), S::ZERO, S::from(11)],
+            vec![S::from(11), S::from(11)],
+        );
+    }
+
+    #[test]
+    fn we_can_query_negative_valued_decimals_exactly_matching_db_data() {
+        run_query(
+            "SELECT * FROM table WHERE c = -1.0",
+            2,
+            1,
+            vec![S::from(-1), S::ZERO, -S::ONE],
+            vec![S::from(-1), -S::ONE],
+        );
+    }
+
+    #[test]
+    fn we_can_query_zero_as_decimal() {
+        run_query(
+            "SELECT * FROM table WHERE c = 0.0",
+            2,
+            1,
+            vec![S::ZERO, S::ONE, S::ZERO],
+            vec![S::ZERO, S::ZERO],
+        );
+    }
+    #[test]
+    fn we_can_query_negative_decimals_with_different_scale_than_db_data() {
+        run_query(
+            "SELECT * FROM table WHERE c = -1.0",
+            2,
+            3,
+            vec![S::from(-100), S::ZERO, S::from(-100)],
+            vec![S::from(-100), S::from(-100)],
+        );
+    }
+    #[test]
+    fn we_can_query_with_negative_values_with_trailing_zeros() {
+        run_query(
+            "SELECT * FROM table WHERE c = -1.000",
+            4,
+            2,
+            vec![S::from(-100), S::ZERO, S::from(-100)],
+            vec![S::from(-100), S::from(-100)],
+        );
+    }
+    #[test]
+    fn we_can_query_decimals_without_leading_zeros() {
+        run_query(
+            "SELECT * FROM table WHERE c = .1",
+            2,
+            1,
+            vec![S::from(1), S::ZERO, S::ONE],
+            vec![S::from(1), S::ONE],
+        );
+    }
+
+    #[test]
+    fn we_can_query_decimals_with_leading_zeros() {
+        run_query(
+            "SELECT * FROM table WHERE c = 0.1",
+            1,
+            1,
+            vec![S::from(1), S::ZERO, S::ONE],
+            vec![S::from(1), S::ONE],
+        );
+    }
+
+    #[test]
+    fn we_can_query_decimals_with_lower_scale_than_db_data() {
+        run_query(
+            "SELECT * FROM table WHERE c = 0.1",
+            1,
+            6,
+            vec![S::from(100000), S::ZERO, S::from(100000)],
+            vec![S::from(100000), S::from(100000)],
+        );
+    }
+
+    #[test]
+    fn we_can_query_negative_decimals_with_trailing_zeros_exactly_matching_db_data() {
+        run_query(
+            "SELECT * FROM table WHERE c = -0.100000",
+            7,
+            1,
+            vec![S::from(-1), S::ZERO, -S::ONE],
+            vec![S::from(-1), -S::ONE],
+        );
+    }
+
+    #[test]
+    fn we_can_query_with_varying_scale_and_precision() {
+        run_query(
+            "SELECT * FROM table WHERE c = 123.456",
+            6,
+            3,
+            vec![S::from(123456), S::ZERO, S::from(123456)],
+            vec![S::from(123456), S::from(123456)],
+        );
+    }
+
+    #[test]
+    fn we_cannot_round_decimal_queries() {
+        run_query(
+            "SELECT * FROM table WHERE c = -0.1000001",
+            7,
+            7,
+            vec![S::from(-1000001), S::ZERO, S::from(-1000001)],
+            vec![S::from(-1000001), S::from(-1000001)],
+        );
+    }
+
+    #[test]
+    fn we_can_query_integers_against_decimals() {
+        run_query(
+            "SELECT * FROM table WHERE c = 12345",
+            7,
+            2,
+            vec![S::from(1234500), S::ZERO, S::from(1234500)],
+            vec![S::from(1234500), S::from(1234500)],
+        );
+    }
+
+    #[test]
+    fn we_can_query_negative_integers_against_decimals() {
+        run_query(
+            "SELECT * FROM table WHERE c = -12345",
+            7,
+            2,
+            vec![-S::from(1234500), S::ZERO, -S::from(1234500)],
+            vec![-S::from(1234500), -S::from(1234500)],
+        );
+    }
+}
