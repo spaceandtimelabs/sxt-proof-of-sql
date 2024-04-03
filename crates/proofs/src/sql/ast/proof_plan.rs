ff545273515a8a273043dada41ffff7424145551
Ian Alexander Joiner <14581281+iajoiner@users.noreply.github.com>
2024-04-03 12:42:56 -0400

    feat: add `ProofPlan::GroupBy` for provable groupby queries (PROOF-765) (#521)
    * feat: make instances of Commitment in proofs::ast generic (PROOF-765)
    
    * fix: fix compilation issues with sum_aggregate_slice_by_index_counts and prove_filter
    
    * fix: fix test-related compilation issues
    
    - Specify usage of Curve25519 or Dory for generic expressions
    
    * feat: add ast changes for groupby
    
    ---------
    
    Co-authored-by: Trevor Lovell <trevor.lovell@spaceandtime.io>

--- crates/proofs/src/sql/ast/proof_plan.rs
+++ crates/proofs/src/sql/ast/proof_plan.rs
@@ -1,4 +1,4 @@
-use super::FilterExpr;
+use super::{FilterExpr, GroupByExpr};
 use crate::{
     base::commitment::Commitment,
     sql::proof::{ProofExpr, ProverEvaluate},
@@ -13,6 +13,16 @@ pub enum ProofPlan<C: Commitment> {
     ///     SELECT <result_expr1>, ..., <result_exprN> FROM <table> WHERE <where_clause>
     /// ```
     Filter(FilterExpr<C>),
+    /// Provable expressions for queries of the form
+    /// ```ignore
+    ///     SELECT <group_by_expr1>, ..., <group_by_exprM>,
+    ///         SUM(<sum_expr1>.0) as <sum_expr1>.1, ..., SUM(<sum_exprN>.0) as <sum_exprN>.1,
+    ///         COUNT(*) as count_alias
+    ///     FROM <table>
+    ///     WHERE <where_clause>
+    ///     GROUP BY <group_by_expr1>, ..., <group_by_exprM>
+    /// ```
+    GroupBy(GroupByExpr<C>),
 }
 
 impl<C: Commitment> ProofExpr<C> for ProofPlan<C> {
@@ -23,18 +33,21 @@ impl<C: Commitment> ProofExpr<C> for ProofPlan<C> {
     ) -> Result<(), crate::base::proof::ProofError> {
         match self {
             ProofPlan::Filter(expr) => expr.count(builder, accessor),
+            ProofPlan::GroupBy(expr) => expr.count(builder, accessor),
         }
     }
 
     fn get_length(&self, accessor: &dyn crate::base::database::MetadataAccessor) -> usize {
         match self {
             ProofPlan::Filter(expr) => expr.get_length(accessor),
+            ProofPlan::GroupBy(expr) => expr.get_length(accessor),
         }
     }
 
     fn get_offset(&self, accessor: &dyn crate::base::database::MetadataAccessor) -> usize {
         match self {
             ProofPlan::Filter(expr) => expr.get_offset(accessor),
+            ProofPlan::GroupBy(expr) => expr.get_offset(accessor),
         }
     }
 
@@ -45,18 +58,21 @@ impl<C: Commitment> ProofExpr<C> for ProofPlan<C> {
     ) -> Result<(), crate::base::proof::ProofError> {
         match self {
             ProofPlan::Filter(expr) => expr.verifier_evaluate(builder, accessor),
+            ProofPlan::GroupBy(expr) => expr.verifier_evaluate(builder, accessor),
         }
     }
 
     fn get_column_result_fields(&self) -> Vec<crate::base::database::ColumnField> {
         match self {
             ProofPlan::Filter(expr) => expr.get_column_result_fields(),
+            ProofPlan::GroupBy(expr) => expr.get_column_result_fields(),
         }
     }
 
     fn get_column_references(&self) -> std::collections::HashSet<crate::base::database::ColumnRef> {
         match self {
             ProofPlan::Filter(expr) => expr.get_column_references(),
+            ProofPlan::GroupBy(expr) => expr.get_column_references(),
         }
     }
 }
@@ -70,6 +86,7 @@ impl<C: Commitment> ProverEvaluate<C::Scalar> for ProofPlan<C> {
     ) {
         match self {
             ProofPlan::Filter(expr) => expr.result_evaluate(builder, alloc, accessor),
+            ProofPlan::GroupBy(expr) => expr.result_evaluate(builder, alloc, accessor),
         }
     }
 
@@ -81,6 +98,7 @@ impl<C: Commitment> ProverEvaluate<C::Scalar> for ProofPlan<C> {
     ) {
         match self {
             ProofPlan::Filter(expr) => expr.prover_evaluate(builder, alloc, accessor),
+            ProofPlan::GroupBy(expr) => expr.prover_evaluate(builder, alloc, accessor),
         }
     }
 }
