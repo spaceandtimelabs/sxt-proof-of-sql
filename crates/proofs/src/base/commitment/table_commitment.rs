32ce69e25e31153fe19b6fd90f39a688c44dc022
Jay White <JayWhite2357@users.noreply.github.com>
2024-05-01 13:33:09 -0400

    feat: add `RecordBatch` capabilities to `TableCommitment` ( PROOF-857 ) (#547)
    feat: add `RecordBatch` capabilities to `TableCommitment`

--- crates/proofs/src/base/commitment/table_commitment.rs
+++ crates/proofs/src/base/commitment/table_commitment.rs
@@ -1,12 +1,17 @@
 use super::{
     committable_column::CommittableColumn, AppendColumnCommitmentsError, ColumnCommitments,
-    ColumnCommitmentsMismatch, DuplicateIdentifiers, VecCommitmentExt,
+    ColumnCommitmentsMismatch, Commitment, DuplicateIdentifiers, VecCommitmentExt,
 };
 use crate::base::{
-    database::{ColumnField, CommitmentAccessor, OwnedTable, TableRef},
+    database::{
+        ArrayRefExt, ArrowArrayToColumnConversionError, Column, ColumnField, CommitmentAccessor,
+        OwnedTable, TableRef,
+    },
     scalar::Scalar,
 };
-use proofs_sql::Identifier;
+use arrow::record_batch::RecordBatch;
+use bumpalo::Bump;
+use proofs_sql::{Identifier, ParseError};
 use serde::{Deserialize, Serialize};
 use std::ops::Range;
 use thiserror::Error;
@@ -55,6 +60,28 @@ pub enum TableCommitmentArithmeticError {
     /// Cannot perform arithmetic for noncontiguous table commitments.
     #[error("cannot perform table commitment arithmetic for noncontiguous table commitments")]
     NonContiguous,
+}
+
+/// Errors that can occur when trying to create or extend a [`TableCommitment`] from a record batch.
+#[derive(Debug, Error)]
+pub enum RecordBatchToColumnsError {
+    /// Error converting from arrow array
+    #[error(transparent)]
+    ArrowArrayToColumnConversionError(#[from] ArrowArrayToColumnConversionError),
+    #[error(transparent)]
+    /// This error occurs when convering from a record batch name to an identifier fails. (Which may be impossible.)
+    FieldParseFail(#[from] ParseError),
+}
+
+/// Errors that can occur when attempting to append a record batch to a [`TableCommitment`].
+#[derive(Debug, Error)]
+pub enum AppendRecordBatchTableCommitmentError {
+    /// During commitment operation, metadata indicates that operand tables cannot be the same.
+    #[error(transparent)]
+    ColumnCommitmentsMismatch(#[from] ColumnCommitmentsMismatch),
+    /// Error converting from arrow array
+    #[error(transparent)]
+    ArrowBatchToColumnError(#[from] RecordBatchToColumnsError),
 }
 
 /// Commitment for an entire table, with column and table metadata.
@@ -331,6 +358,84 @@ where
             range,
         })
     }
+
+    /// Append an arrow [`RecordBatch`] to the existing [`TableCommitment`].
+    ///
+    /// The row offset is assumed to be the end of the [`TableCommitment`]'s current range.
+    ///
+    /// Will error on a variety of mismatches, or if the provided columns have mixed length.
+    pub fn try_append_record_batch(
+        &mut self,
+        batch: &RecordBatch,
+        setup: &Setup<C>,
+    ) -> Result<(), AppendRecordBatchTableCommitmentError> {
+        match self.try_append_rows(
+            batch_to_columns::<<Decompressed<C> as Commitment>::Scalar>(batch, &Bump::new())?
+                .iter()
+                .map(|(a, b)| (a, b)),
+            setup,
+        ) {
+            Ok(()) => Ok(()),
+            Err(AppendTableCommitmentError::MixedLengthColumns(_)) => {
+                panic!("RecordBatches cannot have columns of mixed length")
+            }
+            Err(AppendTableCommitmentError::AppendColumnCommitments(
+                AppendColumnCommitmentsError::DuplicateIdentifiers(_),
+            )) => {
+                panic!("RecordBatches cannot have duplicate identifiers")
+            }
+            Err(AppendTableCommitmentError::AppendColumnCommitments(
+                AppendColumnCommitmentsError::Mismatch(e),
+            )) => Err(e)?,
+        }
+    }
+    /// Returns a [`TableCommitment`] to the provided arrow [`RecordBatch`].
+    pub fn try_from_record_batch(
+        batch: &RecordBatch,
+        setup: &Setup<C>,
+    ) -> Result<TableCommitment<C>, RecordBatchToColumnsError> {
+        Self::try_from_record_batch_with_offset(batch, 0, setup)
+    }
+
+    /// Returns a [`TableCommitment`] to the provided arrow [`RecordBatch`] with the given row offset.
+    pub fn try_from_record_batch_with_offset(
+        batch: &RecordBatch,
+        offset: usize,
+        setup: &Setup<C>,
+    ) -> Result<TableCommitment<C>, RecordBatchToColumnsError> {
+        match Self::try_from_columns_with_offset(
+            batch_to_columns::<<Decompressed<C> as Commitment>::Scalar>(batch, &Bump::new())?
+                .iter()
+                .map(|(a, b)| (a, b)),
+            offset,
+            setup,
+        ) {
+            Ok(commitment) => Ok(commitment),
+            Err(TableCommitmentFromColumnsError::MixedLengthColumns(_)) => {
+                panic!("RecordBatches cannot have columns of mixed length")
+            }
+            Err(TableCommitmentFromColumnsError::DuplicateIdentifiers(_)) => {
+                panic!("RecordBatches cannot have duplicate identifiers")
+            }
+        }
+    }
+}
+
+fn batch_to_columns<'a, S: Scalar + 'a>(
+    batch: &'a RecordBatch,
+    alloc: &'a Bump,
+) -> Result<Vec<(Identifier, Column<'a, S>)>, RecordBatchToColumnsError> {
+    batch
+        .schema()
+        .fields()
+        .into_iter()
+        .zip(batch.columns())
+        .map(|(field, array)| {
+            let identifier: Identifier = field.name().parse()?;
+            let column: Column<S> = array.to_column(alloc, &(0..array.len()), None)?;
+            Ok((identifier, column))
+        })
+        .collect()
 }
 
 /// Return the number of rows for the provided columns, erroring if they have mixed length.
@@ -356,7 +461,7 @@ mod tests {
     use super::*;
     use crate::{
         base::{database::OwnedColumn, scalar::Curve25519Scalar},
-        owned_table,
+        owned_table, record_batch,
     };
     use curve25519_dalek::RistrettoPoint;
     use indexmap::IndexMap;
@@ -1109,5 +1214,58 @@ mod tests {
             try_negative_low_difference_result,
             Err(TableCommitmentArithmeticError::NegativeRange(_))
         ));
+    }
+
+    #[test]
+    fn we_can_create_and_append_table_commitments_with_record_batchs() {
+        let batch = record_batch!(
+            "a" => [1i64, 2, 3],
+            "b" => ["1", "2", "3"],
+        );
+
+        let b_scals = ["1".into(), "2".into(), "3".into()];
+
+        let columns = [
+            (
+                &"a".parse().unwrap(),
+                &Column::<Curve25519Scalar>::BigInt(&[1, 2, 3]),
+            ),
+            (
+                &"b".parse().unwrap(),
+                &Column::<Curve25519Scalar>::VarChar((&["1", "2", "3"], &b_scals)),
+            ),
+        ];
+
+        let mut expected_commitment =
+            TableCommitment::<RistrettoPoint>::try_from_columns_with_offset(columns, 0, &())
+                .unwrap();
+
+        let mut commitment =
+            TableCommitment::<RistrettoPoint>::try_from_record_batch(&batch, &()).unwrap();
+
+        assert_eq!(commitment, expected_commitment);
+
+        let batch2 = record_batch!(
+            "a" => [4i64, 5, 6],
+            "b" => ["4", "5", "6"],
+        );
+
+        let b_scals2 = ["4".into(), "5".into(), "6".into()];
+
+        let columns2 = [
+            (
+                &"a".parse().unwrap(),
+                &Column::<Curve25519Scalar>::BigInt(&[4, 5, 6]),
+            ),
+            (
+                &"b".parse().unwrap(),
+                &Column::<Curve25519Scalar>::VarChar((&["4", "5", "6"], &b_scals2)),
+            ),
+        ];
+
+        expected_commitment.try_append_rows(columns2, &()).unwrap();
+        commitment.try_append_record_batch(&batch2, &()).unwrap();
+
+        assert_eq!(commitment, expected_commitment);
     }
 }
