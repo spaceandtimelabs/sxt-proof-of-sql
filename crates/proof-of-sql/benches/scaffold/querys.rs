df2fb56b7eba55aa569b635c03b96da16c5fd0d7
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2024-06-26 14:06:16 -0400

    feat: add arithmetic to benches

--- crates/proof-of-sql/benches/scaffold/querys.rs
+++ crates/proof-of-sql/benches/scaffold/querys.rs
@@ -27,6 +27,21 @@ const MULTI_COLUMN_FILTER_COLUMNS: &[(&str, ColumnType, OptionalRandBound)] = &[
     ),
     ("c", ColumnType::VarChar, None),
 ];
+const ARITHMETIC_TITLE: &str = "Arithmetic";
+const ARITHMETIC_SQL: &str = "SELECT a + b as r0, a * b - 2 as r1, c FROM table WHERE a >= b";
+const ARITHMETIC_COLUMNS: &[(&str, ColumnType, OptionalRandBound)] = &[
+    (
+        "a",
+        ColumnType::BigInt,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    (
+        "b",
+        ColumnType::BigInt,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    ("c", ColumnType::VarChar, None),
+];
 
 #[allow(clippy::type_complexity)]
 pub const QUERIES: &[(&str, &str, &[(&str, ColumnType, OptionalRandBound)])] = &[
@@ -40,4 +55,5 @@ pub const QUERIES: &[(&str, &str, &[(&str, ColumnType, OptionalRandBound)])] = &
         MULTI_COLUMN_FILTER_SQL,
         MULTI_COLUMN_FILTER_COLUMNS,
     ),
+    (ARITHMETIC_TITLE, ARITHMETIC_SQL, ARITHMETIC_COLUMNS),
 ];
