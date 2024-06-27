e274dd3f05a8046fabe6cbc66d83804e21b84498
Jay White <JayWhite2357@users.noreply.github.com>
2024-06-27 10:55:10 -0400

    ci: update semantic release to dry run cargo publish

---
+++ ci/publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail
+
+# number to be used to tag the compressed files
+NEW_VERSION=$1
+
+if ! [[ ${NEW_VERSION} =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]
+then
+    echo "Incorrect version format: " $NEW_VERSION
+    exit 1
+fi
+
+# configure rust lib to release
+sed -i 's/version = "*.*.*" # DO NOT CHANGE THIS LINE! This will be automatically updated/version = "'${NEW_VERSION}'"/' Cargo.toml
+sed -i 's/path = "[^"]*"/version = "'${NEW_VERSION}'"/g' Cargo.toml
+
+cargo publish -p proof-of-sql-parser --dry-run
+cargo publish -p proof-of-sql --dry-run
