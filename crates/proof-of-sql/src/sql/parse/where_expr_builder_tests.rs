a4db0de86b80153998b92629e39624ebf0849fde
dustin.ray <dustin.vaughan@spaceandtime.io>
2024-08-07 11:42:23 -0700

    fix: use equal in util

--- crates/proof-of-sql/src/sql/parse/where_expr_builder_tests.rs
+++ crates/proof-of-sql/src/sql/parse/where_expr_builder_tests.rs
@@ -19,6 +19,7 @@ mod tests {
         intermediate_ast::{BinaryOperator, Expression, Literal},
         intermediate_decimal::IntermediateDecimal,
         posql_time::{PoSQLTimeUnit, PoSQLTimeZone, PoSQLTimestamp},
+        utility::equal,
         Identifier, SelectStatement,
     };
     use std::{collections::HashMap, str::FromStr};
@@ -74,7 +75,7 @@ mod tests {
             ),
         );
         column_mapping.insert(
-            Identifier::try_new("timestamp_column").unwrap(),
+            Identifier::try_new("timestamp_second_column").unwrap(),
             ColumnRef::new(
                 "sxt.sxt_tab".parse().unwrap(),
                 Identifier::try_new("timestamp_column").unwrap(),
@@ -82,7 +83,7 @@ mod tests {
             ),
         );
         column_mapping.insert(
-            Identifier::try_new("timestamp_column").unwrap(),
+            Identifier::try_new("timestamp_millisecond_column").unwrap(),
             ColumnRef::new(
                 "sxt.sxt_tab".parse().unwrap(),
                 Identifier::try_new("timestamp_column").unwrap(),
@@ -90,7 +91,7 @@ mod tests {
             ),
         );
         column_mapping.insert(
-            Identifier::try_new("timestamp_column").unwrap(),
+            Identifier::try_new("timestamp__microsecond_column").unwrap(),
             ColumnRef::new(
                 "sxt.sxt_tab".parse().unwrap(),
                 Identifier::try_new("timestamp_column").unwrap(),
@@ -101,7 +102,7 @@ mod tests {
             Identifier::try_new("timestamp_column").unwrap(),
             ColumnRef::new(
                 "sxt.sxt_tab".parse().unwrap(),
-                Identifier::try_new("timestamp_column").unwrap(),
+                Identifier::try_new("timestamp_nanosecond_column").unwrap(),
                 ColumnType::TimestampTZ(PoSQLTimeUnit::Nanosecond, PoSQLTimeZone::Utc),
             ),
         );
@@ -309,49 +310,49 @@ mod tests {
     fn we_can_check_varying_precision_eq_for_timestamp() {
         let column_mapping = get_column_mappings_for_testing();
 
-        let expr_decimal = Expression::Binary {
-            op: BinaryOperator::Equal,
-            left: Box::new(Expression::Column(
+        let expr = equal(
+            Box::new(Expression::Column(
                 Identifier::try_new("timestamp_column").unwrap(),
             )),
-            right: Box::new(Expression::Literal(Literal::Timestamp(
-                PoSQLTimestamp::try_from("1970-01-01T00:00:00.123456789Z").unwrap(),
-            ))),
-        };
-        run_test_case(&column_mapping, expr_decimal);
+            Box::new(proof_of_sql_parser::intermediate_ast::Expression::Literal(
+                Literal::Timestamp(
+                    PoSQLTimestamp::try_from("1970-01-01T00:00:00.123456789Z").unwrap(),
+                ),
+            )),
+        );
+        run_test_case(&column_mapping, *expr);
 
-        let expr_decimal = Expression::Binary {
-            op: BinaryOperator::Equal,
-            left: Box::new(Expression::Column(
+        let expr = equal(
+            Box::new(Expression::Column(
                 Identifier::try_new("timestamp_column").unwrap(),
             )),
-            right: Box::new(Expression::Literal(Literal::Timestamp(
-                PoSQLTimestamp::try_from("1970-01-01T00:00:00.123456Z").unwrap(),
-            ))),
-        };
-        run_test_case(&column_mapping, expr_decimal);
+            Box::new(proof_of_sql_parser::intermediate_ast::Expression::Literal(
+                Literal::Timestamp(
+                    PoSQLTimestamp::try_from("1970-01-01T00:00:00.123456Z").unwrap(),
+                ),
+            )),
+        );
+        run_test_case(&column_mapping, *expr);
 
-        let expr_decimal = Expression::Binary {
-            op: BinaryOperator::Equal,
-            left: Box::new(Expression::Column(
+        let expr = equal(
+            Box::new(Expression::Column(
                 Identifier::try_new("timestamp_column").unwrap(),
             )),
-            right: Box::new(Expression::Literal(Literal::Timestamp(
-                PoSQLTimestamp::try_from("1970-01-01T00:00:00.123Z").unwrap(),
-            ))),
-        };
-        run_test_case(&column_mapping, expr_decimal);
+            Box::new(proof_of_sql_parser::intermediate_ast::Expression::Literal(
+                Literal::Timestamp(PoSQLTimestamp::try_from("1970-01-01T00:00:00.123Z").unwrap()),
+            )),
+        );
+        run_test_case(&column_mapping, *expr);
 
-        let expr_decimal = Expression::Binary {
-            op: BinaryOperator::Equal,
-            left: Box::new(Expression::Column(
+        let expr = equal(
+            Box::new(Expression::Column(
                 Identifier::try_new("timestamp_column").unwrap(),
             )),
-            right: Box::new(Expression::Literal(Literal::Timestamp(
-                PoSQLTimestamp::try_from("1970-01-01T00:00:00Z").unwrap(),
-            ))),
-        };
-        run_test_case(&column_mapping, expr_decimal);
+            Box::new(proof_of_sql_parser::intermediate_ast::Expression::Literal(
+                Literal::Timestamp(PoSQLTimestamp::try_from("1970-01-01T00:00:00Z").unwrap()),
+            )),
+        );
+        run_test_case(&column_mapping, *expr);
     }
 
     #[test]
