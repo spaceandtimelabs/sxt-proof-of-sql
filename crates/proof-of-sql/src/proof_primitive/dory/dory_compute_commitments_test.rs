3c40fc96f665997350fc840595397d80ef7d5a25
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-08-09 11:20:29 -0400

    test: add more mixed commitment tests

--- crates/proof-of-sql/src/proof_primitive/dory/dory_compute_commitments_test.rs
+++ crates/proof-of-sql/src/proof_primitive/dory/dory_compute_commitments_test.rs
@@ -363,10 +363,10 @@ fn test_compute_dory_commitment_with_zero_sigma_and_with_an_offset() {
 }
 
 #[test]
-fn we_can_compute_a_dory_commitment_with_mixed_committable_columns() {
+fn we_can_compute_a_dory_commitment_with_mixed_committable_columns_with_fewer_rows_than_columns() {
     let public_parameters = PublicParameters::rand(5, &mut test_rng());
     let prover_setup = ProverSetup::from(&public_parameters);
-    let setup = DoryProverPublicSetup::new(&prover_setup, 5);
+    let setup = DoryProverPublicSetup::new(&prover_setup, 2);
     let res = compute_dory_commitments(
         &[
             CommittableColumn::BigInt(&[0, 1]),
@@ -385,7 +385,7 @@ fn we_can_compute_a_dory_commitment_with_mixed_committable_columns() {
             CommittableColumn::TimestampTZ(
                 PoSQLTimeUnit::Second,
                 PoSQLTimeZone::Utc,
-                &[17, 18, 19, 20, 21],
+                &[17, 18, 19, 20],
             ),
         ],
         0,
@@ -434,7 +434,260 @@ fn we_can_compute_a_dory_commitment_with_mixed_committable_columns() {
     let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(17)
         + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(18)
         + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(19)
-        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(20)
-        + Pairing::pairing(Gamma_1[4], Gamma_2[0]) * F::from(21);
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(20);
+    assert_eq!(res[9].0, expected);
+}
+
+#[test]
+fn we_can_compute_a_dory_commitment_with_mixed_committable_columns_with_an_offset_and_fewer_rows_than_columns() {
+    let public_parameters = PublicParameters::rand(5, &mut test_rng());
+    let prover_setup = ProverSetup::from(&public_parameters);
+    let setup = DoryProverPublicSetup::new(&prover_setup, 2);
+    let res = compute_dory_commitments(
+        &[
+            CommittableColumn::BigInt(&[0, 1]),
+            CommittableColumn::BigInt(&[2, 3]),
+            CommittableColumn::Int(&[4, 5, 10]),
+            CommittableColumn::SmallInt(&[6, 7]),
+            CommittableColumn::Int128(&[8, 9]),
+            CommittableColumn::Boolean(&[true, true]),
+            CommittableColumn::Decimal75(
+                Precision::new(1).unwrap(),
+                0,
+                vec![[10, 0, 0, 0], [11, 0, 0, 0], [12, 0, 0, 0], [13, 0, 0, 0]],
+            ),
+            CommittableColumn::Scalar(vec![[14, 0, 0, 0], [15, 0, 0, 0]]),
+            CommittableColumn::VarChar(vec![[16, 0, 0, 0]]),
+            CommittableColumn::TimestampTZ(
+                PoSQLTimeUnit::Second,
+                PoSQLTimeZone::Utc,
+                &[17, 18, 19, 20],
+            ),
+        ],
+        2,
+        &setup,
+    );
+    let Gamma_1 = public_parameters.Gamma_1;
+    let Gamma_2 = public_parameters.Gamma_2;
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(0)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(1);
+    assert_eq!(res[0].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(2)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(3);
+    assert_eq!(res[1].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(4)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(5)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(10);
+    assert_eq!(res[2].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(6)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(7);
+    assert_eq!(res[3].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(8)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(9);
+    assert_eq!(res[4].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(true)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(true);
+    assert_eq!(res[5].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(10)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(11)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(12)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(13);
+    assert_eq!(res[6].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(14)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(15);
+    assert_eq!(res[7].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(16);
+    assert_eq!(res[8].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(17)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(18)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(19)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(20);
+    assert_eq!(res[9].0, expected);
+}
+
+#[test]
+fn we_can_compute_a_dory_commitment_with_mixed_committable_columns_with_signed_values() {
+    let public_parameters = PublicParameters::rand(5, &mut test_rng());
+    let prover_setup = ProverSetup::from(&public_parameters);
+    let setup = DoryProverPublicSetup::new(&prover_setup, 2);
+    let res = compute_dory_commitments(
+        &[
+            CommittableColumn::BigInt(&[-2, -1, 0, 1, 2]),
+            CommittableColumn::BigInt(&[-3, -2, 2, 3]),
+            CommittableColumn::Int(&[-6, -5, -4, 4, 5, 6]),
+            CommittableColumn::SmallInt(&[-7, -6, 6, 7]),
+            CommittableColumn::Int128(&[-9, -8, 8, 9]),
+            CommittableColumn::Boolean(&[true, true]),
+            CommittableColumn::Decimal75(
+                Precision::new(1).unwrap(),
+                0,
+                vec![[10, 0, 0, 0], [11, 0, 0, 0], [12, 0, 0, 0], [13, 0, 0, 0]],
+            ),
+            CommittableColumn::Scalar(vec![[14, 0, 0, 0], [15, 0, 0, 0]]),
+            CommittableColumn::VarChar(vec![[16, 0, 0, 0]]),
+            CommittableColumn::TimestampTZ(
+                PoSQLTimeUnit::Second,
+                PoSQLTimeZone::Utc,
+                &[-18, -17, 17, 18],
+            ),
+        ],
+        0,
+        &setup,
+    );
+    let Gamma_1 = public_parameters.Gamma_1;
+    let Gamma_2 = public_parameters.Gamma_2;
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(-2)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(-1)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(0)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(1)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(2);
+    assert_eq!(res[0].0, expected);
+
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(-3)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(-2)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(2)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(3);
+    assert_eq!(res[1].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(-6)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(-5)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(-4)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(4)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(5)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(6);
+    assert_eq!(res[2].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(-7)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(-6)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(6)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(7);
+    assert_eq!(res[3].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(-9)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(-8)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(8)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(9);
+    assert_eq!(res[4].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(true)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(true);
+    assert_eq!(res[5].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(10)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(11)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(12)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(13);
+    assert_eq!(res[6].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(14)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(15);
+    assert_eq!(res[7].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(16);
+    assert_eq!(res[8].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[0]) * F::from(-18)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[0]) * F::from(-17)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[0]) * F::from(17)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[0]) * F::from(18);
+    assert_eq!(res[9].0, expected);
+}
+
+#[test]
+fn we_can_compute_a_dory_commitment_with_mixed_committable_columns_with_an_offset_and_signed_values() {
+    let public_parameters = PublicParameters::rand(5, &mut test_rng());
+    let prover_setup = ProverSetup::from(&public_parameters);
+    let setup = DoryProverPublicSetup::new(&prover_setup, 2);
+    let res = compute_dory_commitments(
+        &[
+            CommittableColumn::BigInt(&[-2, -1, 0, 1, 2]),
+            CommittableColumn::BigInt(&[-3, -2, 2, 3]),
+            CommittableColumn::Int(&[-6, -5, -4, 4, 5, 6]),
+            CommittableColumn::SmallInt(&[-7, -6, 6, 7]),
+            CommittableColumn::Int128(&[-9, -8, 8, 9]),
+            CommittableColumn::Boolean(&[true, true]),
+            CommittableColumn::Decimal75(
+                Precision::new(1).unwrap(),
+                0,
+                vec![[10, 0, 0, 0], [11, 0, 0, 0], [12, 0, 0, 0], [13, 0, 0, 0]],
+            ),
+            CommittableColumn::Scalar(vec![[14, 0, 0, 0], [15, 0, 0, 0]]),
+            CommittableColumn::VarChar(vec![[16, 0, 0, 0]]),
+            CommittableColumn::TimestampTZ(
+                PoSQLTimeUnit::Second,
+                PoSQLTimeZone::Utc,
+                &[-18, -17, 17, 18],
+            ),
+        ],
+        4,
+        &setup,
+    );
+    let Gamma_1 = public_parameters.Gamma_1;
+    let Gamma_2 = public_parameters.Gamma_2;
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(-2)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(-1)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(0)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(1)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[2]) * F::from(2);
+    assert_eq!(res[0].0, expected);
+
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(-3)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(-2)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(2)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(3);
+    assert_eq!(res[1].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(-6)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(-5)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(-4)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(4)
+        + Pairing::pairing(Gamma_1[0], Gamma_2[2]) * F::from(5)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[2]) * F::from(6);
+    assert_eq!(res[2].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(-7)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(-6)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(6)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(7);
+    assert_eq!(res[3].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(-9)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(-8)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(8)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(9);
+    assert_eq!(res[4].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(true)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(true);
+    assert_eq!(res[5].0, expected);
+    
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(10)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(11)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(12)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(13);
+    assert_eq!(res[6].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(14)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(15);
+    assert_eq!(res[7].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(16);
+    assert_eq!(res[8].0, expected);
+
+    let expected: GT = Pairing::pairing(Gamma_1[0], Gamma_2[1]) * F::from(-18)
+        + Pairing::pairing(Gamma_1[1], Gamma_2[1]) * F::from(-17)
+        + Pairing::pairing(Gamma_1[2], Gamma_2[1]) * F::from(17)
+        + Pairing::pairing(Gamma_1[3], Gamma_2[1]) * F::from(18);
     assert_eq!(res[9].0, expected);
 }
