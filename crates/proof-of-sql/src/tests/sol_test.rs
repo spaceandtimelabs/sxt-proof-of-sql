df578bc0ab921a63bf463e91367a58d616a46040
Jay White <JayWhite2357@users.noreply.github.com>
2024-08-30 09:38:16 -0400

    refactor: move solidity files to `sol_src`

--- crates/proof-of-sql/src/tests/sol_test.rs
+++ crates/proof-of-sql/src/tests/sol_test.rs
@@ -5,7 +5,7 @@ use forge_script::ScriptArgs;
 #[tokio::test(flavor = "multi_thread")]
 async fn we_can_run_solidity_script_from_rust() {
     ScriptArgs {
-        path: "./src/tests/TestScript.t.sol".to_string(),
+        path: "./sol_src/tests/TestScript.t.sol".to_string(),
         sig: "rustTestWeCanThrowErrorDependingOnParameter".to_string(),
         args: vec![U256::from(1234).to_string()],
         ..Default::default()
@@ -15,7 +15,7 @@ async fn we_can_run_solidity_script_from_rust() {
     .unwrap();
 
     assert!(ScriptArgs {
-        path: "./src/tests/TestScript.t.sol".to_string(),
+        path: "./sol_src/tests/TestScript.t.sol".to_string(),
         sig: "rustTestWeCanThrowErrorDependingOnParameter".to_string(),
         args: vec![U256::from(0).to_string()],
         ..Default::default()
@@ -26,12 +26,12 @@ async fn we_can_run_solidity_script_from_rust() {
 }
 #[tokio::test(flavor = "multi_thread")]
 async fn we_can_pass_custom_struct_into_solidity_from_rust() {
-    sol!("./src/tests/TestScript.t.sol");
+    sol!("./sol_src/tests/TestScript.t.sol");
     let arg = TestScript::CustomStruct {
         value: U256::from(1234),
     };
     ScriptArgs {
-        path: "./src/tests/TestScript.t.sol".to_string(),
+        path: "./sol_src/tests/TestScript.t.sol".to_string(),
         sig: "rustTestWeCanAcceptCustomStructAsEncodedBytes".to_string(),
         args: vec![Bytes::from(arg.abi_encode()).to_string()],
         ..Default::default()
