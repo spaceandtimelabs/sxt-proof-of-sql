b33f19402edeb029e48871a5199c133fc9604cca
Mehul Mathur <mathur.mehul01@gmail.com>
2024-09-28 19:09:35 +0000

    allow pedantic for lalrpop generated code

--- crates/proof-of-sql-parser/src/lib.rs
+++ crates/proof-of-sql-parser/src/lib.rs
@@ -33,7 +33,7 @@ pub mod resource_id;
 pub use resource_id::ResourceId;
 
 // lalrpop-generated code is not clippy-compliant
-lalrpop_mod!(#[allow(clippy::all, missing_docs, clippy::missing_docs_in_private_items)] pub sql);
+lalrpop_mod!(#[allow(clippy::all, clippy::pedantic, missing_docs, clippy::missing_docs_in_private_items)] pub sql);
 
 /// Implement Deserialize through `FromStr` to avoid invalid identifiers.
 #[macro_export]
