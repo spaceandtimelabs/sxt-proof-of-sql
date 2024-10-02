63b0d5ae1f878daf0b588fb81a985cbc426bd0d6
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-10-02 09:59:30 -0400

    only return untrimmed matrix sizes

--- crates/proof-of-sql/src/proof_primitive/dory/dynamic_dory_structure.rs
+++ crates/proof-of-sql/src/proof_primitive/dory/dynamic_dory_structure.rs
@@ -64,9 +64,11 @@ pub(crate) fn index_from_row_and_column(row: usize, column: usize) -> Option<usi
 }
 
 /// Returns a matrix size, (height, width), that can hold the given number of data points being committed with respect to an offset.
+///
+/// Note: when data_len = 0 and offset = 0, this function returns an empty matrix with size (0, 0).
 #[allow(dead_code)]
 pub(crate) const fn matrix_size(data_len: usize, offset: usize) -> (usize, usize) {
-    if data_len == 0 {
+    if data_len == 0 && offset == 0 {
         return (0, 0);
     }
 
@@ -212,10 +214,9 @@ mod tests {
     #[test]
     fn we_can_produce_the_correct_matrix_size_with_offset() {
         // NOTE: returned tuple is (height, width).
-        for i in 0..100 {
-            assert_eq!(matrix_size(0, i), (0, 0));
-        }
-
+        assert_eq!(matrix_size(0, 0), (0, 0));
+        assert_eq!(matrix_size(0, 1), (1, 1));
+        assert_eq!(matrix_size(0, 2), (2, 2));
         assert_eq!(matrix_size(1, 1), (2, 2));
         assert_eq!(matrix_size(1, 2), (3, 2));
         assert_eq!(matrix_size(1, 3), (3, 2));
