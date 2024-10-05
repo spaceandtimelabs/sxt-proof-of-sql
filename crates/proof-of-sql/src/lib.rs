51e86f78348e773a867d58c4273ec1aa000093b5
Mehul Mathur <mathur.mehul01@gmail.com>
2024-10-05 09:23:59 +0000

    chore: move denied lints to cargo.toml

--- crates/proof-of-sql/src/lib.rs
+++ crates/proof-of-sql/src/lib.rs
@@ -2,20 +2,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::module_name_repetitions)]
-#![deny(
-    clippy::doc_markdown,
-    clippy::match_same_arms,
-    clippy::similar_names,
-    clippy::many_single_char_names,
-    clippy::explicit_iter_loop,
-    clippy::implicit_clone,
-    clippy::uninlined_format_args,
-    clippy::semicolon_if_nothing_returned,
-    clippy::unnested_or_patterns,
-    clippy::unreadable_literal,
-    clippy::must_use_candidate,
-    clippy::range_plus_one
-)]
 
 extern crate alloc;
 
