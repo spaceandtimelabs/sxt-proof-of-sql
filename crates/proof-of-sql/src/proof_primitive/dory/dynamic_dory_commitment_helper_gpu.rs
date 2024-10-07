0548a9a938a3c8b860d57153ce8d1aaf53ad98f0
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-10-07 15:28:17 -0400

    feat: add single_packed_byte_size function to dynamic_dory_commitment_helper_gpu module

--- crates/proof-of-sql/src/proof_primitive/dory/dynamic_dory_commitment_helper_gpu.rs
+++ crates/proof-of-sql/src/proof_primitive/dory/dynamic_dory_commitment_helper_gpu.rs
@@ -5,6 +5,23 @@ use super::{
 use crate::base::commitment::CommittableColumn;
 use alloc::{vec, vec::Vec};
 
+const BYTE_SIZE: u32 = 8;
+
+/// Finds the single packed byte size of all committable columns.
+///
+/// # Arguments
+///
+/// * `committable_columns` - A reference to the committable columns.
+///
+/// # Returns
+///
+/// The single packed byte size of all committable columns.
+fn single_packed_byte_size(committable_columns: &[CommittableColumn]) -> usize {
+    committable_columns
+        .iter()
+        .fold(0, |acc, x| acc + x.column_type().byte_size())
+}
+
 #[tracing::instrument(name = "compute_dory_commitment_impl (cpu)", level = "debug", skip_all)]
 /// # Panics
 ///
@@ -73,4 +90,53 @@ pub(super) fn compute_dynamic_dory_commitments(
         .iter()
         .map(|column| compute_dory_commitment(column, offset, setup))
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::base::math::decimal::Precision;
+    use proof_of_sql_parser::posql_time::{PoSQLTimeUnit, PoSQLTimeZone};
+
+    #[test]
+    fn we_can_get_a_single_packed_bit_size() {
+        let committable_columns = [
+            CommittableColumn::BigInt(&[1]),
+            CommittableColumn::BigInt(&[1, 2, 3]),
+        ];
+
+        let single_packed_byte_size = single_packed_byte_size(&committable_columns);
+        let full_byte_size = ((64 + 64) / BYTE_SIZE) as usize;
+        assert_eq!(single_packed_byte_size, full_byte_size);
+    }
+
+    #[test]
+    fn we_can_get_a_single_packed_bit_size_with_mixed_columns() {
+        let committable_columns = [
+            CommittableColumn::Boolean(&[true, false]),
+            CommittableColumn::TinyInt(&[1, 2, 3]),
+            CommittableColumn::SmallInt(&[1]),
+            CommittableColumn::Int(&[1, 2]),
+            CommittableColumn::Int128(&[1, 2, 3, 4]),
+            CommittableColumn::BigInt(&[1, 2, 3]),
+            CommittableColumn::Decimal75(
+                Precision::new(1).unwrap(),
+                0,
+                vec![
+                    [1, 0, 0, 0],
+                    [2, 0, 0, 0],
+                    [3, 0, 0, 0],
+                    [4, 0, 0, 0],
+                    [5, 0, 0, 0],
+                ],
+            ),
+            CommittableColumn::Scalar(vec![[1, 0, 0, 0], [2, 0, 0, 0], [3, 0, 0, 0], [4, 0, 0, 0]]),
+            CommittableColumn::VarChar(vec![[1, 0, 0, 0], [2, 0, 0, 0], [3, 0, 0, 0]]),
+            CommittableColumn::TimestampTZ(PoSQLTimeUnit::Second, PoSQLTimeZone::Utc, &[1]),
+        ];
+
+        let single_packed_byte_size = single_packed_byte_size(&committable_columns);
+        let full_byte_size = (8 + 16 + 32 + 64 + 128 + 256 + 256 + 256 + 8 + 64) / 8;
+        assert_eq!(single_packed_byte_size, full_byte_size);
+    }
 }
