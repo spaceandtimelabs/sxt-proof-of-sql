7c01d6c438327f984b89be1fae18e3c0d907c7a3
Tarek <tareknaser360@gmail.com>
2024-10-08 21:58:06 +0300

    meta: cargo fmt
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- crates/proof-of-sql/src/base/database/owned_table_utility.rs
+++ crates/proof-of-sql/src/base/database/owned_table_utility.rs
@@ -15,8 +15,7 @@
 //! ```
 use super::{OwnedColumn, OwnedTable};
 use crate::base::scalar::Scalar;
-use alloc::string::String;
-use alloc::vec::Vec;
+use alloc::{string::String, vec::Vec};
 use core::ops::Deref;
 use proof_of_sql_parser::{
     posql_time::{PoSQLTimeUnit, PoSQLTimeZone},
