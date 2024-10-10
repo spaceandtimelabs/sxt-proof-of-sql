bb6599a679d55e2ce0344179cff2b33bfefd85bf
stuarttimwhite <stuart.white@spaceandtime.io>
2024-10-10 16:40:39 +0000

    add dynamic dory

--- crates/proof-of-sql/src/utils/mod.rs
+++ crates/proof-of-sql/src/utils/mod.rs
@@ -2,6 +2,7 @@
 /// Parse DDLs and find bigdecimal columns
 pub mod parse;
 /// Utility for reading a parquet file and writing to a blob which represents a `TableCommitment`
+#[cfg(test)]
 pub mod parquet_to_commitment_blob;
 #[cfg(test)]
 mod parquet_to_commitment_blob_integration_tests;
