67e791e6576f3b2ad5426f3048c76dd393bfabfb
stuarttimwhite <stuart.white@spaceandtime.io>
2024-10-14 16:23:34 +0000

    read file at a time

--- crates/proof-of-sql/src/utils/parquet_to_commitment_blob.rs
+++ crates/proof-of-sql/src/utils/parquet_to_commitment_blob.rs
@@ -5,8 +5,8 @@ use crate::{
     },
 };
 use arrow::{
-    array::RecordBatch,
-    compute::{concat_batches, sort_to_indices, take},
+    array::{Int32Array, RecordBatch},
+    compute::concat_batches,
     error::ArrowError,
 };
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
@@ -16,16 +16,22 @@ use rand_chacha::ChaCha20Rng;
 use serde::{Deserialize, Serialize};
 use std::{fs::File, io::Write};
 
+pub static PARQUET_FILE_PROOF_ORDER_COLUMN: &str = "META_ROW_NUMBER";
+
 /// Performs the following:
-/// Reads a parquet file into a `RecordBatch`,
-/// Calculates the `TableCommitment` for the `RecordBatch` using multiple commitment strategies,
-/// Serializes the commitment to a blob, which is saved in the same directory as the original parquet file
+/// Reads a collection of parquet files which in aggregate represent a single table of data,
+/// Calculates the `TableCommitment` for the table using multiple commitment strategies,
+/// Serializes each commitment to a blob, which is saved in the same directory as the original parquet file
 ///
 /// # Panics
 ///
-/// Panics when fails any part of the process
+/// Panics when any part of the process fails
 pub fn read_parquet_file_to_commitment_as_blob(paths: Vec<&str>, output_path_prefix: &str) {
-    let unsorted_record_batches_with_unmodified_schema: Vec<RecordBatch> = paths
+    let mut offset: usize = 0;
+    let commitments: Vec<(
+        TableCommitment<DoryCommitment>,
+        TableCommitment<DynamicDoryCommitment>,
+    )> = paths
         .iter()
         .map(|path| {
             let file = File::open(path).unwrap();
@@ -39,72 +45,81 @@ pub fn read_parquet_file_to_commitment_as_blob(paths: Vec<&str>, output_path_pre
                 .map(|record_batch_result| record_batch_result.unwrap())
                 .collect();
             let schema = record_batches.first().unwrap().schema();
-            concat_batches(&schema, &record_batches).unwrap()
+            let mut record_batch = concat_batches(&schema, &record_batches).unwrap();
+            let meta_row_number_column = record_batch
+                .column_by_name(PARQUET_FILE_PROOF_ORDER_COLUMN)
+                .unwrap()
+                .as_any()
+                .downcast_ref::<Int32Array>()
+                .unwrap();
+            let length = meta_row_number_column.len();
+            let new_offset = offset + length;
+            let range = ((offset + 1) as i32)..((new_offset + 1) as i32);
+            assert_eq!(
+                meta_row_number_column,
+                &Int32Array::from(range.collect::<Vec<_>>())
+            );
+            record_batch.remove_column(schema.index_of(PARQUET_FILE_PROOF_ORDER_COLUMN).unwrap());
+            let setup_seed = "spaceandtime".to_string();
+            let mut rng = {
+                // Convert the seed string to bytes and create a seeded RNG
+                let seed_bytes = setup_seed
+                    .bytes()
+                    .chain(std::iter::repeat(0u8))
+                    .take(32)
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .expect("collection is guaranteed to contain 32 elements");
+                ChaCha20Rng::from_seed(seed_bytes) // Seed ChaChaRng
+            };
+            let public_parameters = PublicParameters::rand(12, &mut rng);
+            let prover_setup = ProverSetup::from(&public_parameters);
+            let dory_prover_setup = DoryProverPublicSetup::new(&prover_setup, 20);
+            let dory_commitment =
+                TableCommitment::<DoryCommitment>::try_from_record_batch_with_offset(
+                    &record_batch,
+                    offset,
+                    &dory_prover_setup,
+                )
+                .unwrap();
+            let dynamic_dory_commitment =
+                TableCommitment::<DynamicDoryCommitment>::try_from_record_batch_with_offset(
+                    &record_batch,
+                    offset,
+                    &&prover_setup,
+                )
+                .unwrap();
+            offset = new_offset;
+            (dory_commitment, dynamic_dory_commitment)
         })
         .collect();
-    let schema = unsorted_record_batches_with_unmodified_schema
-        .first()
-        .unwrap()
-        .schema();
-    let unsorted_record_batch_with_unmodified_schema =
-        concat_batches(&schema, &unsorted_record_batches_with_unmodified_schema).unwrap();
-    let indices = sort_to_indices(
-        unsorted_record_batch_with_unmodified_schema
-            .column_by_name("SXTMETA_ROW_NUMBER")
-            .unwrap(),
-        None,
-        None,
-    )
-    .unwrap();
-    let index = schema.index_of("SXTMETA_ROW_NUMBER").unwrap();
-    let columns = unsorted_record_batch_with_unmodified_schema
-        .columns()
-        .iter()
-        .map(|c| take(c, &indices, None).unwrap())
-        .collect();
-    let mut record_batch = RecordBatch::try_new(
-        unsorted_record_batch_with_unmodified_schema.schema(),
-        columns,
-    )
-    .unwrap();
-    record_batch.remove_column(index);
-
-    let setup_seed = "spaceandtime".to_string();
-    let mut rng = {
-        // Convert the seed string to bytes and create a seeded RNG
-        let seed_bytes = setup_seed
-            .bytes()
-            .chain(std::iter::repeat(0u8))
-            .take(32)
-            .collect::<Vec<_>>()
-            .try_into()
-            .expect("collection is guaranteed to contain 32 elements");
-        ChaCha20Rng::from_seed(seed_bytes) // Seed ChaChaRng
-    };
-    let public_parameters = PublicParameters::rand(20, &mut rng);
-    let prover_setup = ProverSetup::from(&public_parameters);
-    let dory_prover_setup = DoryProverPublicSetup::new(&prover_setup, 12);
-    write_record_batch_to_blob::<DoryCommitment>(
-        &record_batch,
-        dory_prover_setup,
-        format!("{output_path_prefix}_dory_commitment"),
-    );
-    write_record_batch_to_blob::<DynamicDoryCommitment>(
-        &record_batch,
-        &prover_setup,
-        format!("{output_path_prefix}_dynamic_dory_commitment"),
+    let unzipped = commitments.into_iter().unzip();
+    aggregate_commitments_to_blob(unzipped.0, format!("{output_path_prefix}-dory-commitment"));
+    aggregate_commitments_to_blob(
+        unzipped.1,
+        format!("{output_path_prefix}-dynamic-dory-commitment"),
     );
 }
 
 /// # Panics
 ///
-/// Panics when fails any part of the process
-fn write_record_batch_to_blob<C: Commitment + Serialize + for<'a> Deserialize<'a>>(
-    record_batch: &RecordBatch,
-    setup: C::PublicSetup<'_>,
+/// Panics when any part of the process fails
+fn aggregate_commitments_to_blob<C: Commitment + Serialize + for<'a> Deserialize<'a>>(
+    commitments: Vec<TableCommitment<C>>,
     output_file_base: String,
 ) {
-    let commitment = TableCommitment::<C>::try_from_record_batch(record_batch, &setup).unwrap();
+    let commitment = commitments
+        .into_iter()
+        .fold(
+            None,
+            |aggregate_commitment: Option<TableCommitment<C>>, next_commitment| {
+                match aggregate_commitment {
+                    Some(agg) => Some(agg.try_add(next_commitment).unwrap()),
+                    None => Some(next_commitment),
+                }
+            },
+        )
+        .unwrap();
     let bytes: Vec<u8> = to_allocvec(&commitment).unwrap();
     let path_extension = "txt";
     let mut output_file = File::create(format!("{output_file_base}.{path_extension}")).unwrap();
