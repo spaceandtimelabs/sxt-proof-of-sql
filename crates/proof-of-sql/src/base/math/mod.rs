0fd1e5b388c583f7ace2b7b02cad993a1a98a4f2
Jay White <JayWhite2357@users.noreply.github.com>
2024-10-17 13:51:28 -0400

    refactor: rename `decimal_tests` to `precision_test`

--- crates/proof-of-sql/src/base/math/mod.rs
+++ crates/proof-of-sql/src/base/math/mod.rs
@@ -1,8 +1,6 @@
 //! This module defines math utilities used in Proof of SQL.
 /// Handles parsing between decimal tokens received from the lexer into native `Decimal75` Proof of SQL type.
 pub mod decimal;
-#[cfg(test)]
-mod decimal_tests;
 mod log;
 pub(crate) use log::log2_up;
 /// TODO: add docs
@@ -13,3 +11,5 @@ pub(crate) use big_decimal_ext::BigDecimalExt;
 
 mod precision;
 pub use precision::{InvalidPrecisionError, Precision};
+#[cfg(test)]
+mod precision_test;
