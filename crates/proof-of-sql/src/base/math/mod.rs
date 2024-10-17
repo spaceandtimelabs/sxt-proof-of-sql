10e07e210e24793597f4f1cab001104f7d5343fc
Jay White <JayWhite2357@users.noreply.github.com>
2024-10-17 13:56:00 -0400

    refactor: rename decimal to big_decimal_ext_test

--- crates/proof-of-sql/src/base/math/mod.rs
+++ crates/proof-of-sql/src/base/math/mod.rs
@@ -1,6 +1,6 @@
 //! This module defines math utilities used in Proof of SQL.
 /// Handles parsing between decimal tokens received from the lexer into native `Decimal75` Proof of SQL type.
-pub mod decimal;
+pub mod big_decimal_ext_test;
 mod log;
 pub(crate) use log::log2_up;
 /// TODO: add docs
