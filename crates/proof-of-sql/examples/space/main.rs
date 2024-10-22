3df1fa55dc8c3e2c1545d0923ad804bcb908be83
JayWhite2357 <JayWhite2357@users.noreply.github.com>
2024-10-21 22:55:38 -0400

    feat: load csv file in `space` example

--- crates/proof-of-sql/examples/space/main.rs
+++ crates/proof-of-sql/examples/space/main.rs
@@ -5,4 +5,20 @@
 // https://www.kaggle.com/datasets/kaushiksinghrawat/humans-to-have-visited-space
 // under the Apache 2.0 license.
 
-fn main() {}
+use arrow::datatypes::SchemaRef;
+use arrow_csv::{infer_schema_from_files, ReaderBuilder};
+use std::fs::File;
+
+fn main() {
+    let filename = "./crates/proof-of-sql/examples/space/space_travellers.csv";
+    let space_travellers_batch = ReaderBuilder::new(SchemaRef::new(
+        infer_schema_from_files(&[filename.to_string()], b',', None, true).unwrap(),
+    ))
+    .with_header(true)
+    .build(File::open(filename).unwrap())
+    .unwrap()
+    .next()
+    .unwrap()
+    .unwrap();
+    println!("{space_travellers_batch:?}");
+}
