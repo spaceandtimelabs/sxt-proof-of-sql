392ee0c814dbf97e5ec927f83bbc4effaa318b3c
David Montemayor <david.montemayor@spaceandtime.io>
2024-10-22 15:07:31 -0700

    feat: load csv file in `census` example

--- crates/proof-of-sql/examples/census/main.rs
+++ crates/proof-of-sql/examples/census/main.rs
@@ -3,4 +3,20 @@
 
 // Note: the census-income.csv was obtained from
 // https://github.com/domoritz/vis-examples/blob/master/data/census-income.csv
-fn main() {}
+use arrow::datatypes::SchemaRef;
+use arrow_csv::{infer_schema_from_files, ReaderBuilder};
+use std::fs::File;
+
+fn main() {
+    let filename = "./crates/proof-of-sql/examples/census/census-income.csv";
+    let census_income_batch = ReaderBuilder::new(SchemaRef::new(
+        infer_schema_from_files(&[filename.to_string()], b',', None, true).unwrap(),
+    ))
+    .with_header(true)
+    .build(File::open(filename).unwrap())
+    .unwrap()
+    .next()
+    .unwrap()
+    .unwrap();
+    println!("{census_income_batch:?}");
+}
