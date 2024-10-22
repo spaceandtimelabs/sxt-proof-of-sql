7d0e0fb4b3da5f29195e391b74e15ff6163cc0a7
David Montemayor <david.montemayor@spaceandtime.io>
2024-10-22 14:42:57 -0700

    feat: add census-income.csv dataset for census example

--- crates/proof-of-sql/examples/census/main.rs
+++ crates/proof-of-sql/examples/census/main.rs
@@ -1,3 +1,6 @@
 //! Example to use Proof of SQL with census datasets
 //! To run, use `cargo run --example census`.
+
+// Note: the census-income.csv was obtained from
+// https://github.com/domoritz/vis-examples/blob/master/data/census-income.csv
 fn main() {}
