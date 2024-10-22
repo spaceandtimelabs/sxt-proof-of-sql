96a0529f41caed5a61db07e7975a1a0ca33c1982
JayWhite2357 <JayWhite2357@users.noreply.github.com>
2024-10-21 22:55:28 -0400

    feat: add space_travellers.csv dataset for space example

--- crates/proof-of-sql/examples/space/main.rs
+++ crates/proof-of-sql/examples/space/main.rs
@@ -1,3 +1,8 @@
 //! This is an non-interactive example of using Proof of SQL with some space related datasets.
 //! To run this, use `cargo run --example space`.
+
+// Note: the space_travellers.csv file was obtained from
+// https://www.kaggle.com/datasets/kaushiksinghrawat/humans-to-have-visited-space
+// under the Apache 2.0 license.
+
 fn main() {}
