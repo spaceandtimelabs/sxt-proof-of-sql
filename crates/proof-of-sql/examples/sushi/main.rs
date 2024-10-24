d68dce4de202c80689ba8f7c9dcfc48f0a49c995
aw-sxt <alex.williams@spaceandtime.io>
2024-10-24 18:36:32 -0500

    feat: load dataset for sushi example

--- crates/proof-of-sql/examples/sushi/main.rs
+++ crates/proof-of-sql/examples/sushi/main.rs
@@ -3,4 +3,20 @@
 
 //! NOTE: If this doesn't work because you do not have the appropriate GPU drivers installed,
 //! you can run `cargo run --release --example space --no-default-features --features="arrow cpu-perf"` instead. It will be slower for proof generation.
-fn main() {}
+use arrow::datatypes::SchemaRef;
+use arrow_csv::{infer_schema_from_files, ReaderBuilder};
+use std::fs::File;
+
+fn main() {
+	let filename = "./crates/proof-of-sql/examples/sushi/fish.csv";
+    let fish_batch = ReaderBuilder::new(SchemaRef::new(
+        infer_schema_from_files(&[filename.to_string()], b',', None, true).unwrap(),
+    ))
+		.with_header(true)
+		.build(File::open(filename).unwrap())
+		.unwrap()
+		.next()
+		.unwrap()
+		.unwrap();
+    println!("{fish_batch:?}");
+}
