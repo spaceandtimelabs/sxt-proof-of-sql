c9956ee19dfcee5910f7653c6412e9bee8636018
Johnny <132952411+jd-sxt@users.noreply.github.com>
2024-10-23 21:04:26 -0500

    feat: load csv file in example

--- crates/proof-of-sql/examples/avocado-prices/main.rs
+++ crates/proof-of-sql/examples/avocado-prices/main.rs
@@ -1,3 +1,19 @@
 //! Example to use Proof of SQL with datasets
 //! To run, use `cargo run --example avocado-prices`.
-fn main() {}
+use arrow::datatypes::SchemaRef;
+use arrow_csv::{infer_schema_from_files, ReaderBuilder};
+use std::fs::File;
+
+fn main() {
+    let filename = "./crates/proof-of-sql/examples/avocado-prices/avocado-prices.csv";
+    let data_batch = ReaderBuilder::new(SchemaRef::new(
+        infer_schema_from_files(&[filename.to_string()], b',', None, true).unwrap(),
+    ))
+    .with_header(true)
+    .build(File::open(filename).unwrap())
+    .unwrap()
+    .next()
+    .unwrap()
+    .unwrap();
+    println!("{data_batch:?}");
+}
