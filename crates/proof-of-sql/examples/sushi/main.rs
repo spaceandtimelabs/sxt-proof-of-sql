69216e40c6663410392f601791a2fb627158a55c
aw-sxt <alex.williams@spaceandtime.io>
2024-10-24 19:59:32 -0500

    feat: add sushi example stub

---
+++ crates/proof-of-sql/examples/sushi/main.rs
@@ -0,0 +1,6 @@
+//! This is an non-interactive example of using Proof of SQL with some sushi related datasets.
+//! To run this, use `cargo run --example sushi`.
+
+//! NOTE: If this doesn't work because you do not have the appropriate GPU drivers installed,
+//! you can run `cargo run --release --example space --no-default-features --features="arrow cpu-perf"` instead. It will be slower for proof generation.
+fn main() {}
