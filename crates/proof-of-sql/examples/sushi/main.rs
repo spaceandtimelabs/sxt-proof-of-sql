37235f4edded8a2d7372f4b8c51eae804c86bc76
aw-sxt <alex.williams@spaceandtime.io>
2024-10-24 19:51:26 -0500

    ci: add sushi example to lint-and-test workflow

--- crates/proof-of-sql/examples/sushi/main.rs
+++ crates/proof-of-sql/examples/sushi/main.rs
@@ -2,7 +2,7 @@
 //! To run this, use `cargo run --example sushi`.
 
 //! NOTE: If this doesn't work because you do not have the appropriate GPU drivers installed,
-//! you can run `cargo run --release --example space --no-default-features --features="arrow cpu-perf"` instead. It will be slower for proof generation.
+//! you can run `cargo run --release --example sushi --no-default-features --features="arrow cpu-perf"` instead. It will be slower for proof generation.
 use arrow::datatypes::SchemaRef;
 use arrow_csv::{infer_schema_from_files, ReaderBuilder};
 use proof_of_sql::{
