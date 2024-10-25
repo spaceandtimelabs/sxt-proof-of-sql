5e760cdb8530f91a9dd060765a7f3c9139247798
Wael Almattar <waelsy123@gmail.com>
2024-10-25 14:19:48 +0200

    feat: define dory setup constants

--- crates/proof-of-sql/examples/stocks/main.rs
+++ crates/proof-of-sql/examples/stocks/main.rs
@@ -1,2 +1,19 @@
-use rand::{rngs::StdRng, SeedableRng};
-use std::{fs::File, time::Instant};
+use arrow::datatypes::SchemaRef;
+use arrow_csv::{infer_schema_from_files, ReaderBuilder};
+use proof_of_sql::{
+    base::database::{
+        arrow_schema_utility::get_posql_compatible_schema, OwnedTable, OwnedTableTestAccessor,
+        TestAccessor,
+    },
+    proof_primitive::dory::{
+        DynamicDoryCommitment, DynamicDoryEvaluationProof, ProverSetup, PublicParameters,
+        VerifierSetup,
+    },
+    sql::{parse::QueryExpr, postprocessing::apply_postprocessing_steps, proof::QueryProof},
+};
+
+// We generate the public parameters and the setups used by the prover and verifier for the Dory PCS.
+// The `max_nu` should be set such that the maximum table size is less than `2^(2*max_nu-1)`.
+const DORY_SETUP_MAX_NU: usize = 8;
+// This should be a "nothing-up-my-sleeve" phrase or number.
+const DORY_SEED: [u8; 32] = *b"f9d2e8c1b7a654309cfe81d2b7a3c940";
