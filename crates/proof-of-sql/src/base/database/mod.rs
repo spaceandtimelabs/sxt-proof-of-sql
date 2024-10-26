ddcd1a48b739655a68aa77cb05c3f86b8a3b070f
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-10-26 15:24:55 -0400

    chore: remove `test_accessor_utility`

--- crates/proof-of-sql/src/base/database/mod.rs
+++ crates/proof-of-sql/src/base/database/mod.rs
@@ -34,11 +34,6 @@ pub use record_batch_utility::ToArrow;
 #[cfg(feature = "arrow")]
 pub mod arrow_schema_utility;
 
-#[cfg(all(test, feature = "arrow", feature = "test"))]
-mod test_accessor_utility;
-#[cfg(all(test, feature = "arrow", feature = "test"))]
-pub use test_accessor_utility::{make_random_test_accessor_data, RandomTestAccessorDescriptor};
-
 mod owned_column;
 pub(crate) use owned_column::compare_indexes_by_owned_columns_with_direction;
 pub use owned_column::OwnedColumn;
