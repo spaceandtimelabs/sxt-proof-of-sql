679dc5febff6b4af99ef41d4137c45cb666ea77b
Nate Holiday <HolidaySxT@users.noreply.github.com>
2024-10-31 18:44:46 -0400

    refactor: remove `"arrow"` feature gate from `table_commitment` tests

--- crates/proof-of-sql/src/base/commitment/table_commitment.rs
+++ crates/proof-of-sql/src/base/commitment/table_commitment.rs
@@ -377,7 +377,7 @@ fn num_rows_of_columns<'a>(
     Ok(num_rows)
 }
 
-#[cfg(all(test, feature = "arrow", feature = "blitzar"))]
+#[cfg(all(test, feature = "blitzar"))]
 mod tests {
     use super::*;
     use crate::base::{
