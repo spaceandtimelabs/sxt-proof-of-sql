ada20dfec325e049ef2c2f36fd8e20c55ae67da0
Jay White <JayWhite2357@users.noreply.github.com>
2024-11-09 15:41:25 -0500

    refactor: add helper implementations to `SumcheckSubpolynomial`

--- crates/proof-of-sql/src/sql/proof/sumcheck_subpolynomial.rs
+++ crates/proof-of-sql/src/sql/proof/sumcheck_subpolynomial.rs
@@ -3,6 +3,7 @@ use crate::base::{polynomial::MultilinearExtension, scalar::Scalar};
 use alloc::{boxed::Box, vec::Vec};
 
 /// The type of a sumcheck subpolynomial
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
 pub enum SumcheckSubpolynomialType {
     /// The subpolynomial should be zero at every entry/row
     Identity,
@@ -37,6 +38,27 @@ impl<'a, S: Scalar> SumcheckSubpolynomial<'a, S> {
             terms,
             subpolynomial_type,
         }
+    }
+
+    pub fn subpolynomial_type(&self) -> SumcheckSubpolynomialType {
+        self.subpolynomial_type
+    }
+
+    /// Return an iterator over the Subpolynomialterms returning a tuple with the type, coefficient, and multiplicands.
+    /// The multiplier parameters is multiplied by every coefficient.
+    pub fn iter_mul_by(
+        &self,
+        multiplier: S,
+    ) -> impl Iterator<
+        Item = (
+            SumcheckSubpolynomialType,
+            S,
+            &Vec<Box<dyn MultilinearExtension<S> + 'a>>,
+        ),
+    > {
+        self.terms.iter().map(move |(coeff, multiplicands)| {
+            (self.subpolynomial_type, multiplier * *coeff, multiplicands)
+        })
     }
 
     /// Combine the subpolynomial into a combined composite polynomial
