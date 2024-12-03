2c54a261cfbc091c8738118b7848d48e188bc363
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-12-03 11:25:39 -0500

    bench: add large column set query to the jaeger benches

--- crates/proof-of-sql/benches/scaffold/querys.rs
+++ crates/proof-of-sql/benches/scaffold/querys.rs
@@ -86,6 +86,38 @@ const BOOLEAN_FILTER_COLUMNS: &[(&str, ColumnType, OptionalRandBound)] = &[
     ("b", ColumnType::VarChar, None),
     ("c", ColumnType::Boolean, None),
 ];
+const LARGE_COLUMN_SET_TITLE: &str = "Large Column Set";
+const LARGE_COLUMN_SET_SQL: &str = "SELECT * FROM table WHERE b = d";
+const LARGE_COLUMN_SET_COLUMNS: &[(&str, ColumnType, OptionalRandBound)] = &[
+    ("a", ColumnType::Boolean, None),
+    (
+        "b",
+        ColumnType::TinyInt,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    (
+        "c",
+        ColumnType::SmallInt,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    (
+        "d",
+        ColumnType::Int,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    (
+        "e",
+        ColumnType::BigInt,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    (
+        "f",
+        ColumnType::Int128,
+        Some(|size| (size / 10).max(10) as i64),
+    ),
+    ("g", ColumnType::VarChar, None),
+    ("h", ColumnType::Scalar, None),
+];
 
 #[allow(clippy::type_complexity)]
 pub const QUERIES: &[(&str, &str, &[(&str, ColumnType, OptionalRandBound)])] = &[
@@ -106,5 +138,10 @@ pub const QUERIES: &[(&str, &str, &[(&str, ColumnType, OptionalRandBound)])] = &
         BOOLEAN_FILTER_TITLE,
         BOOLEAN_FILTER_SQL,
         BOOLEAN_FILTER_COLUMNS,
+    ),
+    (
+        LARGE_COLUMN_SET_TITLE,
+        LARGE_COLUMN_SET_SQL,
+        LARGE_COLUMN_SET_COLUMNS,
     ),
 ];
