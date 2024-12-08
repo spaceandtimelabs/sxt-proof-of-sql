72746cfdae254924e0ffd67cd73c31c6fd51f29f
Jay White <JayWhite2357@users.noreply.github.com>
2024-12-07 22:58:59 -0500

    feat: add `SumcheckSubpolynomial::subpolynomial_type`

--- crates/proof-of-sql/src/sql/proof/sumcheck_subpolynomial.rs
+++ crates/proof-of-sql/src/sql/proof/sumcheck_subpolynomial.rs
@@ -56,4 +56,9 @@ impl<'a, S: Scalar> SumcheckSubpolynomial<'a, S> {
             }
         }
     }
+
+    #[allow(dead_code)]
+    pub fn subpolynomial_type(&self) -> SumcheckSubpolynomialType {
+        self.subpolynomial_type
+    }
 }
