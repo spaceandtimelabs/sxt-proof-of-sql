6d4c52c9187642ab7da4e783c21f18700e092b20
Jay White <JayWhite2357@users.noreply.github.com>
2024-12-07 23:12:17 -0500

    chore: add ProverState::new

--- crates/proof-of-sql/src/proof_primitive/sumcheck/prover_state.rs
+++ crates/proof-of-sql/src/proof_primitive/sumcheck/prover_state.rs
@@ -19,6 +19,21 @@ pub struct ProverState<S: Scalar> {
 }
 
 impl<S: Scalar> ProverState<S> {
+    pub fn new(
+        list_of_products: Vec<(S, Vec<usize>)>,
+        flattened_ml_extensions: Vec<Vec<S>>,
+        num_vars: usize,
+        max_multiplicands: usize,
+    ) -> Self {
+        ProverState {
+            list_of_products,
+            flattened_ml_extensions,
+            num_vars,
+            max_multiplicands,
+            round: 0,
+        }
+    }
+
     #[tracing::instrument(name = "ProverState::create", level = "debug", skip_all)]
     pub fn create(polynomial: &CompositePolynomial<S>) -> Self {
         assert!(
@@ -33,12 +48,11 @@ impl<S: Scalar> ProverState<S> {
             .map(|x| x.as_ref().clone())
             .collect();
 
-        ProverState {
-            list_of_products: polynomial.products.clone(),
+        ProverState::new(
+            polynomial.products.clone(),
             flattened_ml_extensions,
-            num_vars: polynomial.num_variables,
-            max_multiplicands: polynomial.max_multiplicands,
-            round: 0,
-        }
+            polynomial.num_variables,
+            polynomial.max_multiplicands,
+        )
     }
 }
