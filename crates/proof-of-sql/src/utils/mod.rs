c4449989edae816e2662195057674a6250ebdbd2
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-12-09 10:28:01 -0500

    refactor: move log_memory_usage to new log module in utils

--- crates/proof-of-sql/src/utils/mod.rs
+++ crates/proof-of-sql/src/utils/mod.rs
@@ -1,3 +1,6 @@
 //! This module contains utilities for working with the library
 /// Parse DDLs and find bigdecimal columns
 pub mod parse;
+
+/// This module provides logging utilities for the library, including functions to log system memory usage.
+pub mod log;
