d8e2b56331b2dc12e598ca0ef1ff8a76b7f18970
stuarttimwhite <stuart.white@spaceandtime.io>
2025-01-09 10:18:05 -0500

    test: add valid hyrax configuration using ristretto point and curve 25519

--- crates/proof-of-sql/src/proof_primitive/hyrax/mod.rs
+++ crates/proof-of-sql/src/proof_primitive/hyrax/mod.rs
@@ -1,2 +1,5 @@
 /// The hyrax scheme in generic form.
 pub(super) mod base;
+/// A configuration with ristretto point as the group and curve 25519 scalar. For now, this will only be used for testing purposes.
+#[cfg(test)]
+pub mod ristretto;
