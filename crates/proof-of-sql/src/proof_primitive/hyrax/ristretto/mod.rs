384c4f3f067d19935c501173aa11dca3e55ae235
stuarttimwhite <stuart.white@spaceandtime.io>
2025-01-09 10:19:04 -0500

    test: test ristretto hyrax configuration

--- crates/proof-of-sql/src/proof_primitive/hyrax/ristretto/mod.rs
+++ crates/proof-of-sql/src/proof_primitive/hyrax/ristretto/mod.rs
@@ -1,2 +1,10 @@
+/// A hyrax commitment using the ristretto configuration
+pub mod ristretto_hyrax_commitment;
+/// A hyrax commitment evaluation proof using the ristretto configuration
+pub mod ristretto_hyrax_commitment_evaluation_proof;
 /// A hyrax configuration using `RistrettoPoint` and `Curve25519Scalar`
 pub mod ristretto_hyrax_configuration;
+/// A hyrax public setup using `RistrettoPoint`, which is the group of the ristretto configuration
+pub mod ristretto_hyrax_public_setup;
+/// A hyrax `Curve25519Scalar` wrapper which is compatible with the ristretto configuration
+pub mod ristretto_hyrax_scalar;
