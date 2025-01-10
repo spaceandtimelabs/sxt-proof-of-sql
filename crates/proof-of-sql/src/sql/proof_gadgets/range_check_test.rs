fdf6a371bc0f6265fc15a7e07a4e320fe13c7cf8
dustin.ray <dustin.vaughan@spaceandtime.io>
2025-01-10 12:28:58 -0800

    fix: feat: arbitrary length range check and tests

--- crates/proof-of-sql/src/sql/proof_gadgets/range_check_test.rs
+++ crates/proof-of-sql/src/sql/proof_gadgets/range_check_test.rs
@@ -333,7 +333,7 @@ mod tests {
         let data: OwnedTable<DoryScalar> = owned_table([scalar(
             "a",
             (0..2u32.pow(20))
-                .map(|i| upper_bound - DoryScalar::from(i as u64)) // Count backward from 2^248
+                .map(|i| upper_bound - DoryScalar::from(u64::from(i))) // Count backward from 2^248
                 .collect::<Vec<_>>(),
         )]);
 
