460a2d006471a35068c0859818f1876bcce12c96
Jay White <JayWhite2357@users.noreply.github.com>
2025-01-20 22:08:29 -0500

    feat: simplify serialization of proof plans for EVM compatibility (#490)
    # Rationale for this change
    
    Previously I merged a custom EVM serializer for the query plan. This
    incurs a maintenance cost. This PR leverages serde instead, at the cost
    of a bit less flexibility.
    
    # What changes are included in this PR?
    
    The `DynProofPlan` is converted to a simplified plan. The main change is
    removing any table and column references.
    This simplified plan can be serialized using serde (in particular,
    bincode+bigendian+fixint gives an easy EVM format).
    Ultimately, this is used by an `EVMProofPlan` that is a newtype wrapper
    around `DynProofPlan` that serializes through this simplified plan. This
    allows `EVMProofPlan` to implement `ProofPlan` while still having a
    serialization different from the more complex `DynProofPlan`s.
    
    # Are these changes tested?
    Yes. Later PRs will demonstrate EVM compatibility.

---
+++ crates/proof-of-sql/src/sql/evm_proof_plan/plans.rs
@@ -0,0 +1,62 @@
+use super::{error::Error, exprs::Expr};
+use crate::{
+    base::{
+        database::{ColumnRef, TableRef},
+        map::IndexSet,
+    },
+    sql::proof_plans::{self, DynProofPlan},
+};
+use alloc::vec::Vec;
+use serde::Serialize;
+
+/// Represents a plan that can be serialized for EVM.
+#[derive(Serialize)]
+pub(super) enum Plan {
+    Filter(FilterExec),
+}
+
+impl Plan {
+    /// Try to create a `Plan` from a `DynProofPlan`.
+    pub(super) fn try_from_proof_plan(
+        plan: &DynProofPlan,
+        table_refs: &IndexSet<TableRef>,
+        column_refs: &IndexSet<ColumnRef>,
+    ) -> Result<Self, Error> {
+        match plan {
+            DynProofPlan::Filter(filter_exec) => {
+                FilterExec::try_from_proof_plan(filter_exec, table_refs, column_refs)
+                    .map(Self::Filter)
+            }
+            _ => Err(Error::NotSupported),
+        }
+    }
+}
+
+/// Represents a filter execution plan.
+#[derive(Serialize)]
+pub(super) struct FilterExec {
+    table_number: usize,
+    results: Vec<Expr>,
+    where_clause: Expr,
+}
+
+impl FilterExec {
+    /// Try to create a `FilterExec` from a `proof_plans::FilterExec`.
+    fn try_from_proof_plan(
+        plan: &proof_plans::FilterExec,
+        table_refs: &IndexSet<TableRef>,
+        column_refs: &IndexSet<ColumnRef>,
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            table_number: table_refs
+                .get_index_of(&plan.table.table_ref)
+                .ok_or(Error::TableNotFound)?,
+            results: plan
+                .aliased_results
+                .iter()
+                .map(|result| Expr::try_from_proof_expr(&result.expr, column_refs))
+                .collect::<Result<_, _>>()?,
+            where_clause: Expr::try_from_proof_expr(&plan.where_clause, column_refs)?,
+        })
+    }
+}
