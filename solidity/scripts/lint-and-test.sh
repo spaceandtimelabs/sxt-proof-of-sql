0db194aa25d5f8f5d483d2ea8709705a39aa7a6b
Jay White <JayWhite2357@users.noreply.github.com>
2025-02-12 13:48:13 -0500

    ci: add forge fmt to ci and other improvements

--- solidity/scripts/lint-and-test.sh
+++ solidity/scripts/lint-and-test.sh
@@ -10,5 +10,6 @@ scripts/install_deps.sh
 scripts/pre_forge.sh clean
 scripts/pre_forge.sh test --summary
 scripts/check_coverage.sh
-solhint 'src/**/*.sol' 'test/**/*.sol' -w 0
+scripts/pre_forge.sh fmt --check
+solhint '**/*.sol' -w 0
 slither .
