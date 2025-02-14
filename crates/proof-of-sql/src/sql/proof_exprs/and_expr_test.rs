3bdf25881592c309cf79c45de4b8a16ab312ebe1
stuarttimwhite <stuart.white@spaceandtime.io>
2025-02-14 08:11:07 -0500

    test: add and_expr tests to demonstrate how to test verification_builder in isolation

--- crates/proof-of-sql/src/sql/proof_exprs/and_expr_test.rs
+++ crates/proof-of-sql/src/sql/proof_exprs/and_expr_test.rs
@@ -2,14 +2,19 @@ use crate::{
     base::{
         commitment::InnerProductProof,
         database::{
-            owned_table_utility::*, table_utility::*, Column, OwnedTableTestAccessor, TableRef,
-            TableTestAccessor,
+            owned_table_utility::*, table_utility::*, Column, ColumnRef, ColumnType,
+            OwnedTableTestAccessor, Table, TableRef, TableTestAccessor,
         },
-        scalar::test_scalar::TestScalar,
+        map::indexmap,
+        polynomial::MultilinearExtension,
+        scalar::{test_scalar::TestScalar, Scalar},
     },
     sql::{
-        proof::{exercise_verification, VerifiableQueryResult},
-        proof_exprs::{test_utility::*, DynProofExpr, ProofExpr},
+        proof::{
+            exercise_verification, mock_verification_builder::MockVerificationBuilder,
+            FinalRoundBuilder, VerifiableQueryResult,
+        },
+        proof_exprs::{test_utility::*, AndExpr, ColumnExpr, DynProofExpr, ProofExpr},
         proof_plans::test_utility::*,
     },
 };
@@ -20,6 +25,8 @@ use rand::{
     rngs::StdRng,
 };
 use rand_core::SeedableRng;
+use sqlparser::ast::Ident;
+use std::collections::VecDeque;
 
 #[test]
 fn we_can_prove_a_simple_and_query() {
@@ -175,4 +182,96 @@ fn we_can_compute_the_correct_output_of_an_and_expr_using_result_evaluate() {
     let res = and_expr.result_evaluate(&alloc, &data);
     let expected_res = Column::Boolean(&[false, true, false, false]);
     assert_eq!(res, expected_res);
+}
+
+#[test]
+fn we_can_verify_a_simple_proof() {
+    let alloc = Bump::new();
+    let t: TableRef = "sxt.t".parse().unwrap();
+    let lhs = &[true, true, false, false];
+    let rhs = &[true, false, true, false];
+    let table = Table::try_new(indexmap! {
+        "a".into() => Column::Boolean::<TestScalar>(lhs),
+        "b".into() => Column::Boolean::<TestScalar>(rhs),
+    })
+    .unwrap();
+    let a = ColumnRef::new(t.clone(), Ident::from("a"), ColumnType::Boolean);
+    let b = ColumnRef::new(t, Ident::from("b"), ColumnType::Boolean);
+    let and_expr = AndExpr::new(
+        Box::new(DynProofExpr::Column(ColumnExpr::new(a.clone()))),
+        Box::new(DynProofExpr::Column(ColumnExpr::new(b.clone()))),
+    );
+
+    let mut final_round_builder: FinalRoundBuilder<'_, TestScalar> =
+        FinalRoundBuilder::new(4, VecDeque::new());
+
+    and_expr.prover_evaluate(&mut final_round_builder, &alloc, &table);
+
+    let matrix = verify_row_by_row(
+        4,
+        &final_round_builder,
+        3,
+        |verification_builder, chi_eval, evaluation_point| {
+            let accessor = indexmap! {
+                a.clone() => lhs.inner_product(evaluation_point),
+                b.clone() => rhs.inner_product(evaluation_point)
+            };
+            and_expr
+                .verifier_evaluate(verification_builder, &accessor, chi_eval)
+                .unwrap();
+        },
+    );
+    assert_eq!(matrix, vec![vec![true]; 4]);
+}
+
+#[test]
+fn we_can_reject_a_simple_tampered_proof() {
+    let alloc = Bump::new();
+    let t: TableRef = "sxt.t".parse().unwrap();
+    let lhs = &[true, true, false, false];
+    let rhs = &[true, false, true, false];
+    let a = ColumnRef::new(t.clone(), Ident::from("a"), ColumnType::Boolean);
+    let b = ColumnRef::new(t, Ident::from("b"), ColumnType::Boolean);
+    let and_expr = AndExpr::new(
+        Box::new(DynProofExpr::Column(ColumnExpr::new(a.clone()))),
+        Box::new(DynProofExpr::Column(ColumnExpr::new(b.clone()))),
+    );
+
+    let evaluation_points = (0..4).map(|i| {
+        alloc.alloc_slice_fill_with(4, |j| {
+            if i == j {
+                TestScalar::ONE
+            } else {
+                TestScalar::ZERO
+            }
+        })
+    });
+    let zero_vec = vec![TestScalar::ZERO];
+    // Tampering occurs here. All four rows return false for lhs and rhs
+    let final_round_mles: Vec<_> = evaluation_points
+        .clone()
+        .map(|_| zero_vec.clone())
+        .collect();
+    let mut verification_builder = MockVerificationBuilder::new(Vec::new(), 3, final_round_mles);
+
+    for evaluation_point in evaluation_points {
+        let chi_eval = (&[1, 1, 1, 1]).inner_product(evaluation_point);
+        let accessor = indexmap! {
+            a.clone() => lhs.inner_product(evaluation_point),
+            b.clone() => rhs.inner_product(evaluation_point)
+        };
+        and_expr
+            .verifier_evaluate(&mut verification_builder, &accessor, chi_eval)
+            .unwrap();
+        verification_builder.increment_row_index();
+    }
+    assert_eq!(
+        verification_builder.identity_subpolynomial_evaluations,
+        vec![
+            vec![-TestScalar::ONE],
+            zero_vec.clone(),
+            zero_vec.clone(),
+            zero_vec
+        ]
+    );
 }
