286327b016b6d5a1c22369059f63ff7135c8cb39
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-02-19 20:20:24 -0500

    feat: add `logical_plans::Expr`

---
+++ crates/proof-of-sql/src/sql/logical_plans/mod.rs
@@ -0,0 +1,5 @@
+//! This module contains the logical plan representation used in both proof generation and postprocessing.
+mod error;
+pub use error::LogicalPlanError;
+mod expr;
+pub use expr::Expr;
