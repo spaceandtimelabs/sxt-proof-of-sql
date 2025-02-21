b6f94224c827cff6714529bc1b411eb24e513616
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-02-21 13:42:39 -0500

    fix: address review comments

--- crates/proof-of-sql/src/sql/logical_plans/expr.rs
+++ crates/proof-of-sql/src/sql/logical_plans/expr.rs
@@ -1,15 +1,15 @@
 use super::LogicalPlanError;
 use crate::base::database::{ColumnRef, LiteralValue};
-use alloc::{boxed::Box, format};
+use alloc::boxed::Box;
 use serde::{Deserialize, Serialize};
-use sqlparser::ast::BinaryOperator as SqlBinaryOperator;
+use sqlparser::ast;
 
 /// Enum of column expressions that are either provable or supported in postprocessing
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum Expr {
     /// Column
     Column(ColumnRef),
-    /// Provable CONST expression
+    /// A constant expression
     Literal(LiteralValue),
     /// Binary operation
     Binary {
@@ -30,49 +30,47 @@ pub enum BinaryOperator {
     /// Equals
     Eq,
     /// Not equals
-    Neq,
+    NotEq,
     /// Greater than
     Gt,
     /// Less than
     Lt,
     /// Greater than or equals
-    Gte,
+    GtEq,
     /// Less than or equals
-    Lte,
+    LtEq,
     /// Logical AND
     And,
     /// Logical OR
     Or,
-    /// Add
-    Add,
-    /// Subtract
-    Sub,
+    /// Plus
+    Plus,
+    /// Minus
+    Minus,
     /// Multiply
-    Mul,
+    Multiply,
     /// Divide
-    Div,
+    Divide,
 }
 
-impl TryFrom<SqlBinaryOperator> for BinaryOperator {
+impl TryFrom<ast::BinaryOperator> for BinaryOperator {
     type Error = LogicalPlanError;
 
-    fn try_from(op: SqlBinaryOperator) -> Result<Self, Self::Error> {
+    fn try_from(op: ast::BinaryOperator) -> Result<Self, Self::Error> {
         match op {
-            SqlBinaryOperator::Eq => Ok(Self::Eq),
-            SqlBinaryOperator::NotEq => Ok(Self::Neq),
-            SqlBinaryOperator::Gt => Ok(Self::Gt),
-            SqlBinaryOperator::Lt => Ok(Self::Lt),
-            SqlBinaryOperator::GtEq => Ok(Self::Gte),
-            SqlBinaryOperator::LtEq => Ok(Self::Lte),
-            SqlBinaryOperator::And => Ok(Self::And),
-            SqlBinaryOperator::Or => Ok(Self::Or),
-            SqlBinaryOperator::Plus => Ok(Self::Add),
-            SqlBinaryOperator::Minus => Ok(Self::Sub),
-            SqlBinaryOperator::Multiply => Ok(Self::Mul),
-            SqlBinaryOperator::Divide => Ok(Self::Div),
-            _ => Err(LogicalPlanError::Unsupported {
-                message: format!("Unsupported binary operator: {op:?}"),
-            }),
+            ast::BinaryOperator::Eq => Ok(BinaryOperator::Eq),
+            ast::BinaryOperator::NotEq => Ok(BinaryOperator::NotEq),
+            ast::BinaryOperator::Gt => Ok(BinaryOperator::Gt),
+            ast::BinaryOperator::Lt => Ok(BinaryOperator::Lt),
+            ast::BinaryOperator::GtEq => Ok(BinaryOperator::GtEq),
+            ast::BinaryOperator::LtEq => Ok(BinaryOperator::LtEq),
+            ast::BinaryOperator::And => Ok(BinaryOperator::And),
+            ast::BinaryOperator::Or => Ok(BinaryOperator::Or),
+            ast::BinaryOperator::Plus => Ok(BinaryOperator::Plus),
+            ast::BinaryOperator::Minus => Ok(BinaryOperator::Minus),
+            ast::BinaryOperator::Multiply => Ok(BinaryOperator::Multiply),
+            ast::BinaryOperator::Divide => Ok(BinaryOperator::Divide),
+            _ => Err(LogicalPlanError::UnsupportedBinaryOperator { op }),
         }
     }
 }
@@ -86,31 +84,34 @@ mod tests {
     fn we_can_convert_supported_sqlparser_binary_operators() {
         // Let's test all our supported binary operators.
         let test_cases = vec![
-            (SqlBinaryOperator::Eq, BinaryOperator::Eq),
-            (SqlBinaryOperator::NotEq, BinaryOperator::Neq),
-            (SqlBinaryOperator::Gt, BinaryOperator::Gt),
-            (SqlBinaryOperator::Lt, BinaryOperator::Lt),
-            (SqlBinaryOperator::GtEq, BinaryOperator::Gte),
-            (SqlBinaryOperator::LtEq, BinaryOperator::Lte),
-            (SqlBinaryOperator::And, BinaryOperator::And),
-            (SqlBinaryOperator::Or, BinaryOperator::Or),
-            (SqlBinaryOperator::Plus, BinaryOperator::Add),
-            (SqlBinaryOperator::Minus, BinaryOperator::Sub),
-            (SqlBinaryOperator::Multiply, BinaryOperator::Mul),
-            (SqlBinaryOperator::Divide, BinaryOperator::Div),
+            (ast::BinaryOperator::Eq, BinaryOperator::Eq),
+            (ast::BinaryOperator::NotEq, BinaryOperator::NotEq),
+            (ast::BinaryOperator::Gt, BinaryOperator::Gt),
+            (ast::BinaryOperator::Lt, BinaryOperator::Lt),
+            (ast::BinaryOperator::GtEq, BinaryOperator::GtEq),
+            (ast::BinaryOperator::LtEq, BinaryOperator::LtEq),
+            (ast::BinaryOperator::And, BinaryOperator::And),
+            (ast::BinaryOperator::Or, BinaryOperator::Or),
+            (ast::BinaryOperator::Plus, BinaryOperator::Plus),
+            (ast::BinaryOperator::Minus, BinaryOperator::Minus),
+            (ast::BinaryOperator::Multiply, BinaryOperator::Multiply),
+            (ast::BinaryOperator::Divide, BinaryOperator::Divide),
         ];
 
         for (sql_op, expected) in test_cases {
             let result = BinaryOperator::try_from(sql_op).unwrap();
-            assert_eq!(result, expected,);
+            assert_eq!(result, expected);
         }
     }
 
     #[test]
     fn we_cannot_convert_unsupported_sqlparser_binary_operators() {
         // Let's test an unsupported operator.
-        let unsupported_op = SqlBinaryOperator::Spaceship;
+        let unsupported_op = ast::BinaryOperator::Spaceship;
         let result = BinaryOperator::try_from(unsupported_op);
-        assert!(matches!(result, Err(LogicalPlanError::Unsupported { .. })));
+        assert!(matches!(
+            result,
+            Err(LogicalPlanError::UnsupportedBinaryOperator { .. })
+        ));
     }
 }
