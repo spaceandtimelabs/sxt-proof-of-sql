428d426f8c7c260ad111976578e9267af6647c52
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-02-21 13:42:39 -0500

    fix: address review comments

--- crates/proof-of-sql/src/sql/logical_plans/error.rs
+++ crates/proof-of-sql/src/sql/logical_plans/error.rs
@@ -1,13 +1,12 @@
-use alloc::string::String;
 use snafu::Snafu;
 
 /// Errors encountered during the process of converting `sqlparser::ast::Statement` to `LogicalPlan`
 #[derive(Debug, Snafu)]
 pub enum LogicalPlanError {
-    #[snafu(display("Unsupported feature: {:?}", message))]
-    /// Used when a feature parseable in `sqlparser` is not supported
-    Unsupported {
-        /// The unsupported feature
-        message: String,
+    #[snafu(display("Unsupported binary operator: {:?}", op))]
+    /// Used when a binary operator is not supported
+    UnsupportedBinaryOperator {
+        /// The unsupported binary operator
+        op: sqlparser::ast::BinaryOperator,
     },
 }
