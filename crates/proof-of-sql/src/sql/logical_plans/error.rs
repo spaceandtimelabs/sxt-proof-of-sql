c519e1cf71f0866eb4c02f488a6ed2361744ecae
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-02-24 10:31:58 -0500

    feat: add `sqlparser::ast::Value` -> `LiteralValue` conversion

--- crates/proof-of-sql/src/sql/logical_plans/error.rs
+++ crates/proof-of-sql/src/sql/logical_plans/error.rs
@@ -1,4 +1,6 @@
+use crate::base::math::decimal::DecimalError;
 use snafu::Snafu;
+use sqlparser::ast::{BinaryOperator, Value};
 
 /// Errors encountered during the process of converting `sqlparser::ast::Statement` to `LogicalPlan`
 #[derive(Debug, Snafu)]
@@ -7,6 +9,18 @@ pub enum LogicalPlanError {
     /// Used when a binary operator is not supported
     UnsupportedBinaryOperator {
         /// The unsupported binary operator
-        op: sqlparser::ast::BinaryOperator,
+        op: BinaryOperator,
+    },
+    #[snafu(display("Unsupported Value: {:?}", value))]
+    /// Used when a value is not supported
+    UnsupportedValue {
+        /// The unsupported value
+        value: Value,
+    },
+    /// Used when a value can not be parsed as a decimal
+    #[snafu(transparent)]
+    DecimalParseError {
+        /// The underlying error
+        source: DecimalError,
     },
 }
--- crates/proof-of-sql/src/sql/logical_plans/sqlparser.rs
+++ crates/proof-of-sql/src/sql/logical_plans/sqlparser.rs
@@ -1,6 +1,61 @@
 //! This module contains the conversion functions for converting sqlparser types to our own Logical Plan.
 use super::{BinaryOperator, LogicalPlanError};
+use crate::base::{
+    database::LiteralValue,
+    math::{
+        decimal::{DecimalError, DecimalResult, IntermediateDecimalError, Precision},
+        i256::I256,
+        BigDecimalExt,
+    },
+};
+use bigdecimal::BigDecimal;
+use num_bigint::BigInt;
 use sqlparser::ast;
+
+/// Parse a decimal value from a string
+fn parse_decimal(value: &str) -> DecimalResult<LiteralValue> {
+    let dec: BigDecimal =
+        value
+            .parse()
+            .map_err(|e| DecimalError::IntermediateDecimalConversionError {
+                source: IntermediateDecimalError::ParseError { error: e },
+            })?;
+    let precision = u8::try_from(dec.precision()).map_err(|_| DecimalError::InvalidPrecision {
+        error: format!("Precision {} is too large", dec.precision()),
+    })?;
+    let scale = i8::try_from(dec.scale()).map_err(|_| DecimalError::InvalidScale {
+        scale: format!("Scale {} is too large", dec.scale()),
+    })?;
+    let bigint = dec
+        .try_into_bigint_with_precision_and_scale(precision, scale)
+        .map_err(|source| DecimalError::IntermediateDecimalConversionError { source })?;
+    // Since I256::from_num_bigint() doesn't error out on numbers out of range we need to check here
+    if bigint.bits() > 255 || (bigint.clone() + BigInt::from(1_i64)).bits() > 255 {
+        return Err(DecimalError::InvalidPrecision {
+            error: format!("{bigint} is out of range for I256"),
+        });
+    }
+    let i256 = I256::from_num_bigint(&bigint);
+    let real_precision = Precision::new(precision).map_err(|e| DecimalError::InvalidPrecision {
+        error: format!("Precision {e} is too large"),
+    })?;
+    Ok(LiteralValue::Decimal75(real_precision, scale, i256))
+}
+
+impl TryFrom<ast::Value> for LiteralValue {
+    type Error = LogicalPlanError;
+
+    fn try_from(value: ast::Value) -> Result<Self, Self::Error> {
+        match value {
+            ast::Value::Number(n, _) => {
+                parse_decimal(&n).map_err(|source| LogicalPlanError::DecimalParseError { source })
+            }
+            ast::Value::SingleQuotedString(s) => Ok(LiteralValue::VarChar(s)),
+            ast::Value::Boolean(b) => Ok(LiteralValue::Boolean(b)),
+            _ => Err(LogicalPlanError::UnsupportedValue { value }),
+        }
+    }
+}
 
 impl TryFrom<ast::BinaryOperator> for BinaryOperator {
     type Error = LogicalPlanError;
@@ -27,6 +82,17 @@ impl TryFrom<ast::BinaryOperator> for BinaryOperator {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // Decimal parsing
+    #[test]
+    fn we_can_parse_a_decimal() {
+        let decimal_str = "123.45";
+        let result = parse_decimal(decimal_str).unwrap();
+        assert_eq!(
+            result,
+            LiteralValue::Decimal75(Precision::new(5).unwrap(), 2, I256::from(12345))
+        );
+    }
 
     // Binary operators
     #[test]
