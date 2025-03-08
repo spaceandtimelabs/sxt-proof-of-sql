61ac492f8af815c9a43a5f5a99c4eef3f73eaf6f
Denizhan Dakılır <denizhandakilirn@gmail.com>
2025-03-09 00:44:15 +0300

    Revert "Merge branch 'firstpr' of https://github.com/zelosleone/sxt-proof-of-sql into firstpr"
    This reverts commit 6ee9b927ef8f778b2456035d45804881b7760f55, reversing
    changes made to 7787fd5c453c36af02ad2b3ec02a34eb86c2754f.

--- crates/proof-of-sql/src/base/arrow/owned_and_arrow_conversions.rs
+++ crates/proof-of-sql/src/base/arrow/owned_and_arrow_conversions.rs
@@ -137,9 +137,7 @@ impl<S: Scalar> From<OwnedNullableColumn<S>> for ArrayRef {
         }
 
         let presence = value.presence.unwrap();
-        let null_buffer = (0..presence.len())
-            .map(|i| presence[i])
-            .collect::<NullBuffer>();
+        let null_buffer = NullBuffer::from_iter((0..presence.len()).map(|i| presence[i]));
 
         match value.values {
             OwnedColumn::Boolean(col) => Arc::new(BooleanArray::new(col.into(), Some(null_buffer))),
@@ -165,7 +163,7 @@ impl<S: Scalar> From<OwnedNullableColumn<S>> for ArrayRef {
             OwnedColumn::VarChar(col) => {
                 let mut builder = arrow::array::StringBuilder::with_capacity(
                     col.len(),
-                    col.iter().map(std::string::String::len).sum(),
+                    col.iter().map(|s| s.len()).sum(),
                 );
                 for (i, s) in col.iter().enumerate() {
                     if presence[i] {
@@ -179,7 +177,7 @@ impl<S: Scalar> From<OwnedNullableColumn<S>> for ArrayRef {
             OwnedColumn::VarBinary(col) => {
                 let mut builder = arrow::array::BinaryBuilder::with_capacity(
                     col.len(),
-                    col.iter().map(std::vec::Vec::len).sum(),
+                    col.iter().map(|s| s.len()).sum(),
                 );
                 for (i, s) in col.iter().enumerate() {
                     if presence[i] {
@@ -238,7 +236,6 @@ impl<S: Scalar> TryFrom<ArrayRef> for OwnedNullableColumn<S> {
 impl<S: Scalar> TryFrom<&ArrayRef> for OwnedNullableColumn<S> {
     type Error = OwnedArrowConversionError;
 
-    #[allow(clippy::too_many_lines)]
     fn try_from(value: &ArrayRef) -> Result<Self, Self::Error> {
         let has_nulls = value.null_count() > 0;
 
@@ -250,15 +247,11 @@ impl<S: Scalar> TryFrom<&ArrayRef> for OwnedNullableColumn<S> {
         let len = value.len();
         let mut presence = vec![true; len];
 
-        presence
-            .iter_mut()
-            .enumerate()
-            .take(len)
-            .for_each(|(i, present)| {
-                if value.is_null(i) {
-                    *present = false;
-                }
-            });
+        for i in 0..len {
+            if value.is_null(i) {
+                presence[i] = false;
+            }
+        }
 
         let owned_column = match value.data_type() {
             DataType::Boolean => {
--- crates/proof-of-sql/src/base/database/owned_column.rs
+++ crates/proof-of-sql/src/base/database/owned_column.rs
@@ -54,7 +54,7 @@ pub enum OwnedColumn<S: Scalar> {
     TimestampTZ(PoSQLTimeUnit, PoSQLTimeZone, Vec<i64>),
 }
 
-/// A nullable column that contains a values `OwnedColumn`,
+/// A nullable column that contains a values OwnedColumn,
 /// and an optional boolean presence vector.
 ///
 /// When `presence` is `None`, the column is not nullable (all values are present).
@@ -62,7 +62,7 @@ pub enum OwnedColumn<S: Scalar> {
 /// and a `true` value indicates the presence of a value at the corresponding index,
 /// while a `false` value indicates NULL.
 ///
-/// This implementation follows the `PostgreSQL` approach to NULL values by using
+/// This implementation follows the PostgreSQL approach to NULL values by using
 /// a separate boolean array to track presence.
 #[derive(Debug, PartialEq, Clone, Eq, Serialize, Deserialize)]
 pub struct OwnedNullableColumn<S: Scalar> {
@@ -480,7 +480,7 @@ impl<S: Scalar> OwnedColumn<S> {
 }
 
 impl<S: Scalar> OwnedNullableColumn<S> {
-    /// Creates a new `OwnedNullableColumn` without any NULL values
+    /// Creates a new OwnedNullableColumn without any NULL values
     /// (all values are present)
     #[must_use]
     pub fn new(values: OwnedColumn<S>) -> Self {
@@ -490,7 +490,7 @@ impl<S: Scalar> OwnedNullableColumn<S> {
         }
     }
 
-    /// Creates a new `OwnedNullableColumn` with the given values and presence vector
+    /// Creates a new OwnedNullableColumn with the given values and presence vector
     ///
     /// # Panics
     ///
@@ -538,7 +538,9 @@ impl<S: Scalar> OwnedNullableColumn<S> {
     /// Panics if the index is out of bounds
     #[must_use]
     pub fn is_null(&self, index: usize) -> bool {
-        assert!(index < self.len(), "Index out of bounds");
+        if index >= self.len() {
+            panic!("Index out of bounds");
+        }
         match &self.presence {
             Some(presence) => !presence[index],
             None => false, // When presence is None, no values are NULL
@@ -576,7 +578,9 @@ impl<S: Scalar> OwnedNullableColumn<S> {
     /// Panics if the index is out of bounds
     #[must_use]
     pub fn scalar_at(&self, index: usize) -> Option<Option<S>> {
-        assert!(index < self.len(), "Index out of bounds");
+        if index >= self.len() {
+            panic!("Index out of bounds");
+        }
 
         // Check if the value is NULL
         if let Some(presence) = &self.presence {
@@ -592,14 +596,13 @@ impl<S: Scalar> OwnedNullableColumn<S> {
             OwnedColumn::TinyInt(col) => Some(Some(col[index].into())),
             OwnedColumn::SmallInt(col) => Some(Some(col[index].into())),
             OwnedColumn::Int(col) => Some(Some(col[index].into())),
-            OwnedColumn::BigInt(col) | OwnedColumn::TimestampTZ(_, _, col) => {
-                Some(Some(col[index].into()))
-            }
-            OwnedColumn::VarChar(col) => Some(Some(col[index].clone().into())),
-            OwnedColumn::VarBinary(_col) => None, // VarBinary cannot be converted to scalar directly
-            // VarChar and VarBinary would need hash values
+            OwnedColumn::BigInt(col) => Some(Some(col[index].into())),
             OwnedColumn::Int128(col) => Some(Some(col[index].into())),
-            OwnedColumn::Scalar(col) | OwnedColumn::Decimal75(_, _, col) => Some(Some(col[index])),
+            OwnedColumn::Scalar(col) => Some(Some(col[index])),
+            OwnedColumn::Decimal75(_, _, col) => Some(Some(col[index])),
+            OwnedColumn::TimestampTZ(_, _, col) => Some(Some(col[index].into())),
+            // VarChar and VarBinary would need hash values
+            _ => None,
         }
     }
 
@@ -633,14 +636,13 @@ impl<S: Scalar> OwnedNullableColumn<S> {
             OwnedColumn::TinyInt(col) => Some(Some(col[index].into())),
             OwnedColumn::SmallInt(col) => Some(Some(col[index].into())),
             OwnedColumn::Int(col) => Some(Some(col[index].into())),
-            OwnedColumn::BigInt(col) | OwnedColumn::TimestampTZ(_, _, col) => {
-                Some(Some(col[index].into()))
-            }
-            OwnedColumn::VarChar(col) => Some(Some(col[index].clone().into())),
-            OwnedColumn::VarBinary(_col) => None, // VarBinary cannot be converted to scalar directly
-            // VarChar and VarBinary would need hash values
+            OwnedColumn::BigInt(col) => Some(Some(col[index].into())),
             OwnedColumn::Int128(col) => Some(Some(col[index].into())),
-            OwnedColumn::Scalar(col) | OwnedColumn::Decimal75(_, _, col) => Some(Some(col[index])),
+            OwnedColumn::Scalar(col) => Some(Some(col[index])),
+            OwnedColumn::Decimal75(_, _, col) => Some(Some(col[index])),
+            OwnedColumn::TimestampTZ(_, _, col) => Some(Some(col[index].into())),
+            // VarChar and VarBinary would need hash values
+            _ => None,
         }
     }
 
@@ -668,15 +670,18 @@ impl<S: Scalar> OwnedNullableColumn<S> {
         let mut has_nulls = false;
 
         for opt_scalar in option_scalars {
-            if let Some(val) = opt_scalar {
-                values.push(*val);
-                presence.push(true);
-            } else {
-                // Push a default value for NULL entries
-                // This value won't be used since the presence vector marks it as NULL
-                values.push(S::default());
-                presence.push(false);
-                has_nulls = true;
+            match opt_scalar {
+                Some(val) => {
+                    values.push(*val);
+                    presence.push(true);
+                }
+                None => {
+                    // Push a default value for NULL entries
+                    // This value won't be used since the presence vector marks it as NULL
+                    values.push(S::default());
+                    presence.push(false);
+                    has_nulls = true;
+                }
             }
         }
 
@@ -693,7 +698,7 @@ impl<S: Scalar> OwnedNullableColumn<S> {
     ///
     /// This is similar to `try_from_option_scalars` but works with iterators.
     ///
-    /// ## `ExactSizeIterator` Requirement
+    /// ## ExactSizeIterator Requirement
     ///
     /// This method requires an `ExactSizeIterator` to enable efficient pre-allocation of
     /// the internal vectors. Knowing the exact size upfront allows us to allocate the right
@@ -730,14 +735,17 @@ impl<S: Scalar> OwnedNullableColumn<S> {
         let mut has_nulls = false;
 
         for opt_scalar in iter {
-            if let Some(val) = opt_scalar {
-                values.push(val);
-                presence.push(true);
-            } else {
-                // Push a default value for NULL entries
-                values.push(S::default());
-                presence.push(false);
-                has_nulls = true;
+            match opt_scalar {
+                Some(val) => {
+                    values.push(val);
+                    presence.push(true);
+                }
+                None => {
+                    // Push a default value for NULL entries
+                    values.push(S::default());
+                    presence.push(false);
+                    has_nulls = true;
+                }
             }
         }
 
@@ -754,7 +762,7 @@ impl<S: Scalar> OwnedNullableColumn<S> {
 impl<'a, S: Scalar> From<&NullableColumn<'a, S>> for OwnedNullableColumn<S> {
     fn from(nullable_column: &NullableColumn<'a, S>) -> Self {
         let values = OwnedColumn::from(&nullable_column.values);
-        let presence = nullable_column.presence.map(<[bool]>::to_vec);
+        let presence = nullable_column.presence.map(|p| p.to_vec());
         Self { values, presence }
     }
 }
