2ecb128381b82a0497e0adf6da1742ef3ac19448
Hopium <135053852+Hopium21@users.noreply.github.com>
2025-03-12 05:38:14 +0100

    Update main.rs

--- crates/proof-of-sql/examples/albums/main.rs
+++ crates/proof-of-sql/examples/albums/main.rs
@@ -42,7 +42,7 @@ fn prove_and_verify_query(
     println!("Done in {} ms.", now.elapsed().as_secs_f64() * 1000.);
 
     // Generate the proof and result:
-    println!("Generating proof...");
+    print!("Generating proof...");
     let now = Instant::now();
     let verifiable_result = VerifiableQueryResult::<DynamicDoryEvaluationProof>::new(
         query_plan.proof_expr(),
