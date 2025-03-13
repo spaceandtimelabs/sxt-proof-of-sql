098f3e2b8b7de84a8497bee62cfe71cb38c1bd4f
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-03-13 13:46:07 -0400

    feat: add `AnalyzeError`

---
+++ crates/proof-of-sql/src/sql/error.rs
@@ -0,0 +1,63 @@
+use crate::base::{
+    database::ColumnType,
+    math::decimal::{DecimalError, IntermediateDecimalError},
+};
+use alloc::string::{String, ToString};
+use core::result::Result;
+use snafu::Snafu;
+
+/// Errors related to queries that can not be run due to invalid column references, data types, etc.
+/// Will be replaced once we fully switch to the planner.
+#[derive(Snafu, Debug, PartialEq, Eq)]
+pub enum AnalyzeError {
+    #[snafu(display("Expected '{expected}' but found '{actual}'"))]
+    /// Invalid data type received
+    InvalidDataType {
+        /// Expected data type
+        expected: ColumnType,
+        /// Actual data type found
+        actual: ColumnType,
+    },
+
+    #[snafu(display("Left side has '{left_type}' type but right side has '{right_type}' type"))]
+    /// Data types do not match
+    DataTypeMismatch {
+        /// The left side datatype
+        left_type: String,
+        /// The right side datatype
+        right_type: String,
+    },
+
+    #[snafu(display("Columns have different lengths: {len_a} != {len_b}"))]
+    /// Two columns do not have the same length
+    DifferentColumnLength {
+        /// The length of the first column
+        len_a: usize,
+        /// The length of the second column
+        len_b: usize,
+    },
+
+    #[snafu(transparent)]
+    /// Errors related to decimal operations
+    DecimalConversionError {
+        /// The underlying source error
+        source: DecimalError,
+    },
+}
+
+impl From<AnalyzeError> for String {
+    fn from(error: AnalyzeError) -> Self {
+        error.to_string()
+    }
+}
+
+impl From<IntermediateDecimalError> for AnalyzeError {
+    fn from(err: IntermediateDecimalError) -> AnalyzeError {
+        AnalyzeError::DecimalConversionError {
+            source: DecimalError::IntermediateDecimalConversionError { source: err },
+        }
+    }
+}
+
+/// Result type for analyze errors
+pub type AnalyzeResult<T> = Result<T, AnalyzeError>;
--- crates/proof-of-sql/src/sql/mod.rs
+++ crates/proof-of-sql/src/sql/mod.rs
@@ -1,8 +1,11 @@
 //! This module contains the main logic for Proof of SQL.
 
+mod error;
 /// This module holds the [`EVMProofPlan`] struct and its implementation, which allows for EVM compatible serialization.
 pub mod evm_proof_plan;
 pub mod parse;
+/// This temporarily exists until we switch to using Datafusion Analyzer to handle type checking.
+pub use error::{AnalyzeError, AnalyzeResult};
 pub mod postprocessing;
 pub mod proof;
 pub mod proof_exprs;
