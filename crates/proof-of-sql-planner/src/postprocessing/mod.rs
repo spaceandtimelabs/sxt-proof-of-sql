cfd8e13de7bd6d3b26cc1919a1dd010484886386
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-03-21 13:19:13 -0400

    fix!: fix `postprocessing`

--- crates/proof-of-sql-planner/src/postprocessing/mod.rs
+++ crates/proof-of-sql-planner/src/postprocessing/mod.rs
@@ -1,11 +1,15 @@
 /// Proof of SQL Postprocessing. Used when the last step of the logical plan is an unprovable projection.
 mod error;
-#[cfg(test)]
 pub use error::{PostprocessingError, PostprocessingResult};
 mod expression_evaluation;
-#[cfg(test)]
 pub(crate) use expression_evaluation::evaluate_expr;
 mod expression_evaluation_error;
 pub use expression_evaluation_error::{ExpressionEvaluationError, ExpressionEvaluationResult};
 #[cfg(test)]
 mod expression_evaluation_test;
+mod postprocessing_step;
+pub use postprocessing_step::PostprocessingStep;
+mod select_postprocessing;
+pub use select_postprocessing::SelectPostprocessing;
+#[cfg(test)]
+mod select_postprocessing_test;
