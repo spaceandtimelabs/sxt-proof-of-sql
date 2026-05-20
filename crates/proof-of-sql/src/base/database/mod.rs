71c49fa0c7e10fec0dea5e000b735abe11c72da0
Dr Alex Mitre <30060514+mitre88@users.noreply.github.com>
2026-05-19 22:26:50 -0600

    test: run table test accessor without blitzar

--- crates/proof-of-sql/src/base/database/mod.rs
+++ crates/proof-of-sql/src/base/database/mod.rs
@@ -116,7 +116,7 @@ mod owned_table_test_accessor_test;
 
 mod table_test_accessor;
 pub use table_test_accessor::TableTestAccessor;
-#[cfg(all(test, feature = "blitzar"))]
+#[cfg(test)]
 mod table_test_accessor_test;
 
 /// Module providing utilities for filtering columns based on selection vectors.
